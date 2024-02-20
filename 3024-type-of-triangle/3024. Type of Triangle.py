167c92e37765b6cf72f43219e3a6a53e1b1b2eae
Geoffrey Lee <74619195+LGeoff31@users.noreply.github.com>
2024-02-20 15:07:23 -0500

    Runtime: 39 ms (48.68%), Memory: 16.7 MB (11.68%) - Gitcode

---
+++ 3024-type-of-triangle/3024. Type of Triangle.py
@@ -0,0 +1,10 @@
+class Solution:
+    def triangleType(self, nums: List[int]) -> str:
+        if min(nums) + (sum(nums) - min(nums) - max(nums)) > max(nums):
+            if nums[0] == nums[1] and nums[1] == nums[2]: return "equilateral"
+            elif nums[0] == nums[1] or nums[1] == nums[2] or nums[0] == nums[2]: return "isosceles"
+            else: return "scalene"
+        else:
+            return "none"
+
+        
