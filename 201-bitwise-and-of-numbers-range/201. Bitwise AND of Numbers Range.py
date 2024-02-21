4f4d653a5242a332047787c6d6fcc068e3bf5c06
Geoffrey Lee <74619195+LGeoff31@users.noreply.github.com>
2024-02-20 20:39:11 -0500

    Runtime: 59 ms (31.03%), Memory: 16.5 MB (98.92%) - Gitcode

--- 201-bitwise-and-of-numbers-range/201. Bitwise AND of Numbers Range.py
+++ 201-bitwise-and-of-numbers-range/201. Bitwise AND of Numbers Range.py
@@ -3,8 +3,8 @@ class Solution:
     def rangeBitwiseAnd(self, left: int, right: int) -> int:
         if left == 0: return 0
         if left == right: return left
-        # if left == right - 1: return left & right
         if math.floor(math.log(left, 2)) !=  math.floor(math.log(right, 2)): return 0
+
         res = 2 ** math.floor(math.log(left, 2))
         currPower = math.floor(math.log(left, 2))
 
@@ -13,15 +13,13 @@ class Solution:
         while True:
             if left == 0: return res
             if math.floor(math.log(left, 2)) != math.floor(math.log(right, 2)): return res
-        
             for i in range(currPower-1, 0, -1):
                 if left >= 2 ** i:
                     res += 2 ** i
                     left -= 2 ** i
                     right -= 2** i
                     break
-                    print(math.floor(math.log(2,2)))
-                    print(math.floor(math.log(4,2)))
+
 
 
 
