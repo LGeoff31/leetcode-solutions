e0233c197d952d6a75b64c8769f6046781bf027e
Geoffrey Lee <74619195+LGeoff31@users.noreply.github.com>
2024-02-22 12:49:49 -0500

    Runtime: 48 ms (75.70%), Memory: 17.1 MB (51.56%) - Gitcode

--- 649-dota2-senate/649. Dota2 Senate.py
+++ 649-dota2-senate/649. Dota2 Senate.py
@@ -1,27 +1,17 @@
 from collections import deque
 class Solution:
     def predictPartyVictory(self, senate: str) -> str:
-        n = len(senate)
-        r_queue = deque([])
-        d_queue = deque([]) 
-        for i in range(len(senate)):
-            if senate[i] == "R":
-                r_queue.append(i)
-            else:
-                d_queue.append(i)
+        n, r_queue, d_queue = len(senate), deque(), deque()
+        for i in range(n):
+            r_queue.append(i) if senate[i] == "R" else d_queue.append(i)
 
         while r_queue and d_queue:
             minNum = min(len(r_queue), len(d_queue))
             for i in range(minNum):
-                if r_queue[i] < d_queue[i]:
-                    r_queue.append(n)
-                else:
-                    d_queue.append(n)
+                r_queue.append(n) if r_queue[i] < d_queue[i] else d_queue.append(n)
                 n += 1
             for i in range(minNum):
                 r_queue.popleft()
                 d_queue.popleft()
-
-        if r_queue: return "Radiant"
-        else: return "Dire"
+        return "Radiant" if r_queue else "Dire"
         
