e6e8fbacb9b080d0eeff74d04a981360246445e6
Geoffrey Lee <74619195+LGeoff31@users.noreply.github.com>
2024-03-02 21:22:31 -0500

    Runtime: 48 ms (49.73%), Memory: 16.7 MB (54.40%) - Gitcode

--- 162-find-peak-element/162. Find Peak Element.py
+++ 162-find-peak-element/162. Find Peak Element.py
@@ -4,6 +4,13 @@ class Solution:
 
         if nums[0] > nums[1]: return 0
         if nums[-1] > nums[-2]: return len(nums) - 1
-        for i in range(1, len(nums)-1):
-            if nums[i] > nums[i-1] and nums[i] > nums[i+1]:
-                return i
+        l,r = 0, len(nums)-1
+
+        while l <= r:
+            mid = (l+r) // 2
+            if nums[mid] > nums[mid-1] and nums[mid] > nums[mid+1]:
+                return mid
+            elif nums[mid] > nums[mid+1]:
+                r = mid -1
+            else:
+                l = mid + 1
