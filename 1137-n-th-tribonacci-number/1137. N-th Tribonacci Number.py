dbb3a0fdb0d57df8de1c0b7542dd4a8a23d3785f
Geoffrey Lee <74619195+LGeoff31@users.noreply.github.com>
2024-03-03 12:41:04 -0500

    Runtime: 39 ms (25.14%), Memory: 16.7 MB (33.08%) - Gitcode

--- 1137-n-th-tribonacci-number/1137. N-th Tribonacci Number.py
+++ 1137-n-th-tribonacci-number/1137. N-th Tribonacci Number.py
@@ -1,13 +1,8 @@
 class Solution:
-    @ cache
+    @cache
     def tribonacci(self, n: int) -> int:
-        #[0,1,1,2,4,7...]
-        if n == 0:
-            return 0
-        if n == 1:
-            return 1
-        if n == 2:
-            return 1
-        return self.tribonacci(n-1) + self.tribonacci(n-2) + self.tribonacci(n-3)
-
+        if n == 0: return 0
+        if n == 1: return 1
+        if n == 2: return 1
+        return self.tribonacci(n-3) + self.tribonacci(n-1) + self.tribonacci(n-2)
         
