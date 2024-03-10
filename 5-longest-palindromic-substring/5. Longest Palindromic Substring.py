5ba7a524218985820a4714946a7599ed5e19efb9
Geoffrey Lee <74619195+LGeoff31@users.noreply.github.com>
2024-03-10 17:59:15 -0400

    Runtime: 1540 ms (39.48%), Memory: 24.3 MB (15.18%) - Gitcode

--- 5-longest-palindromic-substring/5. Longest Palindromic Substring.py
+++ 5-longest-palindromic-substring/5. Longest Palindromic Substring.py
@@ -1,17 +1,19 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        if len(s) == 1:
-            return s
-        max_string = ""
-        max_len = 0
-        for i in range(len(s)):
-            for j in range(i+1, len(s)):
-                substring = s[i:j+1]
-                if substring == substring[::-1]:
-                    if len(substring) > max_len:
-                        max_string = substring
-                        max_len = len(substring)
-        if len(max_string) < 1:
-            return s[0]
-        return max_string
+
+        dp = [[False] * len(s) for _ in range(len(s))]
         
+        for i in range(len(s)):
+            dp[i][i] = True
+
+        longestSubstring = s[0]
+        for r in range(len(s)-2, -1, -1):
+            for c in range(r+1, len(s)):
+                if s[r] == s[c]:
+                    if c-r == 1 or dp[r+1][c-1]:
+                        dp[r][c] = True
+                        if c-r+1 > len(longestSubstring):
+                            longestSubstring = s[r:c+1] #very slow
+        return longestSubstring
+
+         
