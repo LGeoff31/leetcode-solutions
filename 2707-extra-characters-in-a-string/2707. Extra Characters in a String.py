f29277c507a298ee8691dc79c529c7ff850acc10
Geoffrey Lee <74619195+LGeoff31@users.noreply.github.com>
2024-03-24 18:41:41 -0400

    Runtime: 204 ms (52.43%), Memory: 20.6 MB (8.62%) - Gitcode

--- 2707-extra-characters-in-a-string/2707. Extra Characters in a String.py
+++ 2707-extra-characters-in-a-string/2707. Extra Characters in a String.py
@@ -1,16 +1,39 @@
+class TrieNode():
+    def __init__(self):
+        self.children = {}
+        self.word = False
+
+class Trie():
+    def __init__(self, dictionary):
+        self.root = TrieNode()
+
+        for word in dictionary:
+            curr = self.root
+            for c in word:
+                if c not in curr.children:
+                    curr.children[c] = TrieNode()
+                curr = curr.children[c]
+            curr.word = True
+
 class Solution:
     def minExtraChar(self, s: str, dictionary: List[str]) -> int:
-        lookup = set(dictionary)
+
+        trie = Trie(dictionary).root
         @cache
-        def dfs(idx):
+        def dfs(idx): #O(n^3)
             if idx == len(s): return 0
 
              #skipping character
             res = 1 + dfs(idx+1)
             #no skipping character
+            curr = trie
             for i in range(idx, len(s)):
-                if s[idx:i+1] in lookup: 
-                    res = min(res,dfs(i+1))
+                if s[i] not in curr.children:
+                    break
+                curr = curr.children[s[i]]
+                if curr.word:
+                    res = min(res, dfs(i+1))
+
             return res
         return dfs(0)
 
