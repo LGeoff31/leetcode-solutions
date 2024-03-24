d843889360ebeb707a92abf947e048455e3a2282
Geoffrey Lee <74619195+LGeoff31@users.noreply.github.com>
2024-03-24 18:54:37 -0400

    Runtime: 37 ms (75.85%), Memory: 16.7 MB (64.07%) - Gitcode

--- 139-word-break/139. Word Break.py
+++ 139-word-break/139. Word Break.py
@@ -2,13 +2,15 @@ class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
 
         lookup = set(wordDict)
-        @cache
+        memo = {}
         def dfs(idx):
             if idx == len(s): return True
-
+            if idx in memo:
+                return memo[idx]
             for i in range(idx, len(s)):
                 if s[idx: i+1] in lookup:
-                    if dfs(i+1): return True 
+                    memo[i+1] = dfs(i+1)
+                    if memo[i+1]: return True 
             return False
         return dfs(0)
 
