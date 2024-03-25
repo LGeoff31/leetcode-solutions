b8c1b4791380b635fea9de21eec64d98b18c612a
Geoffrey Lee <74619195+LGeoff31@users.noreply.github.com>
2024-03-25 00:20:12 -0400

    Runtime: 6296 ms (28.60%), Memory: 173.7 MB (5.03%) - Gitcode

--- 212-word-search-ii/212. Word Search II.py
+++ 212-word-search-ii/212. Word Search II.py
@@ -10,31 +10,19 @@ class TrieNode:
                 curr.children[c] = TrieNode()
             curr = curr.children[c]
         curr.word = True
-class Trie:
-    def __init__(self, words):
-        self.root = TrieNode()
-        for word in words:
-            curr = self.root
-            for c in word:
-                if c not in curr.children:
-                    curr.children[c] = TrieNode()
-                curr = curr.children[c]
-            curr.word=True
-
 
 class Solution:
     def findWords(self, board: List[List[str]], words: List[str]) -> List[str]:
         rows, cols = len(board), len(board[0])
-        root = Trie(words).root
+        root = TrieNode()
         res = []
         visited = set()
-        # for word in words:
-        #     root.addWord(word)
-        
+        for word in words:
+            root.addWord(word)
         
         
         def dfs(r,c, node, word):
-            #base cases
+            #nase cases
             if r < 0 or c < 0 or r >= rows or c >= cols: return
             if (r,c) in visited: return
             if board[r][c] not in node.children: return
@@ -45,11 +33,12 @@ class Solution:
             word+=board[r][c]
 
             if node.word: res.append(word)
-            
+
             #recursion
-            direction = [(0,1), (0,-1), (1,0), (-1,0)]
-            for dr, dc in direction:
-                dfs(r+dr, c+dc, node, word)
+            dfs(r+1,c,node,word)
+            dfs(r-1,c,node,word)
+            dfs(r,c+1,node,word)
+            dfs(r,c-1,node,word)
 
             #remove
             visited.remove((r,c))
