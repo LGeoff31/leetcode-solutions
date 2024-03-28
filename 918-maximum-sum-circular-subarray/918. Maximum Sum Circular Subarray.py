f85f3712bfaea481edeb9b5fcc1ca43a98a2943b
Geoffrey Lee <74619195+LGeoff31@users.noreply.github.com>
2024-03-28 10:08:33 -0400

    Runtime: 405 ms (8.76%), Memory: 21.6 MB (91.34%) - Gitcode

--- 918-maximum-sum-circular-subarray/918. Maximum Sum Circular Subarray.py
+++ 918-maximum-sum-circular-subarray/918. Maximum Sum Circular Subarray.py
@@ -1,16 +1,15 @@
 class Solution:
-  def maxSubarraySumCircular(self, A: List[int]) -> int:
-    totalSum = 0
-    currMaxSum = 0
-    currMinSum = 0
-    maxSum = -math.inf
-    minSum = math.inf
-
-    for a in A:
-      totalSum += a
-      currMaxSum = max(currMaxSum + a, a)
-      currMinSum = min(currMinSum + a, a)
-      maxSum = max(maxSum, currMaxSum)
-      minSum = min(minSum, currMinSum)
-
-    return maxSum if maxSum < 0 else max(maxSum, totalSum - minSum)
+    def maxSubarraySumCircular(self, nums: List[int]) -> int:    
+        if max(nums) < 0: return max(nums)
+        maxSum = 0
+        currMaxSum = 0
+        minSum = 1e9
+        currMinSum = 1e9
+        total = 0
+        for i in range(len(nums)):
+            total += nums[i]
+            currMaxSum = max(currMaxSum + nums[i], nums[i]) #can either add elements or start new
+            maxSum = max(maxSum, currMaxSum)
+            currMinSum = min(currMinSum + nums[i], nums[i])
+            minSum = min(minSum, currMinSum)
+        return max(maxSum, total - minSum)
