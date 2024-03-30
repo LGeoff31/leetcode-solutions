8d9a77be1dbaa19436997dc6df9a9c0150186970
Geoffrey Lee <74619195+LGeoff31@users.noreply.github.com>
2024-03-30 10:26:57 -0400

    Runtime: 1254 ms (39.15%), Memory: 41.6 MB (52.84%) - Gitcode

---
+++ 516-longest-palindromic-subsequence/516. Longest Palindromic Subsequence.py
@@ -0,0 +1,18 @@
+class Solution:
+    def longestPalindromeSubseq(self, s: str) -> int:
+        res = 0
+        def longestCommonSubsequence(text1: str, text2: str) -> int:
+            dp = [[0] * (len(text1)+1) for _ in range(len(text2)+1)]
+            for c in range(len(text1)):
+                dp[-1][c] = 0
+            for r in range(len(text2)):
+                dp[r][-1] = 0
+            
+            for r in range(len(text2)-1, -1, -1):
+                for c in range(len(text1)-1, -1, -1):
+                    if text2[r]==text1[c]:
+                        dp[r][c] = 1 + dp[r+1][c+1]
+                    else:
+                        dp[r][c] = max(dp[r+1][c], dp[r][c+1])
+            return dp[0][0]
+        return longestCommonSubsequence(s, s[::-1])
