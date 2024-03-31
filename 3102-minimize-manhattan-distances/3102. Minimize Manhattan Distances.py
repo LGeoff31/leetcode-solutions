2de9a3510cc61be5cd48e2f005dcf022970e3e6e
Geoffrey Lee <74619195+LGeoff31@users.noreply.github.com>
2024-03-31 00:10:52 -0400

    Runtime: 2348 ms (100.00%), Memory: 64.9 MB (100.00%) - Gitcode

---
+++ 3102-minimize-manhattan-distances/3102. Minimize Manhattan Distances.py
@@ -0,0 +1,16 @@
+from sortedcontainers import SortedList
+class Solution:
+    def minimumDistance(self, points: List[List[int]]) -> int:
+        n = len(points)
+        points = [(x + y, x - y) for x, y in points]
+        points.sort()
+        xs = SortedList([x for x, y in points])
+        ys = SortedList([y for x, y in points])
+        res = float('inf')
+        for x, y in points:
+            xs.discard(x)
+            ys.discard(y)
+            res = min(res, max(xs[-1] - xs[0], ys[-1] - ys[0]))
+            xs.add(x)
+            ys.add(y)
+        return res
