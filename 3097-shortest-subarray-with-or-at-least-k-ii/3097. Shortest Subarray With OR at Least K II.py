7bedcba2e5e33f427f01a4919a01efbba7597cb9
Geoffrey Lee <74619195+LGeoff31@users.noreply.github.com>
2024-03-30 20:02:19 -0400

    Runtime: 2439 ms (100.00%), Memory: 38 MB (100.00%) - Gitcode

--- 3097-shortest-subarray-with-or-at-least-k-ii/3097. Shortest Subarray With OR at Least K II.py
+++ 3097-shortest-subarray-with-or-at-least-k-ii/3097. Shortest Subarray With OR at Least K II.py
@@ -1,15 +1,39 @@
 class Solution:
-    def minimumSubarrayLength(self, A: List[int], target: int) -> int:
-        N = len(A)
+    def minimumSubarrayLength(self, nums: List[int], k: int) -> int:
+        for num in nums:
+            if num >= k: return 1
+        currBit = 0
+        bitArr = [0] * 32
+        l,r = 0, 0
 
-        ans = N + 1
-        dp = {}
-        for j, y in enumerate(A):
-            dp = {y | x : i for x, i in dp.items()}
-            dp[y] = max(dp.get(y, j), j)
-            
-            for x, i in dp.items():
-                if x >= target:
-                    ans = min(ans, j - i + 1)
-        
-        return ans if ans <= N else -1
+        def indexes_of_bit_1(bit):
+            indexes = []
+            for index in range(32):  # assuming 32-bit integer
+                if bit & (1 << index):
+                    indexes.append(index)
+            return indexes
+        def get_num_from_arr(arr):
+            res = 0
+            for i in range(len(arr)):
+                if arr[i]:
+                    res += 2**i
+            return res
+        res = 1e9
+        while r < len(nums):
+            currBit |= nums[r]
+            for idx in indexes_of_bit_1(nums[r]):
+                bitArr[idx] +=1
+            if currBit >= k:
+                res = min(res, r-l+1)
+                while l < len(nums) and get_num_from_arr(bitArr) >= k:
+                    print(nums[l], indexes_of_bit_1(nums[l]))
+                    for idx in indexes_of_bit_1(nums[l]):
+                        bitArr[idx]-=1
+                    l+=1
+                    if get_num_from_arr(bitArr) >= k: res = min(res, r-l+1)
+
+            r+=1
+        if res == 1e9: return -1
+        return res
+
+
