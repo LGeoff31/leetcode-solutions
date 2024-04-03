bcbba011d26a491fc666cc90538873df5d73deac
Geoffrey Lee <74619195+LGeoff31@users.noreply.github.com>
2024-04-03 19:16:20 -0400

    Runtime: 6133 ms (14.38%), Memory: 16.6 MB (26.38%) - Gitcode

--- 79-word-search/79. Word Search.py
+++ 79-word-search/79. Word Search.py
@@ -1,24 +1,30 @@
+
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
-        def dfs(i, j, word, visited):
-            #base case
-            # print("word", word)
-            if len(word) == 0:
+        if len(board) == 1 and len(board[0]) == 1 and len(word) == 1 and word[0] == board[0][0]: return True
+        def dfs(r, c, idx, visited):
+            if idx == len(word):
                 return True
-            directions = [(-1,0), (1,0), (0,-1), (0,1)]
-            for di, dj in directions:
-                ni, nj = i + di, j+dj
-                if 0 <= ni < len(board) and 0 <= nj < len(board[0]) and board[ni][nj] == word[0] and (ni,nj) not in visited:
-                    new_visited = set(visited)
-                    new_visited.add((ni,nj))
-                    if dfs(ni, nj, word[1:], new_visited):
+            if r < 0 or r >= rows or c < 0 or c >= cols: return False
+            if (r,c) in visited: return False
+            if board[r][c] != word[idx]: return False
+            a = visited.copy()
+            a.add((r,c))
+            directions = [(-1, 0), (1,0), (0, 1), (0, -1)]
+            for dr, dc in directions:
+                if 0 <= r + dr < rows and 0 <= c + dc < cols:
+                    if dfs(r+dr, c+dc, idx+1, a): 
                         return True
-            return False
 
-        for i in range(len(board)):
-            for j in range(len(board[0])):
-                if board[i][j] == word[0]:
-                    if dfs(i, j, word[1:], {(i,j)}):
+            return False
+            
+        
+        rows, cols = len(board), len(board[0])
+
+        for r in range(rows):
+            for c in range(cols):
+                if board[r][c] == word[0]:
+                    if dfs(r,c, 0, set()):
                         return True
         return False
-        
+       
