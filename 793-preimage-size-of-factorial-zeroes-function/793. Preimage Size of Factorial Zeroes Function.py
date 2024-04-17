4f7ee6db4dfde5db89d70eebe2d0605f7ef497df
Geoffrey Lee <74619195+LGeoff31@users.noreply.github.com>
2024-04-17 11:43:48 -0400

    Runtime: 42 ms (14.40%), Memory: 16.5 MB (40.80%) - Gitcode

--- 793-preimage-size-of-factorial-zeroes-function/793. Preimage Size of Factorial Zeroes Function.py
+++ 793-preimage-size-of-factorial-zeroes-function/793. Preimage Size of Factorial Zeroes Function.py
@@ -1,22 +1,23 @@
-from sortedcontainers import SortedList
 class Solution:
     def preimageSizeFZF(self, k: int) -> int:
-        def zeros(n) -> int:
-            total = 0
+        def zeros(n):  #34 -> 5: 6, 25: 1
             start = 5
-            while start <= n: #34 -> 5: 6, 25: 1
-                total += n // start
+            res = 0
+            while start <= n:
+                res += n // start
                 start *= 5
-            return total
+            return res
         
-        if k == 0: return 5
-        l, r = 0, 6 * k
-        while l < r:
+        l, r = 0, 6*k 
+        while l <= r:
             mid = (l+r) // 2
             if zeros(mid) == k:
                 return 5
             elif zeros(mid) > k:
                 r = mid - 1
-            elif zeros(mid) < k:
+            else:
                 l = mid + 1
         return 0
+
+
+        
