9f1d649ef6b162673169a81c34a5af68ba9209b1
Geoffrey Lee <74619195+LGeoff31@users.noreply.github.com>
2024-04-18 23:34:09 -0400

    Runtime: 74 ms (46.45%), Memory: 16.8 MB (60.07%) - Gitcode

--- 2859-sum-of-values-at-indices-with-k-set-bits/2859. Sum of Values at Indices With K Set Bits.py
+++ 2859-sum-of-values-at-indices-with-k-set-bits/2859. Sum of Values at Indices With K Set Bits.py
@@ -1,15 +1,3 @@
 class Solution:
     def sumIndicesWithKSetBits(self, nums: List[int], k: int) -> int:
-        def bitCount(n):
-            mask = 1
-            res = 0
-            for i in range(32):
-                if mask & n: res+=1
-                mask <<= 1
-            return res
-        res = 0
-        for i in range(len(nums)):
-            if bitCount(i) == k:
-                res+=nums[i]
-        return res
-        
+        return sum([x for i,x in enumerate(nums) if bin(i).count("1")==k])
