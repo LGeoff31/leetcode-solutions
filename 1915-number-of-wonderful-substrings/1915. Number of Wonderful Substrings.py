086a5ce29fff5d12c70c484cf434f59359aa3bbd
Geoffrey Lee <74619195+LGeoff31@users.noreply.github.com>
2024-04-30 15:33:49 -0400

    Runtime: 1595 ms (27.18%), Memory: 18.3 MB (5.77%) - Gitcode

--- 1915-number-of-wonderful-substrings/1915. Number of Wonderful Substrings.py
+++ 1915-number-of-wonderful-substrings/1915. Number of Wonderful Substrings.py
@@ -1,19 +1,23 @@
 class Solution:
     def wonderfulSubstrings(self, word: str) -> int:
         freq = {}
-        freq[0] = 1
-        mask, res = 0, 0
-        lst = [ord(letter) - ord("a") for letter in word]
+        freq[0] = 1 #00000 
+
+        mask = 0
+        res = 0
+
+        lst = [ord(letter) - ord('a') for letter in word] #aba -> [0,1,0]
 
         for num in lst:
-            mask ^= (1 << num) #a -> 0001, b -> 0010
+            mask ^= (1 << num) #0010 
             if mask in freq:
-                res += freq[mask] #0010, 0010 
+                res += freq[mask]
                 freq[mask] += 1
             else:
                 freq[mask] = 1
             
-            for i in range(10): #a -> j
+            for i in range(10):
                 if mask ^ (1 << i) in freq:
-                    res += freq[mask ^ (1 << i)]
+                    res += freq[mask ^ (1 << i)] 
         return res
+
