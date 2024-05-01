fd577afb3de658af469f087039e0f64a3444908a
Geoffrey Lee <74619195+LGeoff31@users.noreply.github.com>
2024-04-30 21:18:44 -0400

    Runtime: 416 ms (97.40%), Memory: 17.5 MB (32.43%) - Gitcode

---
+++ 322-coin-change/322. Coin Change.py
@@ -0,0 +1,39 @@
+from collections import deque
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        if amount in coins:
+            return 1
+        if amount == 0:
+            return 0
+
+        stage = 1
+        queue = deque(coins) #[1,2,5]
+        visited = set()
+
+        def next_loc(num):
+            lst = []
+            for coin in coins:
+                lst.append(num+coin) #[2,3,6]
+            return lst
+
+        while queue:
+            for i in range(len(queue)):
+                current_num = queue.popleft()
+                for next_num in next_loc(current_num): #[2,3,6]
+                    if next_num <= amount:
+                        if next_num not in visited:
+                            if next_num == amount:
+                                return stage + 1
+                            queue.append(next_num)
+                            visited.add(next_num)
+                        
+            stage += 1
+            if amount in queue:
+                return stage
+                
+
+        return -1
+
+
+
+        
