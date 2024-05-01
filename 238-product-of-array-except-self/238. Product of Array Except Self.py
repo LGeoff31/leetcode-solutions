8c8c16aa0ff807b43bcb17c05a5bbd22a4fb26dc
Geoffrey Lee <74619195+LGeoff31@users.noreply.github.com>
2024-04-30 21:54:24 -0400

    Runtime: 270 ms (26.54%), Memory: 26.3 MB (11.66%) - Gitcode

--- 238-product-of-array-except-self/238. Product of Array Except Self.py
+++ 238-product-of-array-except-self/238. Product of Array Except Self.py
@@ -1,24 +1,21 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        prefix = []
-        suffix = []
-        acc = 1
-        for num in nums:
-            acc *= num
-            prefix.append(acc)
-        acc = 1
-        for num in nums[::-1]:
-            acc *= num
-            suffix.append(acc)
-        suffix = suffix[::-1]
-        print(prefix)
-        print(suffix)
-        lst = []
-        for i in range(len(nums)):
-            if i == 0: lst.append(suffix[i+1])
-            elif i == len(nums) - 1: lst.append(prefix[len(nums)-2])
-            else:
-                lst.append(prefix[i-1] * suffix[i+1])
-        return lst
+        prefix = [1] * len(nums)
+        suffix = [1] * len(nums)
+        acc1 = 1
+        acc2 = 1
+        for i,num in enumerate(nums):
+            acc1 *= num
+            prefix[i] = acc1
+        for i in range(len(nums) -1, -1, -1):
+            acc2 *= nums[i]
+            suffix[i] = acc2
+
+        res = [0] * len(nums)
+        res[0] = suffix[1]
+        res[-1] = prefix[-2]
+        for i in range(1, len(res) - 1):
+            res[i] = prefix[i-1] * suffix[i+1]
+        return res
 
         
