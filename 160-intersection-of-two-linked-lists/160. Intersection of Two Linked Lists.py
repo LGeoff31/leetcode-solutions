cffe857175d6a85a6499bfdeee3c9769bc5351ae
Geoffrey Lee <74619195+LGeoff31@users.noreply.github.com>
2024-05-08 22:44:26 -0400

    Runtime: 1445 ms (5.02%), Memory: 27.1 MB (73.56%) - Gitcode

---
+++ 160-intersection-of-two-linked-lists/160. Intersection of Two Linked Lists.py
@@ -0,0 +1,26 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
+        currA = headA
+        currB = headB
+
+        lst1 = []
+        lst2 = []
+
+        while currA:
+            lst1.append(currA)
+            currA = currA.next
+        
+        while currB:
+            lst2.append(currB)
+            currB = currB.next
+        
+        for elem in lst1:
+            if elem in lst2:
+                return elem
+        return None
