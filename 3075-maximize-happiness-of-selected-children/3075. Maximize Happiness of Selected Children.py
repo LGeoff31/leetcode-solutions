2ff64e23c5a1e3af7c3eaa3b35de8cac9830ab12
Geoffrey Lee <74619195+LGeoff31@users.noreply.github.com>
2024-05-08 21:28:34 -0400

    Runtime: 848 ms (19.41%), Memory: 43.7 MB (8.28%) - Gitcode

---
+++ 3075-maximize-happiness-of-selected-children/3075. Maximize Happiness of Selected Children.py
@@ -0,0 +1,11 @@
+class Solution:
+    def maximumHappinessSum(self, happiness: List[int], k: int) -> int:
+        happiness.sort(reverse=True)
+        res = 0
+        for i in range(k):
+            if happiness[i]-i > 0:
+                res += happiness[i] - i
+        return res
+            
+        
+        
