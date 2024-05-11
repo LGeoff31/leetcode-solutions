5e92769ee91aa148343b60a3c75a8c60fed7e89c
Geoffrey Lee <74619195+LGeoff31@users.noreply.github.com>
2024-05-11 00:41:16 -0400

    Runtime: 152 ms (71.81%), Memory: 19 MB (52.82%) - Gitcode

---
+++ 857-minimum-cost-to-hire-k-workers/857. Minimum Cost to Hire K Workers.py
@@ -0,0 +1,18 @@
+class Solution:
+    def mincostToHireWorkers(self, quality: List[int], wage: List[int], k: int) -> float:
+        ratio = sorted([(w/q, q) for w,q in zip(wage, quality)])
+        max_heap = []
+        quality_sum, max_ratio = 0, 0.0
+
+        for i in range(k):
+            max_ratio = max(max_ratio, ratio[i][0])
+            quality_sum += ratio[i][1]
+            heapq.heappush(max_heap, -ratio[i][1])
+        
+        res = max_ratio * quality_sum
+        for i in range(k, len(quality)):
+            max_ratio = max(max_ratio, ratio[i][0])
+            quality_sum = quality_sum + heapq.heappop(max_heap) + ratio[i][1]
+            heapq.heappush(max_heap, -ratio[i][1])
+            res = min(res, max_ratio * quality_sum)
+        return res
