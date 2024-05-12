6c51fae7f74ba29e8c55bf1761a1f04ac865c41d
Geoffrey Lee <74619195+LGeoff31@users.noreply.github.com>
2024-05-12 16:05:16 -0400

    Runtime: 34 ms (88.53%), Memory: 16.8 MB (7.59%) - Gitcode

--- 88-merge-sorted-array/88. Merge Sorted Array.py
+++ 88-merge-sorted-array/88. Merge Sorted Array.py
@@ -1,8 +1,29 @@
+import copy
 class Solution:
     def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        for i in range(m, len(nums1)):
-            nums1[i] = nums2[i-m]
-        nums1.sort()
+        m -= 1
+        n -= 1
+        curr_idx = m + n + 1
+
+        while m >= 0 and n >= 0:
+            if nums1[m] > nums2[n]:
+                nums1[curr_idx] = nums1[m]
+                m -= 1
+            else:
+                nums1[curr_idx] = nums2[n]
+                n -= 1
+            curr_idx -= 1
+        if m == -1:
+            while n >= 0:
+                nums1[curr_idx] = nums2[n]
+                n -= 1
+                curr_idx -= 1
+        else:
+            while m >= 0:
+                nums1[curr_idx] = nums1[m]
+                m -= 1
+                curr_idx -= 1
+        
