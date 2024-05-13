54d329d032352149769b2a0be5b27a95df2a8f21
Geoffrey Lee <74619195+LGeoff31@users.noreply.github.com>
2024-05-13 19:51:35 -0400

    Runtime: 2001 ms (15.84%), Memory: 17.6 MB (99.49%) - Gitcode

--- 55-jump-game/55. Jump Game.py
+++ 55-jump-game/55. Jump Game.py
@@ -1,10 +1,11 @@
-from collections import deque
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
-        goal = len(nums) - 1
-        for i in range(len(nums)-1,-1,-1):
-            if i + nums[i] >= goal:
-                goal = i
-        return True if goal == 0 else False
-
+        dp = [False] * len(nums)
+        dp[-1] = True
+        for i in range(len(dp)-2, -1, -1):
+            for j in range(i, i+nums[i] + 1):
+                if dp[j]:
+                    dp[i] = True
+                    break   
+        return dp[0]
             
