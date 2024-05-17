b4987ae23903bd056d5c61fa7e35417eb077d441
Geoffrey Lee <74619195+LGeoff31@users.noreply.github.com>
2024-05-16 20:19:45 -0400

    Runtime: 5693 ms (13.87%), Memory: 69.9 MB (7.69%) - Gitcode

--- 2812-find-the-safest-path-in-a-grid/2812. Find the Safest Path in a Grid.py
+++ 2812-find-the-safest-path-in-a-grid/2812. Find the Safest Path in a Grid.py
@@ -1,53 +1,45 @@
 class Solution:
     def maximumSafenessFactor(self, grid: List[List[int]]) -> int:
         n = len(grid)
-        inf = math.inf
 
-        # step 1: calculate distance of each node to closest thieves by 2 pass DP
-        dp = [[inf] * n for _ in range(n)]
-        for i in range(n):
-            for j in range(n):
-                if grid[i][j] == 1:
-                    dp[i][j] = 0
-                else:
-                    dp[i][j] = 1 + min(dp[i-1][j] if i - 1 >= 0 else inf, 
-                                    dp[i][j-1] if j - 1 >= 0 else inf)
-        for i in range(n-1, -1, -1):
-            for j in range(n-1, -1, -1):
-                if grid[i][j] == 0:
-                    dp[i][j] = min(dp[i][j],
-                        (1 + dp[i+1][j]) if i + 1 < n else inf, 
-                        (1 + dp[i][j+1]) if j + 1 < n else inf)
+        def in_bounds(r,c):
+            return min(r,c) >= 0 and max(r,c) < n
 
-        # step 2: define reachable with thresholding the safety
-        def reachable(minSafety):
-            visited = set()
-            visited.add((0, 0))
+        def precompute():
+            q = deque()
+            min_dist = {}
+            for r in range(n):
+                for c in range(n):
+                    if grid[r][c] == 1:
+                        q.append((r,c, 0))
+                        min_dist[(r,c)] = 0
+            while q:
+                r,c,dist = q.popleft()
+                nei = [[r+1, c], [r-1, c], [r, c+1], [r, c-1]]
 
-            def dfs(r, c):
-                if r == n - 1 and c == n - 1:
-                    return True
-                DIR = [0, 1, 0, -1, 0]
-                for dr, dc in zip(DIR[:4], DIR[1:]):
-                    nr, nc = r+dr, c+dc
-                    if (0 <= nr < n and 0 <= nc < n
-                        and (nr, nc) not in visited
-                        and dp[nr][nc] >= minSafety):
-                        visited.add((nr, nc))
-                        if dfs(nr, nc):
-                            return True
-                return False
-            return dfs(0, 0)
-                    
-        # step 3: binary search
-        lo, hi = 0, min(dp[0][0], dp[n-1][n-1])
+                for r2, c2 in nei:
+                    if in_bounds(r2,c2) and (r2, c2) not in min_dist:
+                        min_dist[(r2,c2)] = dist+1
+                        q.append((r2,c2, dist+1))
+            return min_dist
+        min_dist = precompute()
+        # print(min_dist)
+        maxHeap = [(-min_dist[(0,0)], 0, 0)] #minHeap, want store negative numbers make maxHeap
+        visited = set((0,0))
+        while maxHeap:
+            dist, r, c = heapq.heappop(maxHeap)
+            dist = -dist
+            if (r,c) == (n-1, n-1):
+                return dist
+            nei = [[r+1, c], [r-1, c], [r, c+1], [r, c-1]]
+            for r2, c2 in nei:
+                if in_bounds(r2,c2) and (r2, c2) not in visited:
+                    visited.add((r2,c2))
+                    # print(r2, c2)
+                    dist2 = -min(dist, min_dist[(r2,c2)])
+                    heapq.heappush(maxHeap, (dist2, r2, c2))
+        
+          
 
-        while lo < hi:
-            mid = (lo + hi) // 2
-            if not reachable(mid):
-                hi = mid
-            else:
-                lo = mid + 1
-        if reachable(lo):
-            return lo
-        return lo-1
+
+            
