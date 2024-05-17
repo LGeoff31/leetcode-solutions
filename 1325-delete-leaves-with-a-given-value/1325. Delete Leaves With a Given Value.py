c9d332b6c84a7bc56151554111e997d684b7bd61
Geoffrey Lee <74619195+LGeoff31@users.noreply.github.com>
2024-05-16 20:32:44 -0400

    Runtime: 46 ms (53.59%), Memory: 17.2 MB (28.92%) - Gitcode

--- 1325-delete-leaves-with-a-given-value/1325. Delete Leaves With a Given Value.py
+++ 1325-delete-leaves-with-a-given-value/1325. Delete Leaves With a Given Value.py
@@ -6,21 +6,12 @@
 #         self.right = right
 class Solution:
     def removeLeafNodes(self, root: Optional[TreeNode], target: int) -> Optional[TreeNode]:
-        def dfs(node):
-            if not node: return
-            if node.left:
-                if node.left.left is None and node.left.right is None and node.left.val == target:
-                    node.left = None
-            if node.right:
-                if node.right.left is None and node.right.right is None and node.right.val == target:
-                    node.right = None
-            
-            dfs(node.left)
-            dfs(node.right)
-        for i in range(3000):
-            dfs(root)
-        if root.left is None and root.right is None and root.val == target:
+        if not root:
+            return None
+        
+        root.left = self.removeLeafNodes(root.left, target)
+        root.right = self.removeLeafNodes(root.right, target)
+
+        if not root.left and not root.right and root.val == target:
             return None
         return root
-            
-        
