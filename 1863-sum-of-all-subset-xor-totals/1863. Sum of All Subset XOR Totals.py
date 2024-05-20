d0175b5d4716a15a87437f91e28d9641675d205c
Geoffrey Lee <74619195+LGeoff31@users.noreply.github.com>
2024-05-19 20:11:31 -0400

    Runtime: 78 ms (26.06%), Memory: 21.2 MB (7.08%) - Gitcode

--- 1863-sum-of-all-subset-xor-totals/1863. Sum of All Subset XOR Totals.py
+++ 1863-sum-of-all-subset-xor-totals/1863. Sum of All Subset XOR Totals.py
@@ -1,13 +1,13 @@
 class Solution:
     def subsetXORSum(self, nums: List[int]) -> int:
-        def XOR(arr):
+        def XOR(arr): #O(n)
             res = 0
             for n in arr:
                 res ^= n
             return res
         
         self.subsequences = []
-        def dfs(idx, arr):
+        def dfs(idx, arr): #O(2^n)
             if idx == len(nums):
                 self.subsequences.append(arr)
                 return
@@ -20,5 +20,6 @@ class Solution:
         res = 0
         for arr in self.subsequences:
             res += XOR(arr)
-        # print(self.subsequences)
+
+        #O(n * 2^n)
         return res
