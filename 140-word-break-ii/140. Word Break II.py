5b409e5f63e2919c4b4e6eb387a141f9b186dcd5
Geoffrey Lee <74619195+LGeoff31@users.noreply.github.com>
2024-05-25 16:24:18 -0400

    Runtime: 38 ms (35.21%), Memory: 16.5 MB (43.29%) - Gitcode

--- 140-word-break-ii/140. Word Break II.py
+++ 140-word-break-ii/140. Word Break II.py
@@ -2,19 +2,27 @@ class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> List[str]:
         words = set(wordDict)
 
+        cache = {}
         def dfs(idx):
             if idx == len(s):
-                res.append(" ".join(curr))
-
+                return [""]
+            if idx in cache:
+                return cache[idx]
+            res = []
             for i in range(idx, len(s)):
                 substring = s[idx:i+1]
-                if substring in words:
-                    curr.append(substring)
-                    dfs(i+1)
-                    curr.pop()
+                if substring not in words:
+                    continue
+                strings = dfs(i+1)
+                if not strings:
+                    continue
+                for substr in strings:
+                    sentence = substring
+                    if substr:
+                        sentence += " " + substr
+                    res.append(sentence)
+            cache[idx] = res
+            return res
 
-        curr = []
-        res = []
-        dfs(0)
-        return res
+        return dfs(0)
         
