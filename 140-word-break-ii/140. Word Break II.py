456c2f19794b801894d6169fec5f81f4c6c1c82e
Geoffrey Lee <74619195+LGeoff31@users.noreply.github.com>
2024-05-25 16:11:26 -0400

    Runtime: 34 ms (64.14%), Memory: 16.5 MB (87.03%) - Gitcode

--- 140-word-break-ii/140. Word Break II.py
+++ 140-word-break-ii/140. Word Break II.py
@@ -1,20 +1,20 @@
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> List[str]:
-        res = []
-        dic = {}
-        for word in wordDict:
-            dic[word] = len(word)
+        self.res = []
+        words = set(wordDict)
 
-        def dfs(word, string, length): 
-            if length == len(s):
-                res.append(string)
-            for i in range(1, 11):
-                if len(word) < i: break
-                if word[0:i] in dic:  #(i-1) is last index of word
-                    print(word[0:i], word[i:], length + len(word[0:i]), string + word[0:i])
-                    if len(word[i:]) == 0: dfs(word[i:], string + word[0:i], length + len(word[0:i]))
-                    else: dfs(word[i:], string + word[0:i] + ' ', length + len(word[0:i]))
+        def dfs(idx, currWords):
+            if idx == len(s):
+                self.res.append(currWords)
 
-        for i in range(len(s)):
-            dfs(s[i:], "", 0)
-        return res
+            for i in range(idx, len(s)):
+                substring = s[idx:i+1]
+                if substring in words:
+                    print(i+1, currWords + substring)
+                    if currWords == "":
+                        dfs(i+1, substring)
+                    else:
+                        dfs(i+1, currWords + " " + substring)
+        dfs(0, "")
+        return self.res
+        
