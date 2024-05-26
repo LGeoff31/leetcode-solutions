5b82a03af233878db907bc1b4d39cfd4c44bb9d5
Geoffrey Lee <74619195+LGeoff31@users.noreply.github.com>
2024-05-25 20:31:58 -0400

    Runtime: 1021 ms (52.79%), Memory: 41.9 MB (52.49%) - Gitcode

---
+++ 2381-shifting-letters-ii/2381. Shifting Letters II.py
@@ -0,0 +1,23 @@
+class Solution:
+    def shiftingLetters(self, s: str, shifts: List[List[int]]) -> str:
+        prefix = [0] * len(s)
+        for x,y,z in shifts:
+            if z == 0: z = -1
+            prefix[y] += z
+            if x != 0:
+                prefix[x-1] -= z
+
+        for i in range(len(prefix) - 2, -1, -1):
+            prefix[i] += prefix[i+1]
+        print(prefix)
+        lst = [ord(letter) - ord("a") for letter in s]
+        for i in range(len(prefix)):
+            lst[i] = (lst[i] + prefix[i]) % 26  
+
+        res = ""
+        for i in lst:
+            res += chr(i + 97)
+        return res
+
+            
+        
