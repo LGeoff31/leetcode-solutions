0a1a6169dbd7229221ba7fad05cde56c52dec979
Geoffrey Lee <74619195+LGeoff31@users.noreply.github.com>
2024-06-05 20:40:14 -0400

    Runtime: 406 ms (14.41%), Memory: 38 MB (8.08%) - Gitcode

---
+++ 1296-divide-array-in-sets-of-k-consecutive-numbers/1296. Divide Array in Sets of K Consecutive Numbers.py
@@ -0,0 +1,35 @@
+class Solution:
+    def isPossibleDivide(self, hand: List[int], groupSize: int) -> bool:
+        if len(hand) % groupSize != 0:
+            return False
+        
+        hand.sort()
+
+        dic = Counter(hand)
+        idx = 0
+        #[1,2,2,3,3,4,6,7,8]
+        #[1,2,3,4,6,7,8], 3
+        #{2:1, 3:1, 4:1, 6:1, 7:1, 8:1}
+        unique = sorted(list(set(hand)))
+        print("unique", unique)
+        while idx < len(unique):
+            for i in range(groupSize):
+                if unique[idx] + i not in dic:
+                    return False
+                dic[unique[idx]+i] -= 1
+                if dic[unique[idx] + i] == 0:
+                    del dic[unique[idx] + i]
+                # print(dic)
+                # break
+            # print(dic)
+            while idx < len(unique) and unique[idx] not in dic:
+                idx+=1
+            # if unique[idx] not in dic:
+                # idx += 1
+            # print(idx)
+            # break
+            
+
+        return True
+        
+        
