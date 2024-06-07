dbbda224f457c0462bcb22f8243083109b784c6e
Geoffrey Lee <74619195+LGeoff31@users.noreply.github.com>
2024-06-07 09:17:40 -0400

    Runtime: 242 ms (22.01%), Memory: 24.9 MB (81.26%) - Gitcode

--- 648-replace-words/648. Replace Words.py
+++ 648-replace-words/648. Replace Words.py
@@ -1,42 +1,16 @@
-class TrieNode:
-    def __init__(self):
-        self.children = {}
-        self.endWord = False
-
-class Trie:
-    def __init__(self):
-        self.root = TrieNode()
-
-    
-    def add(self, word):
-        cur = self.root
-        for letter in word:
-            if letter not in cur.children:
-                cur.children[letter] = TrieNode()
-            cur = cur.children[letter]
-        cur.endWord = True
-
-
-    def prefix(self, word):
-        cur = self.root
-        for i, letter in enumerate(word):
-            if cur.endWord:
-                return word[:i]
-            elif letter not in cur.children:
-                return word
-            else:
-                cur = cur.children[letter]
-        return word
-
-
 class Solution:
     def replaceWords(self, dictionary: List[str], sentence: str) -> str:
-        trie = Trie()
-        for word in dictionary:
-            trie.add(word)
+        dictionary = sorted(dictionary, key=len)
+        lst = sentence.split()
         res = []
-        for word in sentence.split():
-            res.append(trie.prefix(word))
+        for word in lst:
+            found = False
+            for w in dictionary:
+                if len(w) <= len(word) and w == word[:len(w)]:
+                    res.append(w)
+                    found = True
+                    break
+            if not found:
+                res.append(word)
         return " ".join(res)
 
-        
