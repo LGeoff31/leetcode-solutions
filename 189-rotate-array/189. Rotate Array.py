ce81c4cf202a92658db20ed398f884fae91dd340
Geoffrey Lee <74619195+LGeoff31@users.noreply.github.com>
2024-06-08 17:18:05 -0400

    Runtime: 163 ms (41.93%), Memory: 28 MB (60.82%) - Gitcode

--- 189-rotate-array/189. Rotate Array.py
+++ 189-rotate-array/189. Rotate Array.py
@@ -3,15 +3,28 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
+        #[1,2,3,4,5,6,7,8], k=26 -> 2
+        #[8,7,6,5,4,3,2,1]
+        #[6,7,8,1,2,3,4,5]
         k %= len(nums)
         def reverse(start, end):
             while start <= end:
                 nums[start], nums[end] = nums[end], nums[start]
-                start += 1
-                end -= 1
-        reverse(0, len(nums) - 1)
+                start+=1
+                end-=1
+        reverse(0, len(nums) - 1) #reverse entire array
         reverse(0, k-1)
         reverse(k, len(nums) - 1)
+
+
+        
+
+
+
+
+
+
+
         
 
 
