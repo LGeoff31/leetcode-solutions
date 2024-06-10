ddb3e89dba1a5b8c6f5232e4289bb40bea93273d
Geoffrey Lee <74619195+LGeoff31@users.noreply.github.com>
2024-06-10 00:24:13 -0400

    Runtime: 120 ms (77.46%), Memory: 19.1 MB (95.00%) - Gitcode

--- 135-candy/135. Candy.py
+++ 135-candy/135. Candy.py
@@ -1,12 +1,18 @@
 class Solution:
     def candy(self, ratings: List[int]) -> int:
         candies = [1] * len(ratings)
+
+        #forward
         for i in range(len(ratings) - 1):
             if ratings[i+1] > ratings[i]:
                 candies[i+1] = 1 + candies[i]
         
-        for i in range(len(ratings)-1, 0, -1):
+        #backward
+        for i in range(len(ratings) - 1, 0, -1):
             if ratings[i-1] > ratings[i] and candies[i-1] <= candies[i]:
                 candies[i-1] = 1 + candies[i]
+        
         return sum(candies)
+
+        
         
