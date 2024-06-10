eea453a0e28dceb1831964e80a55f59675e6bd78
Geoffrey Lee <74619195+LGeoff31@users.noreply.github.com>
2024-06-10 18:45:57 -0400

    Runtime: 31 ms (90.19%), Memory: 16.6 MB (24.96%) - Gitcode

--- 3178-find-the-child-who-has-the-ball-after-k-seconds/3178. Find the Child Who Has the Ball After K Seconds.py
+++ 3178-find-the-child-who-has-the-ball-after-k-seconds/3178. Find the Child Who Has the Ball After K Seconds.py
@@ -3,8 +3,7 @@ class Solution:
         cycle = 2 * (n-1)
         lst = list(range(n))
         k %= cycle
-        #only forward
         if k <= (n-1):
-            return lst[k]
+            return k
         else:
-            return lst[2*(n-1) - k]
+            return 2*(n-1) - k
