a3dc694d8bb4b77fe5dfccbcc8e73da54dd53f27
Geoffrey Lee <74619195+LGeoff31@users.noreply.github.com>
2024-06-10 00:26:48 -0400

    Runtime: 107 ms (98.90%), Memory: 19 MB (95.00%) - Gitcode

--- 135-candy/135. Candy.py
+++ 135-candy/135. Candy.py
@@ -11,8 +11,4 @@ class Solution:
         for i in range(len(ratings) - 1, 0, -1):
             if ratings[i-1] > ratings[i] and candies[i-1] <= candies[i]:
                 candies[i-1] = 1 + candies[i]
-        
         return sum(candies)
-
-        
-        
