cdde3217bb657ad3dc04d7513cad6ca08570070f
Geoffrey Lee <74619195+LGeoff31@users.noreply.github.com>
2024-06-13 19:50:40 -0400

    Runtime: 63 ms (50.26%), Memory: 16.4 MB (100.00%) - Gitcode

---
+++ 2507-smallest-value-after-replacing-with-sum-of-prime-factors/2507. Smallest Value After Replacing With Sum of Prime Factors.py
@@ -0,0 +1,26 @@
+class Solution:
+    def smallestValue(self, n: int) -> int:
+        def check_prime(n):
+            for i in range(2, n):
+                if n%i==0:
+                    return False
+            return True
+        
+        def find_prime(n):
+            primes = []
+            divide = 2
+            while n != 1:
+                if n%divide==0:
+                    n//=divide
+                    primes.append(divide)
+                else:
+                    divide += 1
+            return primes
+                
+        visited = set()
+        while not check_prime(n) and n not in visited:
+            visited.add(n)
+            n = sum(find_prime(n))
+        return n
+
+        
