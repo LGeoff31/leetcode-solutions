0e153f2f043a0c1422657e15f0462e7f11c8c629
Geoffrey Lee <74619195+LGeoff31@users.noreply.github.com>
2024-06-24 00:46:17 -0400

    Runtime: 792 ms (64.79%), Memory: 19.5 MB (76.06%) - Gitcode

--- 995-minimum-number-of-k-consecutive-bit-flips/995. Minimum Number of K Consecutive Bit Flips.py
+++ 995-minimum-number-of-k-consecutive-bit-flips/995. Minimum Number of K Consecutive Bit Flips.py
@@ -1,16 +1,15 @@
 class Solution:
     def minKBitFlips(self, nums: List[int], k: int) -> int:
         res = 0
-        flipped = [False] * len(nums)
         flips = 0
 
         for i in range(len(nums)):
             if i >= k: # First time shifting window
-                flips -= flipped[i-k] == True
+                flips -= nums[i-k] == 2
             if flips % 2 == nums[i]: # We must toggle
                 if i+k-1 >= len(nums):
                     return -1
-                flipped[i] = True
+                nums[i] = 2
                 flips += 1
                 res += 1
         return res
