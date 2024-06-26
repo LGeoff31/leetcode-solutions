a16d5fc2710e0761872bda224eb54b49219f455d
Geoffrey Lee <74619195+LGeoff31@users.noreply.github.com>
2024-06-25 20:55:18 -0400

    Runtime: 188 ms (87.22%), Memory: 21.9 MB (60.27%) - Gitcode

---
+++ 1382-balance-a-binary-search-tree/1382. Balance a Binary Search Tree.py
@@ -0,0 +1,29 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def balanceBST(self, root: TreeNode) -> TreeNode:
+        lst = []
+        
+        def traverse(node):
+            if node:
+                lst.append(node.val)
+                traverse(node.left)
+                traverse(node.right)
+        traverse(root)
+        lst.sort()
+        #[1,2,3,4]
+        def bst(l, r):
+            if l > r:
+                return
+            mid = (l + r) // 2
+            node = TreeNode(lst[mid])
+            node.left = bst(l, mid-1) #0, 1
+            node.right = bst(mid+1, r)
+            return node
+
+        return bst(0, len(lst) - 1)
+        
