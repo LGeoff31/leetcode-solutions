91dde0eba9f7e1ea5a69eba13dadffa01e866a6d
Geoffrey Lee <74619195+LGeoff31@users.noreply.github.com>
2024-06-26 23:47:45 -0400

    Runtime: 2470 ms (5.17%), Memory: 43.7 MB (27.03%) - Gitcode

--- 307-range-sum-query-mutable/307. Range Sum Query - Mutable.py
+++ 307-range-sum-query-mutable/307. Range Sum Query - Mutable.py
@@ -1,64 +1,50 @@
-class Node():
-    def __init__(self, start, end):
-        self.start = start
-        self.end = end
-        self.left = None
-        self.right = None
-        self.val = 0
+class SegmentTree:
+    def __init__(self, n):
+        self.n = n
+        self.total = defaultdict(int)
+    
+    def update(self, o, l, r, idx ,val):
+        if l == r:
+            self.total[o] = val
+            return
+        mid = (l+r) // 2
+        if idx <= mid:
+            self.update(2*o, l, mid, idx, val)
+        else:
+            self.update(2*o+1, mid+1, r, idx, val)
+        self.total[o] = self.total[2*o] + self.total[2*o + 1] # I want self.total[0] to store the sum of that node
+        return
+    
+    def query(self, o, l, r, left, right): 
+        # Out of query range
+        if r < left or l > right:
+            return 0
+        
+        # Completely inside query range
+        if l >= left and r <= right:
+            return self.total[o]
+
+        #Partial inside
+        mid = (l+r) // 2
+        res = 0
+        if left <= mid:
+            res += self.query(2*o, l, mid, left, right)
+        if mid+1 <= right:
+            res += self.query(2*o+1, mid+1, r, left, right)
+        return res
 
 class NumArray:
     def __init__(self, nums: List[int]):
-        def createTree(nums, l, r):
-            if l > r:
-                return None
-            if l == r:
-                node = Node(l, r)
-                node.val = nums[l]
-                return node
-            
-            mid = (l+r)//2
-            root = Node(l, r)
-
-            root.left = createTree(nums, l, mid)
-            root.right = createTree(nums, mid+1, r)
-            root.val = root.left.val + root.right.val
-            return root
-
-        self.root = createTree(nums, 0, len(nums) - 1)
+        self.n = len(nums)
+        self.tree = SegmentTree(self.n)
+        self.nums = nums
+        for i in range(len(nums)):
+            self.tree.update(1, 1, self.n, i+1, nums[i])
 
     def update(self, index: int, val: int) -> None:
-        def updateVal(node, i, val):
-            if node.start == node.end:
-                node.val = val
-                return val
-            mid = (node.start + node.end) // 2
-            if i > mid:
-                updateVal(node.right, i, val)
-            else:
-                updateVal(node.left, i, val)
-            node.val = node.left.val + node.right.val
-            return node.val
-        return updateVal(self.root, index, val)
+        self.nums[index] = val
+        self.tree.update(1, 1, self.n, index+1, val)
 
     def sumRange(self, left: int, right: int) -> int:
-        def rangeSum(node, l, r):
-            if node.start == l and node.end == r:
-                return node.val
-            mid = (node.start + node.end) // 2
-            #check if the query range is strictly on the left or right side
-            if r <= mid: #strictly on the left
-                return rangeSum(node.left, l, r)
-            elif l > mid: #strictly on the right
-                return rangeSum(node.right,l, r)
-            else:
-                return rangeSum(node.left, l, mid) + rangeSum(node.right, mid+1, r)
-        return rangeSum(self.root, left, right)
-                
-
-       
+        return self.tree.query(1, 1, self.n, left+1, right+1)
         
-
-# Your NumArray object will be instantiated and called as such:
-# obj = NumArray(nums)
-# obj.update(index,val)
-# param_2 = obj.sumRange(left,right)
