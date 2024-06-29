85321880323c00d37f443030ab12105ed2a56656
Geoffrey Lee <74619195+LGeoff31@users.noreply.github.com>
2024-06-29 16:34:48 -0400

    Runtime: 365 ms (9.45%), Memory: 22 MB (14.93%) - Gitcode

---
+++ 2579-count-total-number-of-colored-cells/2579. Count Total Number of Colored Cells.py
@@ -0,0 +1,15 @@
+class Solution:
+    def coloredCells(self, n: int) -> int:
+        dp = [0] * n
+        dp[0] = 1
+        if n == 1: return 1
+        dp[1] = 5
+        width = 3
+        for i in range(2, n):
+            dp[i] = dp[i-1] + 2*width + 2*(width-2)
+            width += 1
+        # print(dp)
+        return dp[-1]
+
+
+        
