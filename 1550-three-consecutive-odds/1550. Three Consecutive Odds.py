f4b57c1911fa87394200c9ce864c4f0354275cac
Geoffrey Lee <74619195+LGeoff31@users.noreply.github.com>
2024-06-30 20:40:10 -0400

    Runtime: 53 ms (18.20%), Memory: 16.6 MB (67.76%) - Gitcode

--- 1550-three-consecutive-odds/1550. Three Consecutive Odds.py
+++ 1550-three-consecutive-odds/1550. Three Consecutive Odds.py
@@ -1,7 +1,4 @@
 class Solution:
     def threeConsecutiveOdds(self, arr: List[int]) -> bool:
-        for i in range(len(arr) - 2):
-            if arr[i] % 2 and arr[i+1] % 2 and arr[i+2] % 2:
-                return True
-        return False
+        return sum([arr[i] % 2 and arr[i+1] % 2 and arr[i+2] % 2 for i in range(len(arr) - 2)]) != 0
         
