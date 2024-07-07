27465e71b49436fa412f46353c6e3f50701bf597
Geoffrey Lee <74619195+LGeoff31@users.noreply.github.com>
2024-07-07 01:30:42 -0400

    Runtime: 11070 ms (100.00%), Memory: 102 MB (100.00%) - Gitcode

---
+++ 3213-construct-string-with-minimum-cost/3213. Construct String with Minimum Cost.py
@@ -0,0 +1,35 @@
+class Node:
+    def __init__(self):
+        self.children = {}
+        self.endWord = False
+        self.cost = 1e9
+
+class Solution:
+    def minimumCost(self, target: str, words: List[str], costs: List[int]) -> int:
+        trie = Node()
+        # Create the Trie
+        for i in range(len(words)):
+            node = trie
+            for c in words[i]:
+                if c not in node.children:
+                    node.children[c] = Node()
+                node = node.children[c]
+            node.cost = min(node.cost, costs[i])
+            node.endWord = True
+        
+        @cache
+        def dfs(i):
+            if i >= len(target):
+                return 0
+            res = 1e9
+            node = trie
+            for idx in range(i, len(target)):
+                c = target[idx]
+                if c not in node.children:
+                    break
+                node = node.children[c]
+                if node.endWord:
+                    res = min(res, node.cost + dfs(idx+1))
+            return res
+        res = dfs(0)
+        return res if res != 1e9 else -1
