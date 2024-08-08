cb81efe63aae44cd0a11e029b22637e626033061
Geoffrey Lee <74619195+LGeoff31@users.noreply.github.com>
2024-08-07 20:49:47 -0400

    Runtime: 133 ms (96.33%), Memory: 17.8 MB (11.92%) - Gitcode

--- 3016-minimum-number-of-pushes-to-type-word-ii/3016. Minimum Number of Pushes to Type Word II.py
+++ 3016-minimum-number-of-pushes-to-type-word-ii/3016. Minimum Number of Pushes to Type Word II.py
@@ -1,17 +1,43 @@
+from collections import Counter
 class Solution:
     def minimumPushes(self, word: str) -> int:
         dic = Counter(word)
-        # 26 alphabet, 2 -> 9: 8 numbers
-        lst = []
-        for key in dic:
-            lst.append((dic[key], key))
-        lst.sort(reverse=True)
-        print(lst, len(lst))
-        if len(lst) <= 8:
-            return sum(a for a,b in lst)
-        if len(lst) <= 16:
-            return sum(a for a,b in lst[:8]) + 2*sum(a for a,b in lst[8:])
-        if len(lst) <= 24:
-            return sum(a for a,b in lst[:8]) + 2*sum(a for a,b in lst[8:16]) + 3*sum(a for a,b in lst[16:])
+        dic = dict(sorted(dic.items(), key = lambda x: x[1], reverse=True))
+        print(dic)
+        total = 0
+        if len(dic) <= 8:
+            for key in dic:
+                total += dic[key]
+            return total
+        elif len(dic) <= 16: #15
+            count = 1
+            z = 1
+            for key in dic:
+                total += dic[key] * z
+                count += 1
+                if count == 9:
+                    z += 1
+        elif len(dic) <= 24:
+            count = 1
+            z = 1
+            for key in dic:
+                total += dic[key] * z
+                count += 1
+                if count == 9:
+                    z += 1
+                if count == 17:
+                    z += 1
         else:
-            return sum(a for a,b in lst[:8]) + 2*sum(a for a,b in lst[8:16]) + 3*sum(a for a,b in lst[16:24]) + 4*sum(a for a,b in lst[24:])
+            count = 1
+            z = 1
+            for key in dic:
+                total += dic[key] * z
+                count += 1
+                if count == 9:
+                    z += 1
+                if count == 17:
+                    z += 1
+                if count == 25:
+                    z += 1
+            
+        return total
