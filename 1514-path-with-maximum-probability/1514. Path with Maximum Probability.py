9e407d5fbc38b6b894901d0dd1418d7f044e101e
Geoffrey Lee <74619195+LGeoff31@users.noreply.github.com>
2024-08-27 00:56:09 -0400

    Runtime: 589 ms (21.11%), Memory: 28.1 MB (80.72%) - Gitcode

---
+++ 1514-path-with-maximum-probability/1514. Path with Maximum Probability.py
@@ -0,0 +1,27 @@
+class Solution:
+    def maxProbability(self, n: int, edges: List[List[int]], succProb: List[float], start_node: int, end_node: int) -> float:
+        # Idea: Start from start node, and perform Dikjstra's algorithm on each node until you get to end
+        # If all nodes are visited and end is not within it, return 0
+        # Each node should have (num, probability value)
+
+        adj = defaultdict(list)
+        for i in range(len(edges)):
+            u,v = edges[i]
+            adj[u].append((v, succProb[i]))
+            adj[v].append((u, succProb[i]))
+        
+        minHeap = [(-1, start_node)] #Weight : node
+        visited = {}
+
+        while minHeap:
+            w1, n1 = heapq.heappop(minHeap)
+            w1 *= -1
+            if n1 in visited:
+                continue
+            visited[n1] = w1
+            for n2, w2 in adj[n1]:
+                if n2 not in visited:
+                    heapq.heappush(minHeap, (-1*w1*w2, n2))
+        if end_node in visited:
+            return visited[end_node]   
+        return 0
