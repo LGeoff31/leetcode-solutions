0a7c68c6d6dc5992db45b462ee6257391ad16ccf
Geoffrey Lee <geoffrey31415@gmail.com>
2024-09-10 18:39:16 -0400

    Runtime: 579 ms (47.08%), Memory: 78.2 MB (50.32%) - Gitcode

--- 146-lru-cache/146. LRU Cache.py
+++ 146-lru-cache/146. LRU Cache.py
@@ -1,20 +1,52 @@
-from collections import OrderedDict
 
 class LRUCache:
-    __slots__ = ('data', 'capacity')
-
+    class Node:
+        def __init__(self, key, val):
+            self.key = key
+            self.val = val
+            self.prev = None
+            self.next = None
     def __init__(self, capacity: int):
-        self.data: Dict[int, int] = OrderedDict()
-        self.capacity: int = capacity
+        self.capacity = capacity
+        self.head = self.Node(-1, -1)
+        self.tail = self.Node(-1, -1)
+        self.head.next = self.tail
+        self.tail.prev = self.head
+        self.dictionary = {}
+    
+    def addNode(self, node):
+        nextNode = self.head.next
+        node.next = nextNode
+        node.prev = self.head
+        self.head.next = node
+        nextNode.prev = node
 
-    def get(self, key: int) -> int:
-        return -1 if key not in self.data else self.data.setdefault(key, self.data.pop(key))
+    def deleteNode(self, node):
+        prevNode = node.prev
+        nextNode = node.next
+        prevNode.next = nextNode
+        nextNode.prev = prevNode
 
-    def put(self, key: int, value: int) -> None:
-        try:
-            self.data.move_to_end(key)
-            self.data[key] = value
-        except KeyError:
-            self.data[key] = value
-            if len(self.data) > self.capacity:
-                self.data.popitem(last=False)
+    def get(self, key: int) -> int: 
+        if key in self.dictionary:
+            resNode = self.dictionary[key]
+            ans = resNode.val
+            del self.dictionary[key]
+            self.deleteNode(resNode)
+            self.addNode(resNode)
+            self.dictionary[key] = self.head.next
+            return ans
+
+        return -1
+        
+    def put(self, key: int, value: int) -> None: 
+        if key in self.dictionary:
+            curr = self.dictionary[key]
+            del self.dictionary[key]
+            self.deleteNode(curr)
+        if len(self.dictionary) == self.capacity:
+            del self.dictionary[self.tail.prev.key]
+            self.deleteNode(self.tail.prev)
+
+        self.addNode(self.Node(key, value))
+        self.dictionary[key] = self.head.next
