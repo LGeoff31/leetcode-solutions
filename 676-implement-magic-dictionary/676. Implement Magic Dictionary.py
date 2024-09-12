ff3692d5d78faf6c53cf38b4a460584162168f2e
Geoffrey Lee <geoffrey31415@gmail.com>
2024-09-12 17:11:44 -0400

    Runtime: 174 ms (37.64%), Memory: 19.6 MB (27.42%) - Gitcode

--- 676-implement-magic-dictionary/676. Implement Magic Dictionary.py
+++ 676-implement-magic-dictionary/676. Implement Magic Dictionary.py
@@ -1,25 +1,48 @@
+class TrieNode:
+    def __init__(self):
+        self.endWord = False 
+        self.children = {}
+
 class MagicDictionary:
     def __init__(self):
-        self.lst = []
+        self.root = TrieNode()
+    
+    def insertWord(self, word):
+        curr = self.root
+        for c in word:
+            if c not in curr.children:
+                curr.children[c] = TrieNode()
+            curr = curr.children[c]
+        curr.endWord = True
+
+    def buildDict(self, dictionary: List[str]) -> None: 
+        for word in dictionary:
+            self.insertWord(word)
+    
+    def search(self, searchWord: str) -> bool:
+        curr = self.root
+
+        def dfs(node, i, differences):
+            # If there's a mismatch, increment differences by 1 and auto go to correct one
+            if i == len(searchWord):
+                return differences == 1 and node.endWord
+            if differences > 1:
+                return False 
+
+            if searchWord[i] in node.children:
+                if dfs(node.children[searchWord[i]], i + 1, differences):
+                    return True
+
+            for child in node.children:
+                if child != searchWord[i]:
+                    if dfs(node.children[child], i+1, differences + 1):
+                        return True
+            return False
+
+        return dfs(curr, 0, 0)
         
 
-    def buildDict(self, dictionary: List[str]) -> None: #O(n), n is amount of words
-        self.lst = dictionary
-    
-    def isMatch(self, word1, word2): #O(m), where m is max(len(word1), len(word2))
-        differences = 0
-        if len(word1) != len(word2):
-            return False
-        for i in range(len(word1)):
-            if word1[i] != word2[i]:
-                differences += 1
-        return differences == 1
-
-    def search(self, searchWord: str) -> bool: #O(nm)
-        for word in self.lst:
-            if self.isMatch(searchWord, word):
-                return True 
-        return False
+        
         
 
 
