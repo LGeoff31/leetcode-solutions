a45449d7d09ad26d972933e4d208ca1d50ec44b3
Geoffrey Lee <geoffrey31415@gmail.com>
2024-09-12 17:42:32 -0400

    Runtime: 597 ms (24.86%), Memory: 78.2 MB (49.95%) - Gitcode

--- 146-lru-cache/146. LRU Cache.py
+++ 146-lru-cache/146. LRU Cache.py
@@ -1,11 +1,11 @@
-
 class LRUCache:
     class Node:
-        def __init__(self, key, val):
+        def __init__(self, key, value):
             self.key = key
-            self.val = val
-            self.prev = None
+            self.value = value
             self.next = None
+            self.prev = None
+
     def __init__(self, capacity: int):
         self.capacity = capacity
         self.head = self.Node(-1, -1)
@@ -14,39 +14,40 @@ class LRUCache:
         self.tail.prev = self.head
         self.dictionary = {}
     
-    def addNode(self, node):
+    def add(self, node):    
         nextNode = self.head.next
         node.next = nextNode
         node.prev = self.head
         self.head.next = node
         nextNode.prev = node
 
-    def deleteNode(self, node):
-        prevNode = node.prev
+    def delete(self, node):
         nextNode = node.next
+        prevNode = node.prev
         prevNode.next = nextNode
         nextNode.prev = prevNode
 
     def get(self, key: int) -> int: 
         if key in self.dictionary:
             resNode = self.dictionary[key]
-            ans = resNode.val
+            ans = resNode.value
             del self.dictionary[key]
-            self.deleteNode(resNode)
-            self.addNode(resNode)
+            self.delete(resNode)
+            self.add(resNode)
             self.dictionary[key] = self.head.next
             return ans
-
         return -1
         
     def put(self, key: int, value: int) -> None: 
         if key in self.dictionary:
-            curr = self.dictionary[key]
+            node = self.dictionary[key]
             del self.dictionary[key]
-            self.deleteNode(curr)
+            self.delete(node)
+
         if len(self.dictionary) == self.capacity:
             del self.dictionary[self.tail.prev.key]
-            self.deleteNode(self.tail.prev)
-
-        self.addNode(self.Node(key, value))
+            self.delete(self.tail.prev)
+        self.add(self.Node(key, value))
         self.dictionary[key] = self.head.next
+
+      
