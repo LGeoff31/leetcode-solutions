f90c06bd74347db42287499d695fd2d0acbc5095
Geoffrey Lee <geoffrey31415@gmail.com>
2024-09-17 19:31:49 -0400

    Runtime: 43 ms (21.65%), Memory: 17 MB (11.27%) - Gitcode

--- 22-generate-parentheses/22. Generate Parentheses.py
+++ 22-generate-parentheses/22. Generate Parentheses.py
@@ -1,30 +1,16 @@
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
-        self.res = []
+        res = []
 
-        def valid_brackets(string):
-            stack = []
-            for char in string:
-                if char == ")":
-                    if not stack:
-                        return False 
-                    stack.pop()
-                else:
-                    stack.append(char)
-            return not stack
-        def dfs(string):
-            if string.count(")") > string.count("("):
-                return 
-            if len(string) > 2*n:
-                return
-            if len(string) == 2*n:
-                if valid_brackets(string):
-                    print('string', string)
-                    self.res.append(string)
 
-            dfs(string + "(")
-            dfs(string + ")")
-
-        dfs("(")
-
-        return self.res
+        def dfs(o, c, string):
+            if o == c and o+c == 2*n:
+                res.append(string)
+            
+            if o < n:
+                dfs(o+1, c, string + "(")
+            
+            if c < o:
+                dfs(o, c+1, string + ")")
+        dfs(0, 0, "")
+        return res
