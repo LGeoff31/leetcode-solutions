71dd4698603e65410e7d1afd14b4e856442220fd
Geoffrey Lee <geoffrey31415@gmail.com>
2024-09-17 09:37:51 -0400

    Runtime: 248 ms (53.32%), Memory: 27 MB (15.54%) - Gitcode

--- 200-number-of-islands/200. Number of Islands.py
+++ 200-number-of-islands/200. Number of Islands.py
@@ -1,24 +1,20 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         visited = set()
-        res = 0
         rows, cols = len(grid), len(grid[0])
-        directions = [(-1, 0), (1,0), (0,1), (0,-1)]
-        for r in range(rows):
-            for c in range(cols):
-                grid[r][c] = int(grid[r][c])
+        count = 0
+        direction = [(0,1), (0,-1), (1,0), (-1, 0)]
         def dfs(r,c):
             visited.add((r,c))
-            for dr, dc in directions:
-                if 0 <= r + dr < rows and 0 <= c + dc < cols and (r+dr, c+dc) not in visited and grid[r+dr][c+dc] == 1:
-                    dfs(r+dr, c + dc)
+            if 0 <= r < rows and 0 <= c < cols:
+                for dr, dc in direction:
+                    if 0<=r+dr<rows and 0<=c+dc<cols and (r+dr, c+dc) not in visited and grid[r+dr][c+dc] == "1":
+                        dfs(r+dr, c+dc)
 
         for r in range(rows):
             for c in range(cols):
-                if grid[r][c] == 1 and (r,c) not in visited:
-                    res+=1
+                if grid[r][c] == "1" and (r,c) not in visited:
                     dfs(r,c)
-        
-        return res
-
-        
+                    count += 1
+                    # print(visited)
+        return count
