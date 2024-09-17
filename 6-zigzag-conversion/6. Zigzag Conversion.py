0c1cebc6439aeb7e38b3450571482aad77da4c54
Geoffrey Lee <geoffrey31415@gmail.com>
2024-09-17 15:04:46 -0400

    Runtime: 49 ms (68.94%), Memory: 16.8 MB (30.24%) - Gitcode

--- 6-zigzag-conversion/6. Zigzag Conversion.py
+++ 6-zigzag-conversion/6. Zigzag Conversion.py
@@ -1,61 +1,19 @@
-class Solution:
-    def convert(self, s: str, numRows: int) -> str:
-        res = [["a"] for _ in range(len(s))]
-        print('res', res)
-        down = True
-        currRow = 0
-        rowCounter = [0 for _ in range(numRows)]
-        rowCounter2 = [0 for _ in range(numRows)]
-
-        if numRows == 1:
+class Solution(object):
+    def convert(self, s, numRows):
+        if numRows == 1 or numRows >= len(s):
             return s
-        for i in range(len(s)):
-            rowCounter[currRow] += 1
-            if down:
-                currRow += 1
-                if currRow == numRows:
-                    currRow = numRows - 2
-                    down = False
-            else:
-                currRow -= 1
-                if currRow == -1:
-                    currRow = 1
-                    down = True
-        down = True
-        curr = 0
-        currRow = 0
-        for i in range(len(s)):
-            print(currRow, curr)
-            res[sum(rowCounter[:currRow]) + rowCounter2[currRow]] = s[i]
-            rowCounter2[currRow] += 1
-            if down:
-                currRow += 1
-                if currRow == numRows:
-                    currRow = numRows - 2
-                    down = False
-                    curr += 1
-            else:
-                currRow -= 1
-                if currRow == -1:
-                    currRow = 1
-                    down = True
-                    curr += 1
+        
+        rows = [[] for row in range(numRows)]
+        index = 0
+        step = -1
+        for char in s:
+            rows[index].append(char)
+            if index == 0:
+                step = 1
+            elif index == numRows - 1:
+                step = -1
+            index += step
 
-        print(rowCounter)
-        print(res)
-
-        return "".join(res)
-                
-
-        # res = ''
-        # col = 0
-        # while col < len(s):
-        #     for i in range(numRows):
-        #         res += s[col][i]
-        #     for i in range(numRows - 1):
-        #         col += 1
-        #         if col >= len(s):
-        #             return res
-        #         res += s[col][i]
-
-        # return res
+        for i in range(numRows):
+            rows[i] = ''.join(rows[i])
+        return ''.join(rows)
