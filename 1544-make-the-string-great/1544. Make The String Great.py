bd4c438effd2a5bb08998004ba4570d092718cb4
Geoffrey Lee <geoffrey31415@gmail.com>
2024-09-19 13:23:10 -0400

    Runtime: 48 ms (5.39%), Memory: 16.5 MB (70.00%) - Gitcode

--- 1544-make-the-string-great/1544. Make The String Great.py
+++ 1544-make-the-string-great/1544. Make The String Great.py
@@ -1,20 +1,22 @@
 class Solution:
     def makeGood(self, s: str) -> str:
-        print(ord("a"), ord("A"), ord("B"))
-        a,b = 97, 65
-        lst = []
-        for i in range(26):
-            lst.append([a,b])
-            lst.append([b,a])
-            a+=1
-            b+=1
-        dic = {chr(lower):chr(upper) for lower, upper in lst}
-        stack = [s[0]]
+        stack = []
 
-        for i in range(1, len(s)):
-            if stack and dic[s[i]] == stack[-1]:
-                stack.pop()
+        print(ord('a'), ord("A"), ord('a') - ord('A'))
+
+        for i in range(len(s)):
+            print(stack)
+            valid = True
+            if s[i].isupper():
+                if stack and stack[-1].upper() == s[i] and stack[-1] != s[i]:
+                    stack.pop()
+                    valid = False
             else:
-                stack.append(s[i])
+                if stack and stack[-1].lower() == s[i] and stack[-1] != s[i]:
+                    stack.pop()
+                    valid = False
+
+            if valid: stack.append(s[i])
         return "".join(stack)
-        
+
+      
