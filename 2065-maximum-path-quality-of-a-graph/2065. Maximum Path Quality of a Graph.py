8124e3543c6b14d03cdb512b71548bd3af025ebe
Geoffrey Lee <geoffrey31415@gmail.com>
2024-09-28 17:59:24 -0400

    Runtime: 2925 ms (11.04%), Memory: 18.7 MB (22.43%) - Gitcode

---
+++ 2065-maximum-path-quality-of-a-graph/2065. Maximum Path Quality of a Graph.py
@@ -0,0 +1,32 @@
+class Solution:
+    def maximalPathQuality(self, values: List[int], edges: List[List[int]], maxTime: int) -> int:
+        # Brute force: From node 0, dfs to neighboring nodes, and from those dfs
+        # Base case: If get back to node 0, return sum all nodes in visited set
+        # If the sum ever is above maxTime, return
+        # Return the maximum sum from all the path traversals
+
+        adj1 = {}
+        adj2 = defaultdict(list)
+
+        
+        for u,v,time in edges:
+            adj1[(u,v)] = time
+            adj1[(v,u)] = time
+
+            adj2[u].append(v)
+            adj2[v].append(u)
+        res = 0
+        def dfs(root, t, visited, currSum):
+            if t > maxTime:
+                return
+            if root == 0:
+                nonlocal res
+                res = max(res, currSum)
+            visited.add(root)
+            for nei in adj2[root]:
+                if nei not in visited:
+                    dfs(nei, t+adj1[(root, nei)], set(visited), currSum + values[nei])
+                else:
+                    dfs(nei, t+adj1[(root, nei)], set(visited), currSum)
+        dfs(0, 0, set(), values[0])
+        return res
