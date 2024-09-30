930b00e78b1b641a4d3ec628df235d849303753a
Geoffrey Lee <geoffrey31415@gmail.com>
2024-09-29 21:50:46 -0400

    Runtime: 65 ms (96.01%), Memory: 17.7 MB (18.01%) - Gitcode

--- 1381-design-a-stack-with-increment-operation/1381. Design a Stack With Increment Operation.py
+++ 1381-design-a-stack-with-increment-operation/1381. Design a Stack With Increment Operation.py
@@ -1,22 +1,22 @@
 class CustomStack:
-
     def __init__(self, maxSize: int):
         self.lst = []
         self.maxSize = maxSize
+        self.inc = []
         
 
     def push(self, x: int) -> None:
         if len(self.lst) < self.maxSize:
             self.lst.append(x)    
+            self.inc.append(0)
 
     def pop(self) -> int:
-        if self.lst:
-            return self.lst.pop()
-        return -1
+        if not self.lst: return -1
+        if len(self.inc) > 1:
+            self.inc[-2] += self.inc[-1]
+        return self.lst.pop() + self.inc.pop()
        
-        
     def increment(self, k: int, val: int) -> None:
-        for i in range(min(k, len(self.lst))):
-            self.lst[i] += val
+        if self.inc: self.inc[min(k-1, len(self.lst) - 1)] += val
         
       
