ebad990736e38250e1d2839e6ae34a897a91ccc2
Geoffrey Lee <geoffrey31415@gmail.com>
2024-10-01 15:43:36 -0400

    Runtime: 41 ms (69.05%), Memory: 16.5 MB (86.30%) - Gitcode

--- 2979-most-expensive-item-that-can-not-be-bought/2979. Most Expensive Item That Can Not Be Bought.py
+++ 2979-most-expensive-item-that-can-not-be-bought/2979. Most Expensive Item That Can Not Be Bought.py
@@ -1,15 +1,3 @@
 class Solution:
     def mostExpensiveItem(self, primeOne: int, primeTwo: int) -> int:
-        n = primeOne * primeTwo - 1
-        dp = [0] * n
-        dp[primeOne] = 1
-        dp[primeTwo] = 1
-        for i in range(2, n):
-            if dp[i] == 1: continue
-            if dp[i - primeOne] == 1 or dp[i - primeTwo] == 1:
-                dp[i] = 1
-        for i in range(len(dp) -1, -1, -1):
-            if dp[i] == 0:
-                return i
-        print(dp)
-        return primeOne * primeTwo - 1
+        return primeOne * primeTwo - primeOne - primeTwo
