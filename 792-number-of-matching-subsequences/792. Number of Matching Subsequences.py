f7a78a8c26076ad0586cd5560eede18c46a72d71
Geoffrey Lee <geoffrey31415@gmail.com>
2024-10-15 19:57:20 -0400

    Runtime: 336 ms (76.10%), Memory: 18.9 MB (38.68%) - Gitcode

--- 792-number-of-matching-subsequences/792. Number of Matching Subsequences.py
+++ 792-number-of-matching-subsequences/792. Number of Matching Subsequences.py
@@ -1,16 +1,16 @@
 class Solution:
     def numMatchingSubseq(self, s: str, words: List[str]) -> int:
-        a = Counter(words)
-        words = list(set(words))
-        def match(subsequence):
-            idx = 0
-            for i in range(len(s)):
-                if s[i] == subsequence[idx]:
-                    idx += 1
-                    if idx == len(subsequence): return True
-            return idx == len(subsequence)
-        res = 0
+        res = 0 
+        dic = defaultdict(list)
         for word in words:
-            if match(word):
-                res += a[word]
+            dic[word[0]].append(word[1:])
+        
+        for c in s:
+            tmp = dic[c]
+            dic[c] = [] # restart
+            for w in tmp:
+                if not w: 
+                    res += 1
+                else:
+                    dic[w[0]].append(w[1:])
         return res
