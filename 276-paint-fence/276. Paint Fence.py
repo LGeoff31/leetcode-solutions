ce7655dad93a2c2ecd40d4dcb387806f75f6a535
Geoffrey Lee <geoffrey31415@gmail.com>
2024-10-15 12:03:22 -0400

    Runtime: 33 ms (68.16%), Memory: 16.4 MB (79.33%) - Gitcode

--- 276-paint-fence/276. Paint Fence.py
+++ 276-paint-fence/276. Paint Fence.py
@@ -6,6 +6,9 @@ class Solution:
         dp = [0] * (n)
         dp[0]=k
         dp[1]=k*k
+        prevprev, prev = k, k*k
         for i in range(2, n):
-            dp[i] = (k-1) * dp[i-1] + (k-1) * dp[i-2]
+            dp[i] = (k-1) * prev + (k-1) * prevprev
+            prevprev = prev
+            prev = dp[i]
         return dp[-1]
