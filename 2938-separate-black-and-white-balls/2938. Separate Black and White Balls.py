9a8338303e411fa981a6ca8c9b3c7bf5826cfa2b
Geoffrey Lee <geoffrey31415@gmail.com>
2024-10-15 01:23:44 -0400

    Runtime: 104 ms (52.54%), Memory: 19.6 MB (5.97%) - Gitcode

--- 2938-separate-black-and-white-balls/2938. Separate Black and White Balls.py
+++ 2938-separate-black-and-white-balls/2938. Separate Black and White Balls.py
@@ -3,10 +3,10 @@ class Solution:
         ones = s.count("1")
         res = 0
         lst = []
-        for i in range(len(s) - ones, len(s)):
+        for i in range(len(s)-1, len(s) - ones - 1, -1):
             if s[i] == "0":
                 lst.append(i)
-        lst.sort(reverse=True)
+
         for i in range(len(s) - ones - 1, -1, -1):
             if s[i] == "1":
                 res += lst[-1] - i
