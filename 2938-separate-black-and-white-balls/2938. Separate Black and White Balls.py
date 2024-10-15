c7461a212c5762b0dab3d5712d54e4b1d41a6987
Geoffrey Lee <geoffrey31415@gmail.com>
2024-10-15 01:15:50 -0400

    Runtime: 111 ms (40.60%), Memory: 19.4 MB (7.16%) - Gitcode

--- 2938-separate-black-and-white-balls/2938. Separate Black and White Balls.py
+++ 2938-separate-black-and-white-balls/2938. Separate Black and White Balls.py
@@ -1,12 +1,16 @@
 class Solution:
     def minimumSteps(self, s: str) -> int:
+        ones = s.count("1")
         res = 0
-        zeroCount = 0
-        for i in range(len(s)-1, -1, -1):
+        lst = []
+        for i in range(len(s) - ones, len(s)):
+            if s[i] == "0":
+                lst.append(i)
+        lst.sort(reverse=True)
+        for i in range(len(s) - ones - 1, -1, -1):
             if s[i] == "1":
-                res+=zeroCount
-            else:
-                zeroCount+=1
+                res += lst[-1] - i
+                lst.pop()
         return res
             
         
