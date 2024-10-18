8e9938ba68e67bd7f139a67b3538605556f9c92d
Geoffrey Lee <geoffrey31415@gmail.com>
2024-10-17 20:09:24 -0400

    Runtime: 259 ms (66.80%), Memory: 16.7 MB (44.07%) - Gitcode

---
+++ 2044-count-number-of-maximum-bitwise-or-subsets/2044. Count Number of Maximum Bitwise-OR Subsets.py
@@ -0,0 +1,23 @@
+class Solution:
+    def countMaxOrSubsets(self, nums: List[int]) -> int:
+        # If find x that work, the gaps between adds 2^gap
+
+        res = 0
+        target = 0
+        target = 0
+
+        for num in nums:
+            target |= num
+        print(target)
+        def dfs(i, curr):
+            if i == len(nums):
+                return target == curr
+            res = 0
+            # Take num
+            res += dfs(i+1, curr | nums[i])
+
+            # Don't take
+            res += dfs(i+1, curr)
+
+            return res
+        return dfs(0, 0)
