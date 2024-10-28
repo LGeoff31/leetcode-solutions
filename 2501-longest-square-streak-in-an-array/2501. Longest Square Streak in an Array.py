70142c1fdee5f84e3c1121d59e1cdacce95302cd
Geoffrey Lee <geoffrey31415@gmail.com>
2024-10-27 20:10:58 -0400

    Runtime: 172 ms (16.22%), Memory: 49 MB (5.33%) - Gitcode

---
+++ 2501-longest-square-streak-in-an-array/2501. Longest Square Streak in an Array.py
@@ -0,0 +1,14 @@
+class Solution:
+    def longestSquareStreak(self, nums: List[int]) -> int:
+        nums.sort()
+        visited = set(nums)
+        res = -1
+        def dfs(num):
+            if num not in visited:
+                return 0
+            return 1 + dfs(num * num)
+        for i in range(len(nums)):
+            total = dfs(nums[i])
+            if total != 1:
+                res = max(res, dfs(nums[i]))
+        return res
