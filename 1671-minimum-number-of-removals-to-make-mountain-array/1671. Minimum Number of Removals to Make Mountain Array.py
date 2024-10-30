62e29882787e360d10e6ec9aa8eb57176b4fd8be
Geoffrey Lee <geoffrey31415@gmail.com>
2024-10-30 16:09:42 -0400

    Runtime: 27 ms (81.13%), Memory: 17.2 MB (6.79%) - Gitcode

--- 1671-minimum-number-of-removals-to-make-mountain-array/1671. Minimum Number of Removals to Make Mountain Array.py
+++ 1671-minimum-number-of-removals-to-make-mountain-array/1671. Minimum Number of Removals to Make Mountain Array.py
@@ -1,10 +1,8 @@
 class Solution:
     def minimumMountainRemovals(self, nums: List[int]) -> int:
-        left = [nums[0]]
-        a = nums.copy()
-        right = a[::-1]
-        res = 1e9
-        def increasing(nums):
+        a = []
+        b = []
+        def increasing(nums, option):
             stack = []
             for i in range(len(nums)):
                 idx = bisect_left(stack, nums[i])
@@ -12,15 +10,18 @@ class Solution:
                     stack.append(nums[i])
                 else:
                     stack[idx] = nums[i]
-            return len(stack)
-
-        for i in range(1, len(nums) - 1):
-            # Place peak at index i
-            left.append(nums[i])
-            right.pop()
-            a,b = increasing(left), increasing(right)
-            if a > 1 and b > 1:
-                print(a,b)
-                res = min(res, len(nums) - a - b + 1)
+                if option:
+                    a.append(len(stack))
+                else:
+                    b.append(len(stack))
+        increasing(nums, 1)
+        increasing(nums[::-1], 0)
+        res = 1e9
+        for i in range(1, len(a) - 1):
+            if a[i] > 1 and b[len(nums) - i - 1] > 1:
+                res = min(res, len(nums) - (a[i] + b[len(nums) - i - 1]) + 1) 
+                if res == 4: print(i)
+        print(a)
+        print(b)
 
         return res
