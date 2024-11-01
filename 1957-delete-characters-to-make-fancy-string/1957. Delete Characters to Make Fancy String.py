7e76475da9ec1abcb815cca75ca9cf0310605672
Geoffrey Lee <geoffrey31415@gmail.com>
2024-10-31 20:08:13 -0400

    Runtime: 291 ms (70.27%), Memory: 18.6 MB (55.17%) - Gitcode

--- 1957-delete-characters-to-make-fancy-string/1957. Delete Characters to Make Fancy String.py
+++ 1957-delete-characters-to-make-fancy-string/1957. Delete Characters to Make Fancy String.py
@@ -1,20 +1,12 @@
 class Solution:
     def makeFancyString(self, s: str) -> str:
-        res = ""
-        idx = 0
-        while idx < len(s):
-            count = 1
-            curr = s[idx]
-            temp_idx = idx
-            while temp_idx+1 < len(s) and s[temp_idx+1] == curr:
-                count += 1
-                temp_idx += 1
-            if count >= 3:
-                res += curr + curr
-                while idx < len(s) and s[idx] == curr:
-                    idx += 1
+        stack = []
+        for c in s:
+            if len(stack) < 2:
+                stack.append(c)
             else:
-                res += curr
-                idx += 1
-        return res
-        
+                if stack[-1] == c and stack[-2] == c:
+                    continue
+                else:
+                    stack.append(c)
+        return "".join(stack)
