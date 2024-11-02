101984607178147fe6fad5be3e318b0ec50f7275
Geoffrey Lee <geoffrey31415@gmail.com>
2024-11-02 00:48:13 -0400

    Runtime: 37 ms (5.12%), Memory: 16.7 MB (20.17%) - Gitcode

--- 9-palindrome-number/9. Palindrome Number.py
+++ 9-palindrome-number/9. Palindrome Number.py
@@ -1,13 +1,16 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
-        if (x % 10 == 0 and x != 0):
-            return False
-        revertedNumber = 0
-        while x > revertedNumber:
-            revertedNumber = revertedNumber * 10 + x % 10
-            x //= 10
-
-        # When the length is an odd number, we can get rid of the middle digit by revertedNumber//10
-        # For example when the input is 12321, at the end of the while loop we get x = 12, revertedNumber = 123,
-        # since the middle digit doesn't matter in palidrome(it will always equal to itself), we can simply get rid of it.
-        return x == revertedNumber or x == revertedNumber // 10
+        if x < 0: return False
+        lst = []
+        while x != 0:
+            lst.append(x%10)
+            x//=10
+            print(x)
+        print(lst)
+        l, r = 0, len(lst) - 1
+        while l < r:
+            if lst[l] != lst[r]:
+                return False
+            l += 1
+            r -= 1
+        return True
