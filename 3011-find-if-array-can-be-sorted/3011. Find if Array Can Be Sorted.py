d1a1c15dbacc1beb47983f5efa0f5845e1807e30
Geoffrey Lee <geoffrey31415@gmail.com>
2024-11-05 21:45:55 -0500

    Runtime: 487 ms (5.45%), Memory: 17.1 MB (23.35%) - Gitcode

---
+++ 3011-find-if-array-can-be-sorted/3011. Find if Array Can Be Sorted.py
@@ -0,0 +1,16 @@
+class Solution:
+    def canSortArray(self, nums: List[int]) -> bool:
+        def valid(a,b):
+            return bin(a)[2:].count("1") == bin(b)[2:].count("1")
+        
+        for i in range(len(nums)):
+            for j in range(len(nums) - i - 1):
+                if nums[j] > nums[j+1] and valid(nums[j], nums[j+1]):
+                    print('swap', nums[i], nums[j])
+                    nums[j], nums[j+1] = nums[j+1], nums[j]
+            print(nums)
+        print(nums)
+        return nums == sorted(nums)
+
+
+
