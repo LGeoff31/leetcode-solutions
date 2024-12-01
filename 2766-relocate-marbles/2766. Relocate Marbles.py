cff2841eab4be9b10eaa04d91c1222dd8154e92f
Geoffrey Lee <geoffrey31415@gmail.com>
2024-11-30 22:19:04 -0500

    Runtime: 148 ms (6.12%), Memory: 42.2 MB (5.44%) - Gitcode

---
+++ 2766-relocate-marbles/2766. Relocate Marbles.py
@@ -0,0 +1,13 @@
+class Solution:
+    def relocateMarbles(self, nums: List[int], moveFrom: List[int], moveTo: List[int]) -> List[int]:
+        dic = Counter(nums)
+        for i in range(len(moveFrom)):
+            if moveFrom[i] == moveTo[i]:
+                continue
+            dic[moveTo[i]] = dic[moveFrom[i]] + dic.get(moveTo[i], 0)
+            del dic[moveFrom[i]]
+            # print(dic)
+        res = set()
+        for key in dic:
+            res.add(key)
+        return sorted(list(res))
