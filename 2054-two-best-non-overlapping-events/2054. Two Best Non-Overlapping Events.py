0cdfafe4e7e75bea21ab78a023c3d6e06499d5ab
Geoffrey Lee <geoffrey31415@gmail.com>
2024-12-07 19:38:56 -0500

    Runtime: 204 ms (42.72%), Memory: 60.9 MB (46.61%) - Gitcode

--- 2054-two-best-non-overlapping-events/2054. Two Best Non-Overlapping Events.py
+++ 2054-two-best-non-overlapping-events/2054. Two Best Non-Overlapping Events.py
@@ -3,14 +3,14 @@ class Solution:
     def maxTwoEvents(self, events: List[List[int]]) -> int:
         # events.sort()
         events = sorted(events, key = lambda x: x[1])
-        # print(events)
+
         prefix = []
-        prefix_values = SortedList()
+        prefix_values = []
         res = max([v for s,e,v in events]) # Picking singular event
         for start, end, value in events:
             
             if not prefix:
-                prefix_values.add(end)
+                prefix_values.append(end)
                 prefix.append([start, end, value])
             else:
                 # Binary search for the valid index in prefix
@@ -18,13 +18,13 @@ class Solution:
                 idx = bisect_left(prefix_values, start) - 1
                 if idx == -1:
                     prefix.append([start, end, max(value, prefix[-1][2])])
-                    prefix_values.add(end)
+                    prefix_values.append(end)
                     continue
                 # print('idx', idx, start, end, prefix_values)
                 # if len(prefix_values) == idx + 1: idx += 1 
                 prefix_value = prefix[idx][2]
                 res = max(res, value + prefix_value)
                 prefix.append([start, end, max(value, prefix_value, prefix[-1][2])])
-                prefix_values.add(end)
-            # print(prefix)
+                prefix_values.append(end)
+
         return res
