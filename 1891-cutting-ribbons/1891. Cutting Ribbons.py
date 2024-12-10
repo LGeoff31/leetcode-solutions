cd8ae08b43742cb23e6f5fea13885cc72701191d
Geoffrey Lee <geoffrey31415@gmail.com>
2024-12-09 22:15:40 -0500

    Runtime: 344 ms (57.10%), Memory: 29.7 MB (29.99%) - Gitcode

---
+++ 1891-cutting-ribbons/1891. Cutting Ribbons.py
@@ -0,0 +1,17 @@
+class Solution:
+    def maxLength(self, ribbons: List[int], k: int) -> int:
+        if sum(ribbons) < k: return 0
+
+        l, r = 1, max(ribbons)
+        res = 0
+        while l <= r:
+            mid = (l+r) // 2
+            curr = 0
+            for num in ribbons:
+                curr += num // mid
+            if curr >= k:
+                res = max(res, mid)
+                l = mid + 1
+            else:
+                r = mid - 1
+        return res
