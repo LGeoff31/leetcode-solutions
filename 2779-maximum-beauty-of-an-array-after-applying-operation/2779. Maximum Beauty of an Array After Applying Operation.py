33ccaa207cfae96f909ca2e9e5649dbdbaf9257f
Geoffrey Lee <geoffrey31415@gmail.com>
2024-12-10 23:16:07 -0500

    Runtime: 303 ms (31.19%), Memory: 28.2 MB (31.74%) - Gitcode

--- 2779-maximum-beauty-of-an-array-after-applying-operation/2779. Maximum Beauty of an Array After Applying Operation.py
+++ 2779-maximum-beauty-of-an-array-after-applying-operation/2779. Maximum Beauty of an Array After Applying Operation.py
@@ -1,6 +1,5 @@
 class Solution:
     def maximumBeauty(self, nums: List[int], k: int) -> int:
-        if len(nums) == 1: return 1
         nums.sort()
         res = 0
         for i in range(len(nums)):
@@ -8,5 +7,3 @@ class Solution:
             idx = bisect_right(nums, maxNum)
             res = max(res, idx - i)
         return res
-
-        42
