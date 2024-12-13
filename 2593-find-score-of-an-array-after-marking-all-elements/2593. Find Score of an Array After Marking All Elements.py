b4e9d710d20c8a4f1c9d5d6ae1a27e9d019869cb
Geoffrey Lee <geoffrey31415@gmail.com>
2024-12-13 00:25:24 -0500

    Runtime: 639 ms (28.37%), Memory: 51.7 MB (5.47%) - Gitcode

---
+++ 2593-find-score-of-an-array-after-marking-all-elements/2593. Find Score of an Array After Marking All Elements.py
@@ -0,0 +1,19 @@
+class Solution:
+    def findScore(self, nums: List[int]) -> int:
+        lst = [[num, i] for i, num in enumerate(nums)]
+        lst = sorted(lst, key=lambda x: (x[0], x[1]))
+        visited = set()
+
+        idx = 0
+        res = 0
+        print(lst)
+        while idx < len(nums):
+            num, i = lst[idx]
+            if i not in visited:
+                visited.add(i)
+                visited.add(i+1)
+                visited.add(i-1)
+                res += num
+            idx += 1
+
+        return res
