9a251c2670f25a5637edcf6d8c3629e7cb23f2b8
Geoffrey Lee <geoffrey31415@gmail.com>
2024-12-14 13:24:28 -0500

    Runtime: 7 ms (49.15%), Memory: 19 MB (10.85%) - Gitcode

--- 207-course-schedule/207. Course Schedule.py
+++ 207-course-schedule/207. Course Schedule.py
@@ -1,13 +1,5 @@
 class Solution:
-    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        graph = defaultdict(list)
-        for u, v in prerequisites:
-            graph[u].append(v)
-        for i in range(numCourses):
-            if i not in graph:
-                graph[i] = []
-        print('graph', graph)
-        #{1: 0}
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:       
         def dfs(currCourse, visited):
             if currCourse in visited:
                 return False
@@ -21,6 +13,10 @@ class Solution:
             visited.remove(currCourse)
             return True        
         
+        graph = defaultdict(list)
+        for u, v in prerequisites:
+            graph[u].append(v)
+            
         for i in range(numCourses): #run a dfs on each i
             # print(i, graph, dfs(i, set()))
             if not dfs(i, set()): 
