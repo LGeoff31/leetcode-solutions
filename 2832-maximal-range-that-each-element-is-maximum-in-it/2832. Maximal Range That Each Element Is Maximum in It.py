d5360c8c65e7c5b27c4c362cc8bef617369e0dc9
Geoffrey Lee <geoffrey31415@gmail.com>
2024-12-15 19:33:02 -0500

    Runtime: 152 ms (91.84%), Memory: 41.2 MB (17.65%) - Gitcode

---
+++ 2832-maximal-range-that-each-element-is-maximum-in-it/2832. Maximal Range That Each Element Is Maximum in It.py
@@ -0,0 +1,14 @@
+class Solution:
+    def maximumLengthOfRanges(self, nums: List[int]) -> List[int]:
+        stack = [] # (idx, val) in decreasing order
+        n = len(nums)
+        ans = [0] * n
+        nums.append(1e9)
+        for i, num in enumerate(nums):
+            while stack and stack[-1][1] < num:
+                idx, val = stack.pop()
+                left = -1 if not stack else stack[-1][0]
+                ans[idx] = i - left - 1
+            stack.append((i, num))
+        return ans
+                        
