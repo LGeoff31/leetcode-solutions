9a4b640fd23edbff6e6c3bec4e553ac36500cb75
Geoffrey Lee <geoffrey31415@gmail.com>
2024-12-16 18:39:18 -0500

    Runtime: 126 ms (5.59%), Memory: 32.1 MB (5.44%) - Gitcode

--- 2488-count-subarrays-with-median-k/2488. Count Subarrays With Median K.py
+++ 2488-count-subarrays-with-median-k/2488. Count Subarrays With Median K.py
@@ -1,4 +1,3 @@
-from sortedcontainers import SortedList
 class Solution:
     def countSubarrays(self, nums: List[int], k: int) -> int:
         # The subarray must have sum of 0 or 1
@@ -21,21 +20,9 @@ class Solution:
             if prefix[i] in dic or prefix[i]-1 in dic:
                 indexes_sum_0 = dic[prefix[i]]
                 indexes_sum_1 = dic[prefix[i] - 1]
-                # Binary search: Find index such that idx < idx_zero
                 res += bisect_left(indexes_sum_0, idx_zero)
                 res += bisect_left(indexes_sum_1, idx_zero)
-                # for idx in indexes_sum_0: #O(n)?
-                #     if idx >= idx_zero: break
-                #     # Verify that each idx 
-                #     if idx < idx_zero:
-                #         res += 1
-                # for idx in indexes_sum_1:
-                #     if idx >= idx_zero: break
-                #     if idx < idx_zero:
-                #         res += 1
-            print(i, res)
+           
             dic[prefix[i]].append(i)
 
-        print(nums)
-        print(prefix)
         return res
