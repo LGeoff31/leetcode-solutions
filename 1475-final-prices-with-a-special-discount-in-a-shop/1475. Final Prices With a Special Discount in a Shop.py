12546d90b389236ce35c7f0ec2a1f5aad2bf6bbf
Geoffrey Lee <geoffrey31415@gmail.com>
2024-12-17 19:19:13 -0500

    Runtime: 0 ms (100.00%), Memory: 18 MB (10.22%) - Gitcode

--- 1475-final-prices-with-a-special-discount-in-a-shop/1475. Final Prices With a Special Discount in a Shop.py
+++ 1475-final-prices-with-a-special-discount-in-a-shop/1475. Final Prices With a Special Discount in a Shop.py
@@ -1,11 +1,23 @@
 class Solution:
     def finalPrices(self, prices: List[int]) -> List[int]:
-        ans = []
-        for i in range(len(prices)):
-            discount = 0
-            for j in range(i+1, len(prices)):
-                if prices[j] <= prices[i]:
-                    discount = prices[j]
-                    break
-            ans.append(prices[i] - discount)
+        stack = [] # (idx, val) -> increasing order
+        ans = [0] * len(prices)
+        for i, num in enumerate(prices):
+            while stack and num <= stack[-1][1]:
+                idx, val = stack.pop()
+                ans[idx] = val - num
+            stack.append((i, num))
+        while stack:
+            idx, val = stack.pop()
+            ans[idx] = val
         return ans
+
+
+
+# [8, 4]
+
+# []
+
+
+# [(3,2), (4,3)]
+# [4, 2, 4, 0, 0]
