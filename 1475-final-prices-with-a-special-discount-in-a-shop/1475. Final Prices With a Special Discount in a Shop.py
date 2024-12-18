a6cd42a462e3be8ff8ec6829a53dc777ef07a38d
Geoffrey Lee <geoffrey31415@gmail.com>
2024-12-17 19:10:49 -0500

    Runtime: 3 ms (55.13%), Memory: 17.9 MB (10.22%) - Gitcode

--- 1475-final-prices-with-a-special-discount-in-a-shop/1475. Final Prices With a Special Discount in a Shop.py
+++ 1475-final-prices-with-a-special-discount-in-a-shop/1475. Final Prices With a Special Discount in a Shop.py
@@ -1,18 +1,11 @@
 class Solution:
     def finalPrices(self, prices: List[int]) -> List[int]:
-        prefix = [1e9] * len(prices)
-        prefix[-1] = prices[-1]
-        for i in range(len(prices) - 2, -1, -1):
-            prefix[i] = min(prefix[i+1], prices[i])
         ans = []
         for i in range(len(prices)):
-            found = False
+            discount = 0
             for j in range(i+1, len(prices)):
                 if prices[j] <= prices[i]:
-                    ans.append(prices[i] - prices[j])
-                    found = True
+                    discount = prices[j]
                     break
-            if not found:
-                ans.append(prices[i])
-                
+            ans.append(prices[i] - discount)
         return ans
