ad81b18af3a41ce53405ee2768b5f157387b2bfa
Geoffrey Lee <geoffrey31415@gmail.com>
2024-12-24 18:49:44 -0500

    Runtime: 0 ms (100.00%), Memory: 17.1 MB (37.54%) - Gitcode

--- 122-best-time-to-buy-and-sell-stock-ii/122. Best Time to Buy and Sell Stock II.cpp
+++ 122-best-time-to-buy-and-sell-stock-ii/122. Best Time to Buy and Sell Stock II.cpp
@@ -2,36 +2,8 @@ class Solution {
 public:
     int maxProfit(vector<int>& prices) {
         int res = 0;
-        int l = 0, r = 1;
-        while (r < prices.size() && prices[l] >= prices[r]) {
-            l++;
-            r++;
-        } 
-
-        while (r < prices.size()) {
-            bool found = false;
-            while (r+1 < prices.size() && prices[r] <= prices[r+1]) {
-                r++;
-                found = true;
-            }
-            if (found) {
-                if (prices[r] >= prices[r-1]) {
-                    res += prices[r] - prices[l];
-                } else {
-                    res += prices[r-1] - prices[l];
-                }
-                l = r;
-                r++;
-            } else {
-                res += prices[r] - prices[l];
-                l = ++r;
-                r++;
-            }
-            while (r < prices.size() && prices[l] >= prices[r]) {
-                l++;
-                r++;
-            } 
-            cout << res << endl;
+        for (int i = 0; i+1 < prices.size(); i++) {
+            if (prices[i] < prices[i+1]) res += prices[i+1] - prices[i];
         }
         return res;
     }
