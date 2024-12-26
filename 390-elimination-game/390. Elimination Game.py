94287106b4e9c9a8f13bb78ac783c1c3453697a0
Geoffrey Lee <geoffrey31415@gmail.com>
2024-12-25 21:49:40 -0500

    Runtime: 26 ms (5.31%), Memory: 18.1 MB (7.26%) - Gitcode

---
+++ 390-elimination-game/390. Elimination Game.py
@@ -0,0 +1,19 @@
+class Solution:
+    def lastRemaining(self, n: int) -> int:
+
+        def f(start, size, skip, forward):
+            print(start, size, skip, forward)
+            if size == 1:
+                return start
+            
+            if forward:
+                return f(start + skip, size // 2, skip * 2, not forward)
+            else:
+                if size % 2 == 0:
+                    return f(start, size // 2, skip * 2, not forward)
+                else:
+                    return f(start + skip, size // 2, skip * 2, not forward)
+        
+        return f(1, n, 1, True)
+
+        
