0bdca2470c27727135392d0bfb2caa8c7f17b68d
Geoffrey Lee <geoffrey31415@gmail.com>
2024-12-25 21:52:15 -0500

    Runtime: 12 ms (5.84%), Memory: 17.9 MB (7.26%) - Gitcode

--- 390-elimination-game/390. Elimination Game.py
+++ 390-elimination-game/390. Elimination Game.py
@@ -14,6 +14,19 @@ class Solution:
                 else:
                     return f(start + skip, size // 2, skip * 2, not forward)
         
-        return f(1, n, 1, True)
+        start, size, skip, forward = 1, n, 1, True
+
+        while size > 1:
+            if forward:
+                start, size, skip, forward = start + skip, size // 2, skip * 2, not forward
+            else:
+                if size % 2 == 0:
+                    start, size, skip, forward = start, size // 2, skip * 2, not forward
+                else:
+                    start, size, skip, forward = start + skip, size // 2, skip * 2, not forward
+
+        return start
+        
+        # return f(1, n, 1, True)
 
         
