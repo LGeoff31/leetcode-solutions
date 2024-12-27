bcd0ea96506878dbf5088a2983690145a36922b6
Geoffrey Lee <geoffrey31415@gmail.com>
2024-12-27 01:24:57 -0500

    Runtime: 174 ms (40.42%), Memory: 28.8 MB (17.50%) - Gitcode

---
+++ 403-frog-jump/403. Frog Jump.py
@@ -0,0 +1,27 @@
+class Solution:
+    def canCross(self, stones: List[int]) -> bool:
+        if 1 not in stones: return False
+        indicies = {num: i for i, num in enumerate(stones)}
+        # @cache
+        print(indicies)
+        visited = set()
+        def dfs(idx, curr, prevJump):
+            print(idx, curr, prevJump)
+            if idx == len(stones) - 1:
+                return True
+            if prevJump < 0: 
+                return False
+            if (idx, curr, prevJump) in visited:
+                return False
+            visited.add((idx, curr, prevJump))
+            
+            res = False
+            if curr+prevJump-1 in indicies:
+                res = res or dfs(indicies[curr+prevJump-1], curr+prevJump-1, prevJump-1)
+            if curr+prevJump in indicies:
+                res = res or dfs(indicies[curr+prevJump],curr+prevJump, prevJump)
+            if curr+prevJump+1 in indicies:
+                res = res or dfs(indicies[curr+prevJump+1], curr+prevJump+1, prevJump+1)
+            return res
+            
+        return dfs(1, 1, 1)
