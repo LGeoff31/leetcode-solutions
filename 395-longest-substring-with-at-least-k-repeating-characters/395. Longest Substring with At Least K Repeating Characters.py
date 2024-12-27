9dcf00fcaf25a6d11a8dbb2901f10ce4c500a1bb
Geoffrey Lee <geoffrey31415@gmail.com>
2024-12-26 19:46:51 -0500

    Runtime: 11 ms (65.30%), Memory: 21.5 MB (26.14%) - Gitcode

--- 395-longest-substring-with-at-least-k-repeating-characters/395. Longest Substring with At Least K Repeating Characters.py
+++ 395-longest-substring-with-at-least-k-repeating-characters/395. Longest Substring with At Least K Repeating Characters.py
@@ -1,5 +1,6 @@
 class Solution:
     def longestSubstring(self, s: str, k: int) -> int:
+        @cache
         def dfs(string): # This dfs will be called at most 26 times
             if not string:
                 return 0
@@ -17,17 +18,14 @@ class Solution:
                 return len(string)
 
             indexes = indicies[invalidChar]
-            # leftMost = string[: start]
-            # rightMost = string[end+1 : ]
+        
             res = 0
             res = max(res, dfs(string[: indexes[0]]))
             res = max(res, dfs(string[indexes[-1]+1 : ]))
-            for idx in range(1, len(indexes)):
+            for idx in range(1, len(indexes)): # This will be O(n)
                 start, end = indexes[idx-1], indexes[idx]
                 res = max(res, dfs(string[start + 1: end]))
-            # start, end = indicies[invalidChar]
-
-            # print(string, leftMost, rightMost, start, end)
+     
             return res
 
 
