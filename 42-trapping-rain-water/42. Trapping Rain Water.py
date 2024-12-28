023c71ef83ee5491274b22ba7d4021be44fb943d
Geoffrey Lee <geoffrey31415@gmail.com>
2024-12-27 22:20:49 -0500

    Runtime: 73 ms (5.11%), Memory: 19.6 MB (5.81%) - Gitcode

--- 42-trapping-rain-water/42. Trapping Rain Water.py
+++ 42-trapping-rain-water/42. Trapping Rain Water.py
@@ -1,26 +1,19 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
-        res = 0
         n = len(height)
-        leftMax = [0] * n
-        rightMax = [0] * n
-        leftMax[0] = height[0]
-        rightMax[-1] = height[-1]
+        leftMost = [0] * n
+        leftMost[0] = height[0]
+        for i in range(1,n):
+            leftMost[i] = max(leftMost[i-1], height[i])
+        leftMost = [0] + leftMost
+        rightMost = [0] * n
+        rightMost[-1] = height[-1]
+        for i in range(len(height) -2, -1, -1):
+            rightMost[i] = max(rightMost[i+1], height[i])
+        rightMost = rightMost + [0]
 
-        for i in range(1, len(height)):
-            leftMax[i] = max(leftMax[i-1], height[i])
-        for i in range(len(height) - 2, -1, -1):
-            rightMax[i] = max(rightMax[i+1], height[i])
-
-        print(height)
-        print(leftMax)
-        print(rightMax)
-        for i in range(1, n-1):
-            # leftMax = rightMax = 0
-            # for l in range(i, -1, -1):
-            #     leftMax = max(leftMax, height[l])
-            # for r in range(i, n):
-            #     rightMax = max(rightMax, height[r])
-
-            res += min(leftMax[i], rightMax[i]) - height[i]
+        res = 0
+        for i in range(len(height)):
+            res += max(min(leftMost[i], rightMost[i]) - height[i], 0)
+            print(i, res)
         return res
