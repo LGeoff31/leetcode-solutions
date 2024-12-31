a83b044f84e69158f5e8414069deeb3eb7513b07
Geoffrey Lee <geoffrey31415@gmail.com>
2024-12-31 13:26:16 -0500

    Runtime: 406 ms (27.84%), Memory: 16.6 MB (99.95%) - Gitcode

--- 437-path-sum-iii/437. Path Sum III.py
+++ 437-path-sum-iii/437. Path Sum III.py
@@ -7,20 +7,18 @@
 class Solution:
     def pathSum(self, root: Optional[TreeNode], targetSum: int) -> int:
         self.res = 0
-        # Traverse the tree
-        def dfs(node):
-            if not node:
-                return
-            self.res += validPaths(node, 0)
-            dfs(node.left)
-            dfs(node.right)
-        
-        def validPaths(node, currSum):
+        def dfs(node, currSum):
             if not node:
                 return 0
             if currSum + node.val == targetSum:
-                return 1 + validPaths(node.left, currSum + node.val) + validPaths(node.right, currSum + node.val)
-            return validPaths(node.left, currSum + node.val) + validPaths(node.right, currSum + node.val)
+                return 1 + dfs(node.left, currSum + node.val) + dfs(node.right, currSum + node.val) 
+            return dfs(node.left, currSum + node.val) + dfs(node.right, currSum + node.val) 
+        def traverse(node):
+            if not node:
+                return 0
+            return dfs(node, 0) + traverse(node.left) + traverse(node.right)
 
-        dfs(root)
-        return self.res
+        return traverse(root)
+
+
+
