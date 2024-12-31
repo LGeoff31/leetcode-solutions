d8a74939d36fa32694d68a893efab347d4a14108
Geoffrey Lee <geoffrey31415@gmail.com>
2024-12-30 22:08:19 -0500

    Runtime: 686 ms (50.64%), Memory: 19.2 MB (54.51%) - Gitcode

--- 416-partition-equal-subset-sum/416. Partition Equal Subset Sum.py
+++ 416-partition-equal-subset-sum/416. Partition Equal Subset Sum.py
@@ -4,23 +4,23 @@ class Solution:
         if totalSum % 2 == 1: return False
         goal = totalSum // 2
 
-        # Find a subset that sums to goal
-        @cache
-        def dfs(idx, currSum):
-            if currSum > goal:
-                return False
-            if currSum == goal:
-                return True
-            if idx >= len(nums):
-                return False
+        # # Find a subset that sums to goal
+        # @cache
+        # def dfs(idx, currSum): #O(2*10^4 * 2*10^2)
+        #     if currSum > goal:
+        #         return False
+        #     if currSum == goal:
+        #         return True
+        #     if idx >= len(nums):
+        #         return False
             
-            # # TAKE
-            # if dfs(idx+1, currSum + nums[idx]): return True
-            # # DONT TAKE
-            # if dfs(idx+1, currSum): return True
-            return dfs(idx+1, currSum + nums[idx]) or dfs(idx+1, currSum)
+        #     # # TAKE
+        #     # if dfs(idx+1, currSum + nums[idx]): return True
+        #     # # DONT TAKE
+        #     # if dfs(idx+1, currSum): return True
+        #     return dfs(idx+1, currSum + nums[idx]) or dfs(idx+1, currSum)
             
-        return dfs(0 ,0)
+        # return dfs(0 ,0)
 
         # nums.sort()
         # curr = 0
@@ -33,19 +33,19 @@ class Solution:
         # return False
         # if sum(nums) % 2 == 1: return False
 
-        # targetSum = sum(nums) // 2
+        targetSum = sum(nums) // 2
         # #Time: O(n^2)
         # #Memory: O(n)
-        # dp = set()
-        # dp.add(0)
-        # for i in range(len(nums)):
-        #     new_dp = set()
-        #     for elem in dp:
-        #         new_dp.add(elem)
-        #         if elem + nums[i] <= targetSum:
-        #             new_dp.add(elem + nums[i])
-        #     dp = new_dp
-        # return targetSum in dp
+        dp = set()
+        dp.add(0)
+        for i in range(len(nums)):
+            new_dp = set()
+            for elem in dp:
+                new_dp.add(elem)
+                if elem + nums[i] <= targetSum:
+                    new_dp.add(elem + nums[i])
+            dp = new_dp
+        return targetSum in dp
 
 
 
