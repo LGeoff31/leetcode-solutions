d846bcc09a6a55fe3e4016d94c50c86b3c0b4231
Geoffrey Lee <geoffrey31415@gmail.com>
2024-12-31 13:31:57 -0500

    Runtime: 51 ms (57.56%), Memory: 18.3 MB (10.39%) - Gitcode

--- 438-find-all-anagrams-in-a-string/438. Find All Anagrams in a String.py
+++ 438-find-all-anagrams-in-a-string/438. Find All Anagrams in a String.py
@@ -1,25 +1,30 @@
 class Solution:
     def findAnagrams(self, s: str, p: str) -> List[int]:
+        if len(p) > len(s): return []
+        l, r = 0, len(p) - 1
+        dic = defaultdict(int)
+        for i in range(len(p)):
+            dic[s[i]] += 1
+        target = Counter(p)
         res = []
+        # if dic == target:
+        #     res.append(0)
 
-        def equal(a, b):
-            for key in b:
-                if key not in a or b[key] != a[key]: return False
-            return True
-        b = Counter(p)
-        dic = {}
 
-        l = 0
-        for r in range(len(s)):
-            dic[s[r]] = 1 + dic.get(s[r], 0)
-            if r-l+1 < len(p):
-                r+=1
-            else:
-                # print(dic)
-                if equal(dic, b):
-                    res.append(l)
-                r+=1
-                dic[s[l]] -= 1
-                l += 1
-           
+        while r < len(s):
+            if dic == target:
+                res.append(l)
+            r += 1
+            if r == len(s): break
+            dic[s[r]] += 1
+            dic[s[l]] -= 1
+            if dic[s[l]] == 0:
+                del dic[s[l]] 
+            l += 1
         return res
+
+
+
+
+
+        
