ae83825831a3793a55b33616950c364ceb05c4b7
Geoffrey Lee <geoffrey31415@gmail.com>
2024-12-30 19:16:17 -0500

    Runtime: 37 ms (5.67%), Memory: 17.2 MB (63.97%) - Gitcode

--- 983-minimum-cost-for-tickets/983. Minimum Cost For Tickets.py
+++ 983-minimum-cost-for-tickets/983. Minimum Cost For Tickets.py
@@ -1,16 +1,21 @@
 class Solution:
     def mincostTickets(self, days: List[int], costs: List[int]) -> int:
         @cache
-        def dfs(idx):
-            if idx == len(days):
+        def dfs(i):
+            if i == len(days):
                 return 0
-            # res = 1e9
-            # # Try 1 day
-            # res = min(res,  dfs(bisect_right(days, days[idx] + 1)))
-            # # Try 7 days
-            # res = min(res, dfs(bisect_right(days, days[idx] + 7)))
-            # # Try 30 days
-            # res = min(res, dfs(bisect_right(days, days[idx] + 30)))
-            return min(costs[0] + dfs(bisect_right(days, days[idx] + 1 - 1)), costs[1] + dfs(bisect_right(days, days[idx] + 7 - 1)), costs[2] + dfs(bisect_right(days, days[idx] + 30 - 1)))
+            res = 1e9
+            # Try 1 day
+            res = min(res, costs[0] + dfs(i + 1))
+            
+            # Try 7 days
+            next_idx = bisect.bisect_left(days, days[i] + 7)
+            res = min(res, costs[1] + dfs(next_idx))
 
+            # Try 30 days
+            next_idx = bisect.bisect_left(days, days[i] + 30)
+            res = min(res, costs[2] + dfs(next_idx))
+
+            return res
+        
         return dfs(0)
