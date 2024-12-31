16a11a0797946f53455c74b60553bbd1b7d07d8c
Geoffrey Lee <geoffrey31415@gmail.com>
2024-12-30 19:17:29 -0500

    Runtime: 3 ms (80.42%), Memory: 18.4 MB (6.91%) - Gitcode

--- 983-minimum-cost-for-tickets/983. Minimum Cost For Tickets.py
+++ 983-minimum-cost-for-tickets/983. Minimum Cost For Tickets.py
@@ -5,5 +5,4 @@ class Solution:
             if idx == len(days):
                 return 0
             return min(costs[0] + dfs(bisect_left(days, days[idx] + 1)), costs[1] + dfs(bisect_left(days, days[idx] + 7)), costs[2] + dfs(bisect_left(days, days[idx] + 30)))
-
         return dfs(0)
