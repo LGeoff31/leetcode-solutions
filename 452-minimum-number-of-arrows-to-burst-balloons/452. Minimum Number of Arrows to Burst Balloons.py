15c7fd51f9a20ae4c7fb87993aa30f93128f41f4
Geoffrey Lee <geoffrey31415@gmail.com>
2025-01-01 14:45:20 -0500

    Runtime: 165 ms (28.07%), Memory: 51.5 MB (9.97%) - Gitcode

--- 452-minimum-number-of-arrows-to-burst-balloons/452. Minimum Number of Arrows to Burst Balloons.py
+++ 452-minimum-number-of-arrows-to-burst-balloons/452. Minimum Number of Arrows to Burst Balloons.py
@@ -1,24 +1,15 @@
 class Solution:
     def findMinArrowShots(self, points: List[List[int]]) -> int:
-        res = len(points)
-        if len(points) == 1: return 1
-        l, r = 0, 1
-        points.sort()   
-        print(points)
-        prevXMin = points[0][0] #1
-        prevXMax = points[0][1] #6
-         
-        for start, end in points[1:]:
-          
-            if prevXMin <= start <= prevXMax: 
-                res -= 1
-                prevXMin = max(prevXMin, start) #2
-                prevXMax = min(prevXMax, end) #6
-                print(prevXMin, prevXMax)
+        points.sort()
+        res = 0
+        idx = 1
+        min_x = points[0][1]
+        while idx < len(points):
+            start, end = points[idx]
+            if start > min_x:
+                res += 1
+                min_x = end
             else:
-                prevXMin = start
-                prevXMax = end
-                print(prevXMin, prevXMax)
-
-
-        return res
+                min_x = min(min_x, end)
+            idx += 1
+        return res+1
