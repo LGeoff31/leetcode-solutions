76ba1ae5a0b957293beeb2e9e3839543cf993ab7
Geoffrey Lee <geoffrey31415@gmail.com>
2025-01-05 12:25:48 -0500

    Runtime: 1352 ms (91.45%), Memory: 278.6 MB (29.50%) - Gitcode

--- 474-ones-and-zeroes/474. Ones and Zeroes.py
+++ 474-ones-and-zeroes/474. Ones and Zeroes.py
@@ -1,6 +1,6 @@
 class Solution:
     def findMaxForm(self, strs: List[str], m: int, n: int) -> int:
-        lst = [(a.count("0"), a.count("1")) for a in strs]
+        lst = [[a.count("0"), a.count("1")] for a in strs]
         @cache
         def dfs(zeros, ones, idx):
             if zeros < 0 or ones < 0:
@@ -8,5 +8,7 @@ class Solution:
             if idx == len(strs):
                 return 0
             
-            return max(dfs(zeros, ones, idx+1), 1 + dfs(zeros-lst[idx][0], ones-lst[idx][1], idx+1))
+            # Take or don't take situation
+            return max(1 + dfs(zeros - lst[idx][0], ones - lst[idx][1], idx+1), dfs(zeros, ones, idx+1))
+        
         return dfs(m, n, 0)
