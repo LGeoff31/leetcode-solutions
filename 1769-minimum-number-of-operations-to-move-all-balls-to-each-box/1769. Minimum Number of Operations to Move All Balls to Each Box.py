39772ae0e3abed978ba28dcdbfaba527aae3a336
Geoffrey Lee <geoffrey31415@gmail.com>
2025-01-05 21:04:54 -0500

    Runtime: 15 ms (53.09%), Memory: 18.5 MB (6.17%) - Gitcode

--- 1769-minimum-number-of-operations-to-move-all-balls-to-each-box/1769. Minimum Number of Operations to Move All Balls to Each Box.py
+++ 1769-minimum-number-of-operations-to-move-all-balls-to-each-box/1769. Minimum Number of Operations to Move All Balls to Each Box.py
@@ -1,10 +1,28 @@
 class Solution:
     def minOperations(self, boxes: str) -> List[int]:
+        n = len(boxes)
+        right = [0] * n
+        left = [0] * n
+        for i in range(len(boxes) -2, -1, -1):
+            if boxes[i+1] == "1":
+                right[i] = 1 + right[i+1]
+            else:
+                right[i] = right[i+1]
+        for i in range(1, len(boxes)):
+            if boxes[i-1] == "1":
+                left[i] = 1 + left[i-1]
+            else:
+                left[i] = left[i-1]
+        prefixLeft = [0] * n
+        prefixRight = [0] * n
+        for i in range(1, n):
+            prefixLeft[i] = prefixLeft[i-1] + left[i]
+        for i in range(n-2, -1, -1):
+            prefixRight[i] = prefixRight[i+1] + right[i]
         ans = []
-        for i in range(len(boxes)):
-            curr = 0
-            for j in range(len(boxes)):
-                if boxes[j] == "1":
-                    curr += abs(j-i)
-            ans.append(curr)
+        print(prefixRight)
+        print(prefixLeft)
+        for i in range(len(prefixRight)):
+            ans.append(prefixRight[i] + prefixLeft[i])
         return ans
+      
