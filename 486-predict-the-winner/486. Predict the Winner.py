36d071e81000174bddeedb185933cbd8cd08073a
Geoffrey Lee <geoffrey31415@gmail.com>
2025-01-11 21:12:07 -0500

    Runtime: 4 ms (41.72%), Memory: 18.2 MB (7.50%) - Gitcode

--- 486-predict-the-winner/486. Predict the Winner.py
+++ 486-predict-the-winner/486. Predict the Winner.py
@@ -1,33 +1,12 @@
 class Solution:
     def predictTheWinner(self, nums: List[int]) -> bool:
-        self.n = len(nums)
-        def dfs(queue, one, two, turn): # true -> playerOne
-            if not queue:
-                print(one, two)
-                return one >= two
-            
-            if turn:
-                res = False
-                # Take from the front
-                val = queue.popleft()
-                res = res or dfs(queue, one + val if turn else one, two + val if not turn else two, not turn)
-                queue.appendleft(val)
-                # Take from the back
-                val = queue.pop()
-                res = res or dfs(queue, one + val if turn else one, two + val if not turn else two, not turn)
-                queue.append(val)
-                return res
-            else:
-                res = True
-                # Take from the front
-                val = queue.popleft()
-                res = res and dfs(queue, one + val if turn else one, two + val if not turn else two, not turn)
-                queue.appendleft(val)
-                # Take from the back
-                val = queue.pop()
-                res = res and dfs(queue, one + val if turn else one, two + val if not turn else two, not turn)
-                queue.append(val)
-                return res
+        @cache
+        def dfs(i, j):
+            if i > j: 
+                return 0
+            a = nums[i] + min(dfs(i+1, j-1), dfs(i+2, j))
+            b = nums[j] + min(dfs(i+1, j-1), dfs(i, j-2))
+            return max(a,b)
     
-        return dfs(deque(nums), 0, 0, True)
-    
+        p1 = dfs(0, len(nums) - 1)
+        return p1 >= sum(nums) - p1
