34aac3ecd410b13cdea45cda450594f8b7eb31c2
Geoffrey Lee <geoffrey31415@gmail.com>
2025-01-12 20:36:44 -0500

    Runtime: 506 ms (5.30%), Memory: 37.3 MB (26.47%) - Gitcode

--- 1488-avoid-flood-in-the-city/1488. Avoid Flood in The City.py
+++ 1488-avoid-flood-in-the-city/1488. Avoid Flood in The City.py
@@ -10,7 +10,7 @@ class Solution:
                 if rains[i] in dic and dic[rains[i]][0] == -1 and len(free) == 0:
                     return []
                 elif rains[i] in dic and dic[rains[i]][0] == -1:
-                    idx = bisect_left(free, dic[rains[i]][1])
+                    idx = bisect_right(free, dic[rains[i]][1])
                     if idx == len(free): return []
                     a = free[idx]
                     free.remove(a)
@@ -19,9 +19,9 @@ class Solution:
                     #     return []
                     res[a] = rains[i]
                     res[i] = -1
-                    dic[rains[i]] = [-1, i]
+                    dic[rains[i]] = (-1, i)
                 else:
-                    dic[rains[i]] = [-1, i]
+                    dic[rains[i]] = (-1, i)
                     res[i] = -1
             else:
                 free.add(i)
