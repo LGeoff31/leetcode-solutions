e4bc249728b9b8bc703121bd431f7b677cfb7feb
Geoffrey Lee <geoffrey31415@gmail.com>
2025-01-20 19:57:38 -0500

    Runtime: 104 ms (56.72%), Memory: 30.7 MB (9.84%) - Gitcode

--- 2017-grid-game/2017. Grid Game.py
+++ 2017-grid-game/2017. Grid Game.py
@@ -1,11 +1,10 @@
 class Solution:
     def gridGame(self, grid: List[List[int]]) -> int:
         res = float("inf")
-        totalSumFirst = sum(grid[0])
+        n = len(grid[0])
+        totalSumFirstRow = sum(grid[0])
         prefix1 = list(accumulate(grid[0]))
         prefix2 = list(accumulate(grid[1]))
-        n = len(grid[0])
         for i in range(n):
-            res = min(res, max(prefix2[i] - grid[1][i], totalSumFirst - prefix1[i]))
-
+            res = min(res, max(prefix2[i] - grid[1][i], totalSumFirstRow - prefix1[i]))
         return res
