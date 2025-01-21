cb00837b487964bdaecd4546d719dffa989895d3
Geoffrey Lee <geoffrey31415@gmail.com>
2025-01-20 19:53:34 -0500

    Runtime: 96 ms (60.33%), Memory: 30.6 MB (11.80%) - Gitcode

---
+++ 2017-grid-game/2017. Grid Game.py
@@ -0,0 +1,11 @@
+class Solution:
+    def gridGame(self, grid: List[List[int]]) -> int:
+        res = 10000000000
+        totalSumFirst = sum(grid[0])
+        prefix1 = list(accumulate(grid[0]))
+        prefix2 = list(accumulate(grid[1]))
+        n = len(grid[0])
+        for i in range(n):
+            res = min(res, max(prefix2[i] - grid[1][i], totalSumFirst - prefix1[i]))
+
+        return res
