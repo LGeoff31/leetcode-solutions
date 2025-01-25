97bbf3ca957bbf5bb18d3195c53d5e4bf8c85ac6
Geoffrey Lee <geoffrey31415@gmail.com>
2025-01-25 12:09:13 -0500

    Runtime: 630 ms (30.56%), Memory: 58.9 MB (20.84%) - Gitcode

--- 2948-make-lexicographically-smallest-array-by-swapping-elements/2948. Make Lexicographically Smallest Array by Swapping Elements.py
+++ 2948-make-lexicographically-smallest-array-by-swapping-elements/2948. Make Lexicographically Smallest Array by Swapping Elements.py
@@ -1,45 +1,36 @@
 class Solution:
     def lexicographicallySmallestArray(self, nums: List[int], limit: int) -> List[int]:
-        #generate pairs array [i,j] denoting the indexes i and j can be swapped
-        dic = defaultdict(list)
+        a = []
         for i in range(len(nums)):
-            dic[nums[i]].append(i)
-        a = copy.deepcopy(nums)
+            a.append((nums[i], i))
         a.sort()
-        pairs = []
-        for i in range(1, len(a)):
-            if a[i] - a[i-1] <= limit:
-                for elem1 in dic[a[i]]:
-                    for elem2 in dic[a[i-1]]:
-                        pairs.append([elem1, elem2])
+        lst = []
+        curr = []
+        idx = 0
+        # Create disjoint sets
+        while idx < len(a):
+            if not curr:
+                curr.append((a[idx][0], a[idx][1]))
+                idx += 1
+                continue
 
+            if abs(a[idx][0] - curr[-1][0]) <= limit:
+                curr.append((a[idx][0], a[idx][1]))
+            else:
+                lst.append(curr)
+                curr = [(a[idx][0], a[idx][1])]
+            idx += 1
+        if curr:
+            lst.append(curr)
+        res = [0] * len(nums)
+        for arr in lst:
+            idx_lst = []
+            for val, idx in arr:
+                idx_lst.append(idx)
+            idx_lst.sort()
+            for i in range(len(arr)):
+                res[idx_lst[i]] = arr[i][0]
 
-
-        # print(pairs, nums)
-        parent = list(range(len(nums)))
-        def find(a):
-            if parent[a] != a:
-                parent[a] = find(parent[a])
-            return parent[a]
-    
-        def union(a,b):
-            parent[find(a)] = find(b)
-        for a,b in pairs:
-            union(a,b)
-        
-        group_c = defaultdict(list)
-
-        for i in range(len(nums)):
-            group = find(i)
-            group_c[group].append(nums[i])
-        res = []
-        for key in group_c:
-            group_c[key] = sorted(group_c[key], reverse=True)
-        # print(group_c)
-        for i in parent:
-            res.append(group_c[i][-1])
-            group_c[i].pop()
+        print(lst)
 
         return res
-        
-         
