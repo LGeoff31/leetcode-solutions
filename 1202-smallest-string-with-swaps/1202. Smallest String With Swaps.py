35c4c0484e2eb295340ac0b6d7834b2c0fa6c7be
Geoffrey Lee <geoffrey31415@gmail.com>
2025-01-25 11:42:25 -0500

    Runtime: 158 ms (20.44%), Memory: 49.7 MB (30.86%) - Gitcode

--- 1202-smallest-string-with-swaps/1202. Smallest String With Swaps.py
+++ 1202-smallest-string-with-swaps/1202. Smallest String With Swaps.py
@@ -1,36 +1,37 @@
 class Solution:
     def smallestStringWithSwaps(self, s: str, pairs: List[List[int]]) -> str:
         n = len(s)
-        parent = list(range(n))
+        parent = list(range(n)) # [0,1,2,3]
+
         def find(a):
             if parent[a] != a:
                 parent[a] = find(parent[a])
             return parent[a]
 
         def union(a,b):
-            parent[find(a)] = find(b)
+            parent[find(a)] = find(b) # [3,2,2,3]
+
         
-        for a,b in pairs:
-            union(a,b)
-        print(parent)
-        group_i = defaultdict(list)
-        group_ch = defaultdict(list)
-
+        # Create the disjoint sets
+        for u,v in pairs:
+            union(u,v)
+            # print(parent)
+        index = defaultdict(list)
+        characters = defaultdict(list)
         for i in range(len(s)):
-            group = find(i)
-            group_i[group].append(i)
-            group_ch[group].append(s[i])
-        for key in group_ch:
-            group_ch[key].sort()
-        # a = ["@"] * len(s)
-        print(group_i)
-        print(group_ch)
+            group = find(i) # i belongs to which group
+            index[group].append(i)
+            characters[group].append(s[i])
+        for key in characters:
+            characters[key].sort()
+        res = [''] * n
+        for (i, c) in zip(index.values(), characters.values()):
+            for k in range(len(i)):
+                res[i[k]] = c[k]
 
-        res = ["2"] * n
-        for key in group_i:
-            indicies = group_i[key]
-            for idx, val in enumerate(indicies):
-                res[val] = group_ch[key][idx]
-    
+            print(i,c)
+        print(index)
+        print(characters)
+
         return "".join(res)
-
+        
