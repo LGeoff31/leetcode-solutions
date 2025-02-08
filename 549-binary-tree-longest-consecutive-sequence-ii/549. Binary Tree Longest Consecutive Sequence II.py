ab91256cf1a8b49c6cf263e3c7860c377225a713
Geoffrey Lee <geoffrey31415@gmail.com>
2025-02-08 16:39:19 -0500

    Runtime: 23 ms (5.26%), Memory: 20 MB (6.70%) - Gitcode

--- 549-binary-tree-longest-consecutive-sequence-ii/549. Binary Tree Longest Consecutive Sequence II.py
+++ 549-binary-tree-longest-consecutive-sequence-ii/549. Binary Tree Longest Consecutive Sequence II.py
@@ -7,33 +7,37 @@
 class Solution:
     def longestConsecutive(self, root: Optional[TreeNode]) -> int:
         if not root: return 0
-        cache = {}
-        def valid_children(node):
+        # cache = {}
+        def valid_children(node_id):
+            node = id_to_node[node_id]
             isLeftPath = node.left and abs(node.left.val - node.val) == 1
             isRightPath = node.right and abs(node.right.val - node.val) == 1
             return isLeftPath or isRightPath
         self.res = 1
 
-        def dfs(node):
+        @cache
+        def dfs(node_id):
+            print(node_id)
+            node = id_to_node[node_id]
             print('node', node.val)
             # Leaf node
             if node.left == None and node.right == None:
-                return [1,1]
+                return (1,1)
             # If none of children satisfy difference of 1 constraint
-            if not valid_children(node):
-                return [1,1]
+            if not valid_children(node_id):
+                return (1,1)
 
             largest_decr, largest_incr = 1, 1
             # Finding largest decrement
             if node.left and node.val - node.left.val == 1:
-                largest_decr = max(largest_decr, 1 + dfs(node.left)[0]) # 1 + 1 + 1
+                largest_decr = max(largest_decr, 1 + dfs(id(node.left))[0]) # 1 + 1 + 1
             if node.right and node.val - node.right.val == 1:
-                largest_decr = max(largest_decr, 1 + dfs(node.right)[0])
+                largest_decr = max(largest_decr, 1 + dfs(id(node.right))[0])
             # Find largest increment
             if node.left and node.left.val - node.val == 1:
-                largest_incr = max(largest_incr, 1 + dfs(node.left)[1])
+                largest_incr = max(largest_incr, 1 + dfs(id(node.left))[1])
             if node.right and node.right.val - node.val == 1:
-                largest_incr = max(largest_incr, 1 + dfs(node.right)[1])
+                largest_incr = max(largest_incr, 1 + dfs(id(node.right))[1])
             
             self.res = max(self.res, largest_decr, largest_incr)
             # Combine both
@@ -41,16 +45,24 @@ class Solution:
                 decr_incr, incr_decr = 0, 0
                 # Decrement on left, increment on right
                 if node.val - node.left.val == 1 and node.right.val - node.val == 1:
-                    decr_incr =  1 + dfs(node.left)[0] + dfs(node.right)[1]
+                    decr_incr =  1 + dfs(id(node.left))[0] + dfs(id(node.right))[1]
                 # Increment on left, decrement on right
                 if node.left.val - node.val == 1 and node.val - node.right.val == 1:
-                    incr_decr = 1 + dfs(node.left)[1] + dfs(node.right)[0]
+                    incr_decr = 1 + dfs(id(node.left))[1] + dfs(id(node.right))[0]
                 self.res = max(self.res, decr_incr, incr_decr)
 
-            return [largest_decr, largest_incr]
+            return (largest_decr, largest_incr)
+        id_to_node = {}
+        def map_nodes(node):
+            if not node:
+                return
+            id_to_node[id(node)] = node
+            map_nodes(node.left)
+            map_nodes(node.right)
+        map_nodes(root)
         def traversal(node):
-            dfs(node)
-            if node.left: print(node.left.val)
+            id_to_node[id(node)] = node
+            dfs(id(node))
             if node.left: traversal(node.left)
             if node.right: traversal(node.right)
         traversal(root)
