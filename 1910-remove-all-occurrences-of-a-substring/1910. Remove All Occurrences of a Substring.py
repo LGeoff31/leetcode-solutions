a6c7dd3c5bcb524ca150f54b9bb5591c7232b4b7
Geoffrey Lee <geoffrey31415@gmail.com>
2025-02-10 19:18:21 -0500

    Runtime: 72 ms (8.15%), Memory: 16.8 MB (100.00%) - Gitcode

--- 1910-remove-all-occurrences-of-a-substring/1910. Remove All Occurrences of a Substring.py
+++ 1910-remove-all-occurrences-of-a-substring/1910. Remove All Occurrences of a Substring.py
@@ -1,21 +1,14 @@
 class Solution:
     def removeOccurrences(self, s: str, part: str) -> str:
-        stack = []
-        def equal(stack):
-            if len(stack) < len(part):
-                return False
-            idx = len(stack) - 1
-            idx2 = len(part) - 1
-            while idx2 >= 0:
-                if stack[idx] != part[idx2]:
-                    return False
-                idx2 -= 1
-                idx -= 1
-            return True
-        for c in s:
-            stack.append(c)
-            # print(stack, equal(stack))
-            if equal(stack):
-                for i in range(len(part)):
-                    stack.pop()
-        return "".join(stack)
+        while part in s:
+            for i in range(len(s) - len(part)+1): #12-3 = 9
+                possible = s[i:i+len(part)]
+                print("possible", possible)
+                if possible == part:
+                    s = s[0:i] + s[i+len(part):]
+                    print(s)
+                    break
+                # print(s)
+        return s
+
+        
