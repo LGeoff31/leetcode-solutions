17d1c966991ae6641c6ddf5dd56a3df9a986bb34
Geoffrey Lee <geoffrey31415@gmail.com>
2025-02-10 19:03:00 -0500

    Runtime: 47 ms (36.89%), Memory: 18.1 MB (37.23%) - Gitcode

--- 572-subtree-of-another-tree/572. Subtree of Another Tree.py
+++ 572-subtree-of-another-tree/572. Subtree of Another Tree.py
@@ -6,24 +6,12 @@
 #         self.right = right
 class Solution:
     def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
-        def isSameTree(head1, head2):
-            if head1 is None and head2 is None: #if both null
-                return True
-            if not head1 or not head2: #if one of them is null
-                return False
-            if head1.val != head2.val:
-                return False
-            
-            
-            return isSameTree(head1.left, head2.left) and isSameTree(head1.right, head2.right)
-
-        if isSameTree(root, subRoot):
+        def equal(node1, node2):
+            if node1 and node2:
+                return node1.val == node2.val and equal(node1.left, node2.left) and equal(node1.right, node2.right)
+            return not(node1 or node2)
+        if equal(root, subRoot):
             return True
         if root is None:
             return False
         return self.isSubtree(root.left, subRoot) or self.isSubtree(root.right, subRoot)
-        
-        
-        
-        
-        
