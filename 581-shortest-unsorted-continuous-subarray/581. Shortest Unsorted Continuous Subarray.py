cf1e0dadea56d48378c9792e3423ea1a93c13994
Geoffrey Lee <geoffrey31415@gmail.com>
2025-02-11 21:08:52 -0500

    Runtime: 56 ms (5.06%), Memory: 19.3 MB (6.75%) - Gitcode

--- 581-shortest-unsorted-continuous-subarray/581. Shortest Unsorted Continuous Subarray.py
+++ 581-shortest-unsorted-continuous-subarray/581. Shortest Unsorted Continuous Subarray.py
@@ -1,10 +1,27 @@
 class Solution:
     def findUnsortedSubarray(self, nums: List[int]) -> int:
-        a = sorted(nums)
-        print(nums, a)
         start, end = 1e9, -1e9
-        for i in range(len(nums)):
-            if a[i] != nums[i]:
+
+        # Find start
+        stack = [] # (val, idx)
+        idx = 0
+        while idx < len(nums):
+            while stack and nums[idx] < stack[-1][0]:
+                val, i = stack.pop()
                 start = min(start, i)
+            stack.append((nums[idx], idx))
+            idx += 1
+        
+        # Find end
+        stack = []
+        idx = len(nums) - 1
+        while idx >= 0:
+            while stack and nums[idx] > stack[-1][0]:
+                val, i = stack.pop()
                 end = max(end, i)
-        return end-start+1 if start+1 < 100000 else 0
+            stack.append((nums[idx], idx))
+            idx -= 1
+        print(start)
+        print(end)
+
+        return end-start+1 if start < 10000 else 0
