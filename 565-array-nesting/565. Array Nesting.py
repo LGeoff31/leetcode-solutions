f710c7e325c0a74a11594633b384fca989cba18b
Geoffrey Lee <geoffrey31415@gmail.com>
2025-02-14 12:43:49 -0500

    Runtime: 248 ms (12.13%), Memory: 125 MB (5.12%) - Gitcode

---
+++ 565-array-nesting/565. Array Nesting.py
@@ -0,0 +1,20 @@
+class Solution:
+    def arrayNesting(self, nums: List[int]) -> int:
+        visited = set()
+        dic = {}
+
+        for i in range(len(nums)):
+            # if nums[i] == i: continue
+            dic[nums[i]] = nums[nums[i]]
+        print(dic)
+        def explore(num):
+            if num not in visited:
+                visited.add(num)
+                return 1 + explore(dic[num])
+            return 0
+        res = 0
+        for num in nums:
+            if num not in visited:
+                # visited.add(num)
+                res = max(res, explore(num))
+        return res
