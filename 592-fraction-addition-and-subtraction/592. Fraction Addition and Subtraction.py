18e0adde26871aae43cae885b458e0466c5ae76e
Geoffrey Lee <geoffrey31415@gmail.com>
2025-02-13 22:08:06 -0500

    Runtime: 0 ms (100.00%), Memory: 17.9 MB (24.45%) - Gitcode

--- 592-fraction-addition-and-subtraction/592. Fraction Addition and Subtraction.py
+++ 592-fraction-addition-and-subtraction/592. Fraction Addition and Subtraction.py
@@ -1,13 +1,31 @@
 class Solution:
     def fractionAddition(self, expression: str) -> str:
-        nums = list(map(int, re.findall(r'[+-]?\d+', expression)))
-        numerator = 0
-        denominator = 1
-        
-        for i in range(0, len(nums), 2):
-            num, den = nums[i], nums[i + 1]
-            numerator = numerator * den + num * denominator
-            denominator *= den
-        
-        common_divisor = gcd(numerator, denominator)
-        return f"{numerator // common_divisor}/{denominator // common_divisor}"
+        lst = re.split("[+-]", expression)
+        # def gcd(a,b): 
+        #     if a > b: return gcd(b, a)
+        #     if b % a == 0: return a
+        #     return gcd(b%a, a)
+        # def lcm(a,b):
+        #     return a*b//gcd(a,b)
+        def get_num_den(expr):
+            num, den = expr.split("/")
+            return int(num), int(den)
+        operations = []
+        for i, c in enumerate(expression):
+            if c in "-+" and i != 0: operations.append(c)
+        num, den = get_num_den("-" + lst[1] if not lst[0] else lst[0])
+        idx = 0
+        if not lst[0]: lst = lst[1:]
+        for i in range(1, len(lst)):
+            n, d = get_num_den(lst[i])
+            if operations[idx] == '-':
+                n *= -1
+            common_divisor = lcm(den, int(d))
+            num = num * (common_divisor // den) + n * (common_divisor // d)
+            den = common_divisor
+            idx += 1
+        z = gcd(num, den)
+        num //= z
+        den //= z
+        # if num == 0: return 0
+        return f"{num}/{den}"
