47caab4b6d108142d9ca4d2dd966b28175372700
Geoffrey Lee <geoffrey31415@gmail.com>
2025-02-15 12:49:11 -0500

    Runtime: 129 ms (46.39%), Memory: 19 MB (78.35%) - Gitcode

--- 1756-design-most-recently-used-queue/1756. Design Most Recently Used Queue.py
+++ 1756-design-most-recently-used-queue/1756. Design Most Recently Used Queue.py
@@ -1,12 +1,35 @@
 class MRUQueue:
     def __init__(self, n: int):
-        self.lst = list(range(1,n+1))
+        self.chunks = []
+        self.lst = list(range(1, n+1))
+        self.chunksize = ceil(sqrt(n))
+        for i in range(0, n, self.chunksize):
+            chunk = self.lst[i : i + self.chunksize]
+            self.chunks.append(deque(chunk))
 
     def fetch(self, k: int) -> int:
-        num = self.lst[k-1]
-        self.lst.remove(num)
-        self.lst.append(num)
-        return num
+        k -= 1
+        # Find the bucket, and index within bucket
+        bucket_idx = k // self.chunksize # 5 // 2 = 2
+        remainder = k % self.chunksize # 5 % 2 = 1
+        target_num = self.chunks[bucket_idx][remainder]
+
+        # Remove it
+        temp = deque()
+        for i in range(len(self.chunks[bucket_idx])):
+            if i != remainder:
+                temp.append(self.chunks[bucket_idx][i])
+        self.chunks[bucket_idx] = temp
+
+        # Add it to last bucket
+        self.chunks[-1].append(target_num)
+                
+        # Shift over every element by 1 to the left
+        for i in range(bucket_idx, len(self.chunks) - 1): # O(sqrt(len(self.chunks)))
+            curr_bucket = self.chunks[i] # Python is referncing this, so this is reference updating curr_bucket updates self.chunks
+            next_bucket = self.chunks[i+1]
+            curr_bucket.append(next_bucket.popleft())
+        return target_num
         
 
 
