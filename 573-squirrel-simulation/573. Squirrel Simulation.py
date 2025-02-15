a46f996fb8f46223bdd77d8ffbd6dfb9b7ab860c
Geoffrey Lee <geoffrey31415@gmail.com>
2025-02-15 15:19:07 -0500

    Runtime: 4 ms (87.13%), Memory: 18.5 MB (65.35%) - Gitcode

--- 573-squirrel-simulation/573. Squirrel Simulation.py
+++ 573-squirrel-simulation/573. Squirrel Simulation.py
@@ -1,21 +1,15 @@
 class Solution:
     def minDistance(self, height: int, width: int, tree: List[int], squirrel: List[int], nuts: List[List[int]]) -> int:
-        n =  set()
-        for r,c in nuts:
-            n.add((r,c))
-        dic = {}
-        for r in range(height):
-            for c in range(width):
-                if (r,c) in n:
-                    dic[(r,c)] = abs(r-tree[0]) + abs(c-tree[1])
-        print(dic)
-        res = 1e9
-        totalSum = 0
-        for key in dic:
-            r,c = key
-            totalSum += abs(r-tree[0]) + abs(c-tree[1])
-        for key in dic:
-            r,c = key
-            res = min(res, abs(r-squirrel[0]) + abs(c-squirrel[1])  + (2*totalSum) - dic[(r,c)])
-        return res
+        total_distance = 0
 
+        for r, c in nuts:
+            manhattan = abs(tree[0] - r) + abs(tree[1] - c)
+            total_distance += manhattan
+        min_distance = float('inf')
+        for r, c in nuts:
+            distance_to_squirrel = abs(squirrel[0] - r) + abs(squirrel[1] - c)
+            manhattan = abs(tree[0] - r) + abs(tree[1] - c)
+            trip_distance = (total_distance * 2) - manhattan + distance_to_squirrel
+            min_distance = min(min_distance, trip_distance)
+
+        return min_distance 
