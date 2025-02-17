e108029e5b61c7674c816cd4b6e389af189a9483
Geoffrey Lee <geoffrey31415@gmail.com>
2025-02-17 11:32:13 -0500

    Runtime: 132 ms (68.65%), Memory: 40.1 MB (16.09%) - Gitcode

--- 295-find-median-from-data-stream/295. Find Median from Data Stream.py
+++ 295-find-median-from-data-stream/295. Find Median from Data Stream.py
@@ -1,20 +1,40 @@
-from sortedcontainers import SortedList
 class MedianFinder:
-
     def __init__(self):
-        self.lst = SortedList()
-        
+        # All elements in maxHeap <= all elements in minHeap
+        # abs(len(minHeap) - len(maxHeap)) <= 1
+        self.minHeap = []
+        self.maxHeap = []
+    
+    def addNum(self, num: int) -> None: 
+        # Base case
+        if not self.maxHeap:
+            heappush(self.maxHeap, -num)
+            return
 
-    def addNum(self, num: int) -> None:
-        self.lst.add(num)
-        
+        if len(self.maxHeap) > len(self.minHeap): # Greater by 1
+            heappush(self.minHeap, num)
+            if -self.maxHeap[0] > self.minHeap[0]:
+                temp1 = heappop(self.minHeap)
+                temp2 = -heappop(self.maxHeap)
+                heappush(self.maxHeap, -temp1)
+                heappush(self.minHeap, temp2)
+        elif len(self.minHeap) > len(self.maxHeap):
+            heappush(self.maxHeap, -num)
+            if -self.maxHeap[0] > self.minHeap[0]:
+                temp1 = heappop(self.minHeap)
+                temp2 = -heappop(self.maxHeap)
+                heappush(self.maxHeap, -temp1)
+                heappush(self.minHeap, temp2)
+        else:
+            if num <= self.minHeap[0]:
+                heappush(self.maxHeap, -num)
+            else:
+                heappush(self.minHeap, num)
 
     def findMedian(self) -> float:
-        idx = len(self.lst) // 2
-        if len(self.lst)%2==0:
-            return (self.lst[idx-1] + self.lst[idx])/2
-        return self.lst[idx]
-
+        if (len(self.minHeap) + len(self.maxHeap)) % 2 == 1:
+            return self.minHeap[0] if len(self.minHeap) > len(self.maxHeap) else -self.maxHeap[0]
+        return (-self.maxHeap[0] + self.minHeap[0]) / 2
         
 
 
