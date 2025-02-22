4faec48f6850c905da6eb45cc090a0ce64911b16
Geoffrey Lee <geoffrey31415@gmail.com>
2025-02-22 10:19:53 -0500

    Runtime: 39 ms (56.58%), Memory: 18 MB (59.09%) - Gitcode

--- 487-max-consecutive-ones-ii/487. Max Consecutive Ones II.py
+++ 487-max-consecutive-ones-ii/487. Max Consecutive Ones II.py
@@ -1,24 +1,22 @@
 class Solution:
     def findMaxConsecutiveOnes(self, nums: List[int]) -> int:
-        zeroFound = False
-        l, r = 0, 0
         res = 0
-        count = 0
-        global_count = 0
-        while r < len(nums):
-            if nums[r] == 0 and zeroFound:
-                # Must remove that 0
-                while l < len(nums) and nums[l] != 0:
-                    l += 1
-                    count -= 1
-                # zeroFound = False
-                l += 1
-            elif nums[r] == 0:
-                zeroFound = True
-                count += 1
+        zeroFound = 0
+        lastConsectiveOnes = 0
+        curr = 0
+        for i in range(len(nums)):
+            if nums[i] == 0:
+                if zeroFound:
+                    curr = lastConsectiveOnes
+                    lastConsectiveOnes = 0
+                    zeroFound = True
+                    curr += 1
+                else:
+                    zeroFound = True
+                    lastConsectiveOnes = 0
+                    curr += 1
             else:
-                count += 1
-            r += 1
-            global_count = max(global_count, count)
-            print(count)
-        return global_count
+                curr += 1
+                lastConsectiveOnes += 1
+            res = max(res, curr)
+        return res
