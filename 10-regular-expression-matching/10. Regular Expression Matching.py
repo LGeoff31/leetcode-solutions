7dfbaae5c6107097b652ea1d882c01a02f216280
Geoffrey Lee <geoffrey31415@gmail.com>
2025-03-01 15:29:26 -0500

    Runtime: 19 ms (21.38%), Memory: 18.3 MB (10.38%) - Gitcode

--- 10-regular-expression-matching/10. Regular Expression Matching.py
+++ 10-regular-expression-matching/10. Regular Expression Matching.py
@@ -1,14 +1,29 @@
 class Solution:
     def isMatch(self, s: str, p: str) -> bool:
         @cache
-        def dfs(i, j):
-            if i >= len(s) and j >= len(p): return True
-            if j >= len(p): return False 
-
-            match = i < len(s) and (s[i] == p[j] or p[j] == ".")
-            if (j+1) < len(p) and p[j+1] == "*":
-                return dfs(i, j+2) or (match and dfs(i+1, j))
-            if match:
+        def dfs(i, j): # dfs(2, 0)
+            if i == len(s) and j == len(p):
+                return True
+            if i > len(s) or j > len(p):
+                return False
+            if j == len(p):
+                return False
+            if i == len(s) and (j == len(p) - 1 or p[j+1] != "*"):
+                return False
+            print(i, j)
+            if j+1 < len(p) and p[j+1] == "*":
+                # Branch
+                res = dfs(i, j+2)
+                print(res)
+                if (i < len(s) and p[j] == s[i]) or p[j] == ".":
+                    res = res or dfs(i+1, j)
+                print(i, j, res)
+                return res
+            elif p[j] == ".":
                 return dfs(i+1, j+1)
-            return False
+            else:
+                if p[j] != s[i]:
+                    return False
+                return dfs(i+1, j+1)
+            
         return dfs(0, 0)
