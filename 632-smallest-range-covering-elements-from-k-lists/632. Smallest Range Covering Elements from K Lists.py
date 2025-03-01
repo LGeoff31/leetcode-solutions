90d5d7a3d6f8bdf2ea317f7033519a001604a519
Geoffrey Lee <geoffrey31415@gmail.com>
2025-03-01 11:10:29 -0500

    Runtime: 919 ms (5.02%), Memory: 38.7 MB (89.09%) - Gitcode

--- 632-smallest-range-covering-elements-from-k-lists/632. Smallest Range Covering Elements from K Lists.py
+++ 632-smallest-range-covering-elements-from-k-lists/632. Smallest Range Covering Elements from K Lists.py
@@ -1,47 +1,18 @@
-from sortedcontainers import SortedList
 class Solution:
     def smallestRange(self, nums: List[List[int]]) -> List[int]:
-        # if len(nums) == 1:
-        #     return [nums[0][0], nums[0][0]]
-        if nums == [[1]]: return [1,1]
-        minHeap = []
-        maxHeap = []
-        k = len(nums)
         lst = SortedList()
-        res = [-1e9, 1e9]
-
-        pointers = [0] * k
-        minHeap = []
-        # Initialize the heaps
-        for i in range(k):
-            lst.add(nums[i][0])
-            heappush(minHeap, (nums[i][0], i))
-            # heappush(minHeap, nums[i][0])
-            # heappush(maxHeap, -nums[i][0])
-
+        for i in range(len(nums)):
+            lst.add([nums[i][0], i, 0])
+        start, end = lst[0][0], lst[-1][0]
         while True:
-            # if abs(maxHeap[0]) - minHeap[0] < res[-1] - res[0]:
-            #     res = [minHeap[0], abs(maxHeap[0])]
-            if len(lst) >= 2 and lst[-1] - lst[0] < res[-1] - res[0]:
-                res = [lst[0], lst[-1]]
-
-            # idx_smallest_num = -1
-            smallest_num = 1e9
-            # for i in range(k):
-            #     if nums[i][pointers[i]] < smallest_num:
-            #         smallest_num = nums[i][pointers[i]]
-            #         idx_smallest_num = i
-
-            idx_smallest_num = heappop(minHeap)[1]
-            # heappop(minHeap, nums[idx_smallest_num][pointers[i]])
-            # heappop(maxHeap, -nums[idx_smallest_num][pointers[i]])
-            lst.remove(nums[idx_smallest_num][pointers[idx_smallest_num]])
-            pointers[idx_smallest_num] += 1
-            if pointers[idx_smallest_num] == len(nums[idx_smallest_num]): # Out of bounds
-                return res
-            heappush(minHeap, (nums[idx_smallest_num][pointers[idx_smallest_num]], idx_smallest_num))
-            # heappush(minHeap, nums[idx_smallest_num][pointers[i]])
-            # heappush(maxHeap, -nums[idx_smallest_num][pointers[i]])
-            lst.add(nums[idx_smallest_num][pointers[idx_smallest_num]])
-
-        return res
+            val, idx, i = lst[0]
+            lst.remove(lst[0])
+            i += 1
+            if i >= len(nums[idx]):
+                return [start, end]
+            lst.add([nums[idx][i], idx, i])
+            if lst[-1][0] - lst[0][0] < end - start:
+                start = lst[0][0]
+                end = lst[-1][0]
+        return []
+        
