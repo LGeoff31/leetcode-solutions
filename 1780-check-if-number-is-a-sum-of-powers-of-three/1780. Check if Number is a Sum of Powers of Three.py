c76e7538939af5ecbdde32d88730bb630ab6b036
Geoffrey Lee <geoffrey31415@gmail.com>
2025-03-03 19:10:59 -0500

    Runtime: 0 ms (100.00%), Memory: 17.9 MB (18.33%) - Gitcode

--- 1780-check-if-number-is-a-sum-of-powers-of-three/1780. Check if Number is a Sum of Powers of Three.py
+++ 1780-check-if-number-is-a-sum-of-powers-of-three/1780. Check if Number is a Sum of Powers of Three.py
@@ -1,7 +1,9 @@
 class Solution:
     def checkPowersOfThree(self, n: int) -> bool:
-        while n != 0:
-            n, r = n // 3, n % 3
-            if r == 2:
-                return False
-        return True
+        i = floor(log(n, 3))
+        while i >= 0:
+            if 3 ** i <= n:
+                n -= 3 ** i
+            i -= 1
+        
+        return n == 0
