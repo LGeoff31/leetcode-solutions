a14aa81f8b06aa07e6cc95e11f777f5c25315fb1
Geoffrey Lee <geoffrey31415@gmail.com>
2025-03-04 20:34:37 -0500

    Runtime: 0 ms (100.00%), Memory: 17.8 MB (30.45%) - Gitcode

--- 2579-count-total-number-of-colored-cells/2579. Count Total Number of Colored Cells.py
+++ 2579-count-total-number-of-colored-cells/2579. Count Total Number of Colored Cells.py
@@ -5,6 +5,12 @@ class Solution:
         # 3 -> 13 (8 boundary)
         # 4 -> 25 (12 boundary)
 
+        # 1, 1+4, 1+4+8, 1+4+8+12 (3 -> 4-1) 1 + 4(1+2+3)
+
+        # 1 + 4(1+2) 2(3)//2
+
         if n == 1: return 1
 
-        return self.coloredCells(n-1) + 4 * (n-1)
+        return 1 + 4 * (((n-1) * (n)) // 2)
+
+        # 1+2+3 -> 
