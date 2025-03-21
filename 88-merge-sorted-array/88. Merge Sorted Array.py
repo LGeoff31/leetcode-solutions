db0eca08a98158b9a83dc94945b75d4caa7e8d74
Geoffrey Lee <geoffrey31415@gmail.com>
2025-03-20 23:38:53 -0400

    Runtime: 0 ms (100.00%), Memory: 17.7 MB (72.00%) - Gitcode

--- 88-merge-sorted-array/88. Merge Sorted Array.py
+++ 88-merge-sorted-array/88. Merge Sorted Array.py
@@ -4,31 +4,23 @@ class Solution:
         Do not return anything, modify nums1 in-place instead.
         """
         
-        m -= 1
-        n -= 1
-        curr_idx = n + m + 1
-
-        while m >= 0 and n >= 0:
-            if nums1[m] > nums2[n]:
-                nums1[curr_idx] = nums1[m]
-                m -= 1
+        i, j, k = m - 1, len(nums2) - 1, m+n-1
+        while i >= 0 and j >= 0:
+            if nums1[i] >= nums2[j]:
+                nums1[k] = nums1[i]
+                i -= 1
             else:
-                nums1[curr_idx] = nums2[n]
-                n -= 1
-            curr_idx -= 1
-        #fill in remains of non-finished array
-        while n >= 0:
-            nums1[curr_idx] = nums2[n]
-            n -= 1
-            curr_idx -= 1
+                nums1[k] = nums2[j]
+                j -= 1
+            k -= 1
         
-
-            
-
-
-
-
-
-
-
-
+        # Clean up
+        while i >= 0:
+            nums1[k] = nums1[i]
+            i -= 1
+            k -= 1
+        while j >= 0:
+            nums1[k] = nums2[j]
+            j -= 1
+            k -= 1
+        return nums1
