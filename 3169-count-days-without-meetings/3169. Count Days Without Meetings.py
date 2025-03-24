5d600d050794d609247bf733780315860cee5f8b
Geoffrey Lee <geoffrey31415@gmail.com>
2025-03-24 00:34:58 -0400

    Runtime: 179 ms (56.93%), Memory: 53 MB (30.73%) - Gitcode

--- 3169-count-days-without-meetings/3169. Count Days Without Meetings.py
+++ 3169-count-days-without-meetings/3169. Count Days Without Meetings.py
@@ -1,25 +1,17 @@
 class Solution:
     def countDays(self, days: int, meetings: List[List[int]]) -> int:
+        stack = []
         meetings.sort()
-        stack = [meetings[0]]
-
-        for i in range(1, len(meetings)):
-            start, end = meetings[i][0], meetings[i][1]
-            if start <= stack[-1][1]:
-                a,b = stack.pop()
-                stack.append([a, max(end, b)])
+        for start, end in meetings:
+            if not stack:
+                stack.append((start, end))
             else:
-                stack.append([start, end])
-            print(stack)
-        print(stack)
-        res = 0
-        #start
-        res += stack[0][0] - 1
-        #end
-        res += days - stack[-1][1]
-
-        for i in range(1, len(stack)):
-            res += stack[i][0] - stack[i-1][1] - 1
-
-        return res
-        
+                if start <= stack[-1][1]:
+                    s,e = stack.pop()
+                    stack.append((s, max(e, end)))
+                else:
+                    stack.append((start, end))
+        complement_days = 0
+        for start, end in stack:
+            complement_days += end-start+1
+        return days - complement_days
