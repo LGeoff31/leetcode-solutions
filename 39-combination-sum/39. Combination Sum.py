4f91c61d54a132787e80bdf11af4fe64bd7afef8
Geoffrey Lee <geoffrey31415@gmail.com>
2025-04-02 15:04:06 -0400

    Runtime: 1261 ms (5.00%), Memory: 16.7 MB (100.00%) - Gitcode

--- 39-combination-sum/39. Combination Sum.py
+++ 39-combination-sum/39. Combination Sum.py
@@ -2,18 +2,20 @@ class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
         res = []
 
-        def dfs(i, cur, total):
-            if total == target:
-                res.append(cur.copy())
+        def dfs(num, lst):
+            #base cases
+            if num > target:
                 return
-            if i >= len(candidates) or total > target:
-                return
-            candidates[i]
-            cur.append(candidates[i])
-            dfs(i, cur, total + candidates[i])
-            cur.pop()
-            dfs(i+1, cur, total)
-        dfs(0, [], 0)
+            if num == target:
+                lst.sort()
+                if lst not in res:
+                    res.append(lst)
+            #recursion
+            for num in candidates:
+                dfs(sum(lst)+num, lst + [num])
+
+        for num in candidates:
+            dfs(num, [num])
         return res
 
         
