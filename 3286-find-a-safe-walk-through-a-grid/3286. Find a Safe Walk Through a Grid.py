d5740005615298aff2a70c35eb7f8303f5fd6c89
Geoffrey Lee <geoffrey31415@gmail.com>
2025-04-04 18:52:27 -0400

    Runtime: 103 ms (73.87%), Memory: 18.3 MB (61.56%) - Gitcode

--- 3286-find-a-safe-walk-through-a-grid/3286. Find a Safe Walk Through a Grid.py
+++ 3286-find-a-safe-walk-through-a-grid/3286. Find a Safe Walk Through a Grid.py
@@ -1,27 +1,20 @@
 class Solution:
     def findSafeWalk(self, grid: List[List[int]], health: int) -> bool:
-        queue = deque([[0,0]])
         rows, cols = len(grid), len(grid[0])
-
-
-        directions = [(-1, 0), (1, 0), (0, 1), (0, -1)]
-        dictionary = {(0, 0) : health - grid[0][0]}
-
-        while queue:
-            for i in range(len(queue)):
-                r,c = queue.popleft()
-
-                for dr, dc in directions:
-                    nr, nc = r+dr, c+dc
-                    if 0 <= nr < rows and 0 <= nc < cols:
-                        if (nr, nc) not in dictionary:
-                            dictionary[(nr, nc)] = dictionary[(r,c)] + (-1 if grid[nr][nc] == 1 else 0)
-                            queue.append([nr, nc])
-                        elif (nr, nc) in dictionary and dictionary[(r,c)] + (-1 if grid[nr][nc] == 1 else 0) > dictionary[(nr,nc)]:
-                            dictionary[(nr, nc)] = dictionary[(r,c)] + (-1 if grid[nr][nc] == 1 else 0)
-                            queue.append([nr, nc])
-
-        print(dictionary)
-        if (rows-1, cols-1) in dictionary and dictionary[(rows-1, cols-1)] > 0:
-            return True
+        maxHeap = []
+        heappush(maxHeap, (-1*(health - (grid[0][0] == 1)), 0, 0))
+        visited = set()
+        visited.add((0,0))
+        # print(maxHeap)
+        while maxHeap:
+            h, r, c = heappop(maxHeap)
+            h *= -1
+            if r == rows-1 and c == cols-1 and h > 0: return True
+            for new_r, new_c in [(r-1, c), (r+1, c), (r, c+1), (r,c-1)]:
+                if 0<=new_r<rows and 0<=new_c<cols and (new_r, new_c) not in visited:
+                    visited.add((new_r, new_c))
+                    is_one = grid[new_r][new_c] == 1
+                    heappush(maxHeap, (-(h-is_one), new_r, new_c))
+            # print(maxHeap)
         return False
+                 
