19ed6bcdc6a9d284979d75acb7131ac2994d9077
Geoffrey Lee <geoffrey31415@gmail.com>
2025-04-05 21:39:22 -0400

    Runtime: 148 ms (8.01%), Memory: 26.9 MB (77.80%) - Gitcode

--- 121-best-time-to-buy-and-sell-stock/121. Best Time to Buy and Sell Stock.py
+++ 121-best-time-to-buy-and-sell-stock/121. Best Time to Buy and Sell Stock.py
@@ -1,23 +1,10 @@
 class Solution:
-    def noProfit(self, prices):
-        for i in range(len(prices)-1):
-            if prices[i] < prices[i+1]:
-                return False
-        return True
-
     def maxProfit(self, prices: List[int]) -> int:
-        if self.noProfit(prices):
-            return 0 #O(N) => funciton scans through prices
-        max_profit = -99999
-        #kandaine algorithm
-        l, r = 0, 1
-        while r < len(prices):
-            if prices[r] - prices[l] < 0: #negative profit
-                l = r
-                r += 1
-            else:
-                profit = prices[r] - prices[l]
-                max_profit = max(max_profit, profit)
-                r += 1
-        return max_profit
+        min_price = 1e9
+        res = 0
+        for i in range(len(prices)):
+            min_price = min(min_price, prices[i])
+            res = max(res, prices[i] - min_price)
 
+
+        return res
