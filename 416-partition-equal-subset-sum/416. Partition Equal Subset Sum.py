149973c684fd587090ca9659055f83756a5991c7
Geoffrey Lee <geoffrey31415@gmail.com>
2025-04-06 20:22:41 -0400

    Runtime: 668 ms (53.28%), Memory: 19.2 MB (62.21%) - Gitcode

--- 416-partition-equal-subset-sum/416. Partition Equal Subset Sum.py
+++ 416-partition-equal-subset-sum/416. Partition Equal Subset Sum.py
@@ -1,14 +1,51 @@
 class Solution:
     def canPartition(self, nums: List[int]) -> bool:
-        if sum(nums) % 2 != 0: return False
-        target = sum(nums) // 2
-        # 100 * 200 = 20,000 which is pretty small lol
-        @cache
-        def dfs(i, currSum): #O(n^)
-            if currSum == target:
-                return True
-            if i == len(nums) or currSum > target:
-                return False
+        totalSum = sum(nums)
+        if totalSum % 2 == 1: return False
+        goal = totalSum // 2
 
-            return dfs(i+1, currSum + nums[i]) or dfs(i+1, currSum)
-        return dfs(0, 0)
+        # # Find a subset that sums to goal
+        # @cache
+        # def dfs(idx, currSum): #O(2*10^4 * 2*10^2)
+        #     if currSum > goal:
+        #         return False
+        #     if currSum == goal:
+        #         return True
+        #     if idx >= len(nums):
+        #         return False
+            
+        #     # # TAKE
+        #     # if dfs(idx+1, currSum + nums[idx]): return True
+        #     # # DONT TAKE
+        #     # if dfs(idx+1, currSum): return True
+        #     return dfs(idx+1, currSum + nums[idx]) or dfs(idx+1, currSum)
+            
+        # return dfs(0 ,0)
+
+        # nums.sort()
+        # curr = 0
+        # for num in nums:
+        #     curr += num
+        #     if curr == goal:
+        #         return True
+        #     elif curr > goal:
+        #         return False
+        # return False
+        # if sum(nums) % 2 == 1: return False
+
+        targetSum = sum(nums) // 2
+        # #Time: O(n^2)
+        # #Memory: O(n)
+        dp = set()
+        dp.add(0)
+        for i in range(len(nums)):
+            new_dp = set()
+            for elem in dp:
+                new_dp.add(elem)
+                if elem + nums[i] <= targetSum:
+                    new_dp.add(elem + nums[i])
+            dp = new_dp
+        return targetSum in dp
+
+
+
