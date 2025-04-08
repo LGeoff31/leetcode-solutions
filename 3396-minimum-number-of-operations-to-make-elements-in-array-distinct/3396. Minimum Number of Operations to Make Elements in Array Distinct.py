eb83817c1a9787441cc56c783862ccbdd95bf781
Geoffrey Lee <geoffrey31415@gmail.com>
2025-04-07 20:28:22 -0400

    Runtime: 3 ms (69.84%), Memory: 17.8 MB (73.67%) - Gitcode

--- 3396-minimum-number-of-operations-to-make-elements-in-array-distinct/3396. Minimum Number of Operations to Make Elements in Array Distinct.py
+++ 3396-minimum-number-of-operations-to-make-elements-in-array-distinct/3396. Minimum Number of Operations to Make Elements in Array Distinct.py
@@ -1,24 +1,9 @@
 class Solution:
     def minimumOperations(self, nums: List[int]) -> int:
-        dic = Counter(nums)
-        count = 0
-        size = len(nums)
-        lst = deque(nums)
-        while len(dic) != size:
-            if size < 3:
-                count += 1
-                break 
-            else:
-                a = lst.popleft()
-                b = lst.popleft()
-                c = lst.popleft()
-                dic[a] -= 1
-                if dic[a] == 0: del dic[a]
-                dic[b] -= 1
-                if dic[b] == 0: del dic[b]
-                dic[c] -= 1
-                if dic[c] == 0: del dic[c]
-                size -= 3
-            count += 1
-
-        return count
+        res = 0
+        while True:
+            if len(nums) == len(set(nums)):
+                break
+            nums = nums[3:]
+            res += 1
+        return res
