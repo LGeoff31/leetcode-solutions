e46c986f36eecfa7f3061238da559d8c970111db
Geoffrey Lee <geoffrey31415@gmail.com>
2025-04-18 21:42:42 -0400

    Runtime: 428 ms (32.76%), Memory: 32.7 MB (5.14%) - Gitcode

--- 2563-count-the-number-of-fair-pairs/2563. Count the Number of Fair Pairs.py
+++ 2563-count-the-number-of-fair-pairs/2563. Count the Number of Fair Pairs.py
@@ -7,7 +7,7 @@ class Solution:
         for i in range(len(nums)):
             s,e = lower - nums[i], upper - nums[i]
             s_idx, e_idx = bisect_left(nums, s), bisect_right(nums, e)
-            # if e_idx <= i: continue
+            if e_idx <= i: continue
             res += max(bisect_right(nums, e) - max(i,s_idx) - (s <= nums[i] <= e),0)
             print(res)
         return res
