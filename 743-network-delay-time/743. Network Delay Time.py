e00705d4b5af1a9c3113c1e1ff8bfbeebfd0dec7
Geoffrey Lee <geoffrey31415@gmail.com>
2025-04-19 16:02:32 -0400

    Runtime: 361 ms (97.37%), Memory: 18.7 MB (99.99%) - Gitcode

--- 743-network-delay-time/743. Network Delay Time.py
+++ 743-network-delay-time/743. Network Delay Time.py
@@ -1,25 +1,24 @@
 class Solution:
     def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
-        minHeap = [(0, k)]
-        res = {k:0} # {2: 0}
-        adj = defaultdict(list) # node : [(nei, weight), ...]
-        for u,v, w in times:
-            adj[u].append((v, w))
-        # print(adj)
-        while minHeap:
-            weight, node = heappop(minHeap)
-            print('weight', weight, 'node', node)
-            # res[node] = weight
-            for nei, w in adj[node]:
-                if nei not in res:
-                    heappush(minHeap, (w+weight, nei))
-                    res[nei] = w+weight
-                else:
-                    if w+weight < res[nei]:
-                        res[nei] = w+weight
-                        heappush(minHeap, (w+weight, nei))
-        print(res)
-        if len(res) != n:
-            return -1
-        return max(res.values())
+        res = 0
 
+        graph = defaultdict(list)
+        for u, v, t in times:
+            graph[u].append((v, t))
+
+        h = []
+        heapq.heappush(h, (0, k))
+
+        times = [1e9] * n
+        times[k-1] = 0
+        while len(h) > 0:
+            t, node = heapq.heappop(h)
+
+            if t > times[node-1]: continue
+            for new_pos, new_time in graph[node]:
+                if t + new_time < times[new_pos-1]:
+                    times[new_pos-1] = t + new_time
+                    heapq.heappush(h, (t+new_time, new_pos))
+        print(times)
+        return max(times) if max(times) != 1e9 else -1
+        
