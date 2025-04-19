3f69247cf6f65ec0816ae4e0bd519e5e22389304
Geoffrey Lee <geoffrey31415@gmail.com>
2025-04-19 16:02:04 -0400

    Runtime: 394 ms (35.85%), Memory: 19.6 MB (93.49%) - Gitcode

--- 743-network-delay-time/743. Network Delay Time.py
+++ 743-network-delay-time/743. Network Delay Time.py
@@ -1,17 +1,25 @@
 class Solution:
     def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
-        dic = defaultdict(list)
-        for u,v,w in times:
-            dic[u].append((w, v))
         minHeap = [(0, k)]
-        res = {k: 0}
-        print(dic)
+        res = {k:0} # {2: 0}
+        adj = defaultdict(list) # node : [(nei, weight), ...]
+        for u,v, w in times:
+            adj[u].append((v, w))
+        # print(adj)
         while minHeap:
             weight, node = heappop(minHeap)
+            print('weight', weight, 'node', node)
+            # res[node] = weight
+            for nei, w in adj[node]:
+                if nei not in res:
+                    heappush(minHeap, (w+weight, nei))
+                    res[nei] = w+weight
+                else:
+                    if w+weight < res[nei]:
+                        res[nei] = w+weight
+                        heappush(minHeap, (w+weight, nei))
+        print(res)
+        if len(res) != n:
+            return -1
+        return max(res.values())
 
-            for w, v in dic[node]:
-                if v in res and res[v] <= w+weight:
-                    continue
-                res[v] = w+weight
-                heappush(minHeap, (w+weight, v))
-        return max(res.values()) if len(res) == n else -1
