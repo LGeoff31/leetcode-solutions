30e3d38aa42d04b4ea9872b19e80d1424236560b
Geoffrey Lee <geoffrey31415@gmail.com>
2025-04-20 10:43:26 -0400

    Runtime: 3 ms (73.35%), Memory: 17.8 MB (63.02%) - Gitcode

--- 746-min-cost-climbing-stairs/746. Min Cost Climbing Stairs.py
+++ 746-min-cost-climbing-stairs/746. Min Cost Climbing Stairs.py
@@ -1,12 +1,8 @@
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
-        dp = [0] * len(cost)
-        dp[-1] = cost[-1]
-        dp[-2] = cost[-2]
-      
-
-        for i in range(len(cost)-3, -1, -1):
-            dp[i] = cost[i] + min(dp[i+1], dp[i+2])
-        return min(dp[0], dp[1])
-        
-
+        dp = [1e9] * (len(cost))
+        dp[0] = 0
+        dp[1] = 0
+        for i in range(2, len(cost)):
+            dp[i] = min(cost[i-1]+dp[i-1], cost[i-2]+dp[i-2])
+        return min(cost[-2] + dp[-2], dp[-1] + cost[-1])
