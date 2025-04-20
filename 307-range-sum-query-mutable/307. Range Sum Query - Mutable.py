ff8f78e453af034cb33db5f953106965394e8031
Geoffrey Lee <geoffrey31415@gmail.com>
2025-04-20 16:38:55 -0400

    Runtime: 489 ms (73.16%), Memory: 36.5 MB (75.07%) - Gitcode

--- 307-range-sum-query-mutable/307. Range Sum Query - Mutable.py
+++ 307-range-sum-query-mutable/307. Range Sum Query - Mutable.py
@@ -1,50 +1,45 @@
 class SegmentTree:
-    def __init__(self, n):
-        self.n = n
-        self.total = defaultdict(int)
-    
-    def update(self, o, l, r, idx ,val):
-        if l == r:
-            self.total[o] = val
-            return
-        mid = (l+r) // 2
-        if idx <= mid:
-            self.update(2*o, l, mid, idx, val)
-        else:
-            self.update(2*o+1, mid+1, r, idx, val)
-        self.total[o] = self.total[2*o] + self.total[2*o + 1] # I want self.total[0] to store the sum of that node
-        return
-    
-    def query(self, o, l, r, left, right): 
-        # Out of query range
-        if r < left or l > right:
-            return 0
-        
-        # Completely inside query range
-        if l >= left and r <= right:
-            return self.total[o]
+    def __init__(self, nums):
+        self.n = len(nums)
+        self.trees = [0] * (2*self.n) # Note that the first element is a blank??????
 
-        #Partial inside
-        mid = (l+r) // 2
-        res = 0
-        if left <= mid:
-            res += self.query(2*o, l, mid, left, right)
-        if mid+1 <= right:
-            res += self.query(2*o+1, mid+1, r, left, right)
-        return res
+        # Populate the leaf nodes
+        for i in range(self.n):
+            self.trees[i+self.n] = nums[i]
+        # Populate remainder tree
+        for i in range(self.n - 1, 0, -1):
+            self.trees[i] = self.trees[2*i] + self.trees[2*i+1]
+
 
 class NumArray:
     def __init__(self, nums: List[int]):
+        self.segmentTree = SegmentTree(nums)
         self.n = len(nums)
-        self.tree = SegmentTree(self.n)
-        self.nums = nums
-        for i in range(len(nums)):
-            self.tree.update(1, 1, self.n, i+1, nums[i])
 
     def update(self, index: int, val: int) -> None:
-        self.nums[index] = val
-        self.tree.update(1, 1, self.n, index+1, val)
+        trees = self.segmentTree.trees
+        pos = index + self.n
+        trees[pos] = val
+        # Update the tree accordingly
+        while pos > 1:
+            pos //= 2
+            trees[pos] = trees[2*pos] + trees[2*pos+1]
 
-    def sumRange(self, left: int, right: int) -> int:
-        return self.tree.query(1, 1, self.n, left+1, right+1)
+    def sumRange(self, left: int, right: int) -> int:   
+        res = 0
+        left += self.n
+        right += self.n
+        trees = self.segmentTree.trees
+        while left <= right:
+            if left % 2 == 1:
+                res += trees[left]
+                left += 1
+            if right % 2 == 0:
+                res += trees[right]
+                right -= 1
+            left //= 2
+            right //= 2
+        return res
+
+
         
