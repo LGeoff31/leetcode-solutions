1625525f2377247514f6b356b002ea1240ceb7ad
Geoffrey Lee <geoffrey31415@gmail.com>
2025-04-20 12:59:10 -0400

    Runtime: 0 ms (100.00%), Memory: 19 MB (34.53%) - Gitcode

--- 1-two-sum/1. Two Sum.py
+++ 1-two-sum/1. Two Sum.py
@@ -1,6 +1,9 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+        dic = {}
         for i in range(len(nums)):
-            for j in range(i+1, len(nums)):
-                if nums[i] + nums[j] == target:
-                    return [i,j]
+            compl = target - nums[i]
+            if compl in dic:
+                return [i,dic[compl]]
+
+            dic[nums[i]] = i
