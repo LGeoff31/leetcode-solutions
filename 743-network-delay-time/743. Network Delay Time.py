33c43a71d66fb82410045e64d2c5faecbbcba879
Geoffrey Lee <geoffrey31415@gmail.com>
2025-04-26 18:00:40 -0400

    Runtime: 465 ms (9.21%), Memory: 20 MB (22.34%) - Gitcode

--- 743-network-delay-time/743. Network Delay Time.py
+++ 743-network-delay-time/743. Network Delay Time.py
@@ -1,24 +1,20 @@
 class Solution:
-    def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
-        res = 0
-
-        graph = defaultdict(list)
-        for u, v, t in times:
-            graph[u].append((v, t))
-
-        h = []
-        heapq.heappush(h, (0, k))
-
-        times = [1e9] * n
-        times[k-1] = 0
-        while len(h) > 0:
-            t, node = heapq.heappop(h)
-
-            if t > times[node-1]: continue
-            for new_pos, new_time in graph[node]:
-                if t + new_time < times[new_pos-1]:
-                    times[new_pos-1] = t + new_time
-                    heapq.heappush(h, (t+new_time, new_pos))
-        print(times)
-        return max(times) if max(times) != 1e9 else -1
+    def networkDelayTime(self, edges: List[List[int]], n: int, k: int) -> int:
+        adj = defaultdict(list)
+        for u,v,w in edges:
+            adj[u].append((v,w))
+            
+        minHeap = [(0, k)] # (weight, node)
+        dist = {k: 0}
+            
+        while minHeap:
+            weight, node = heappop(minHeap)
+            for nei, nei_weight in adj[node]:
+                print(nei)
+                if nei in dist and dist[nei] <= weight + nei_weight:
+                    continue 
+                dist[nei] = weight + nei_weight 
+                heappush(minHeap, (weight + nei_weight, nei))
+        print(dist)
+        return max(dist.values()) if len(dist) == n else -1
         
