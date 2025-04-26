a263c99487c1abb7817d8b67848ecacd0d0fbdd4
Geoffrey Lee <geoffrey31415@gmail.com>
2025-04-25 23:44:31 -0400

    Runtime: 711 ms (5.23%), Memory: 31 MB (7.60%) - Gitcode

--- 2444-count-subarrays-with-fixed-bounds/2444. Count Subarrays With Fixed Bounds.py
+++ 2444-count-subarrays-with-fixed-bounds/2444. Count Subarrays With Fixed Bounds.py
@@ -1,17 +1,11 @@
 class Solution:
     def countSubarrays(self, nums: List[int], minK: int, maxK: int) -> int:
+        left, right, bad = -1, -1, -1
         res = 0
-        min_pos = max_pos = bad_pos = -1
-        
         for i, num in enumerate(nums):
-            if num < minK or num > maxK:
-                bad_pos = i  # reset bad index
-
-            if num == minK:
-                min_pos = i
-            if num == maxK:
-                max_pos = i
-
-            res += max(0, min(min_pos, max_pos) - bad_pos)
-        
+            if not minK <= num <= maxK:
+                bad = i
+            if num == minK: left = i
+            if num == maxK: right = i
+            res += max(0, min(left, right) - bad)
         return res
