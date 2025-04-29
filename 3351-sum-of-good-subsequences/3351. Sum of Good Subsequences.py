a81daef6f5fab65dd23c85bf24a3e9d8038b720b
Geoffrey Lee <geoffrey31415@gmail.com>
2025-04-29 13:13:06 -0400

    Runtime: 891 ms (47.90%), Memory: 29.7 MB (87.40%) - Gitcode

--- 3351-sum-of-good-subsequences/3351. Sum of Good Subsequences.py
+++ 3351-sum-of-good-subsequences/3351. Sum of Good Subsequences.py
@@ -3,26 +3,9 @@ class Solution:
         MOD = 10 ** 9 + 7
         dp_s = [0] * (max(nums) + 2)
         dp_c = [0] * (max(nums) + 2)
-        dp_s[nums[0]] = nums[0]
-        dp_c[nums[0]] = 1
-        # print(dp_s)
-        # print(dp_c)
-        # print()
-        for num in nums[1:]:
+        # dp_s[nums[0]] = nums[0]
+        # dp_c[nums[0]] = 1
+        for num in nums:
             dp_s[num] += num*(1+dp_c[num+1]+dp_c[num-1])  + dp_s[num+1] + dp_s[num-1]
             dp_c[num] += dp_c[num+1] + dp_c[num-1] + 1
-            # if dp_c[num+1] != 0:
-            #     dp_s[num] += (num*(1+dp_c[num+1]) + dp_s[num+1])
-            #     dp_c[num] += dp_c[num+1] + 1
-            # if dp_c[num-1] != 0:
-            #     dp_s[num] += (num*(1+dp_c[num-1]) + dp_s[num-1])
-            #     dp_c[num] += dp_c[num-1] + 1
-            # if dp_c[num+1] == 0 and dp_c[num-1] == 0:
-            #     dp_s[num] += num
-            #     dp_c[num] += 1
-
-            # dp_c[num] += dp_c[num-1] + dp_c[num+1]
-            # print(dp_s)
-            # print(dp_c)
-            # print()
         return sum(dp_s) % MOD
