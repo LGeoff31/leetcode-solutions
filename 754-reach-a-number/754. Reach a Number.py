1dfa3f19d2e3fd05ee74f074bbc5294709196645
Geoffrey Lee <geoffrey31415@gmail.com>
2025-05-01 16:07:07 -0400

    Runtime: 103 ms (6.08%), Memory: 17.9 MB (16.57%) - Gitcode

---
+++ 754-reach-a-number/754. Reach a Number.py
@@ -0,0 +1,28 @@
+class Solution:
+    def reachNumber(self, target: int) -> int:
+        # 5: 1+2+3+4+5 = 15, want subtract 10, 2(1+4)
+        def valid(mid):
+            currSum = mid * (mid + 1) // 2
+            if currSum < abs(target):
+                return False
+            diff = currSum - target
+            if diff % 2 == 1:
+                return False
+            # if diff > currSum:
+            #     return False
+            return True
+        for i in range(1, 10 ** 5):
+            if valid(i):
+                return i
+        # l, r = 0, 10 ** 5
+        # moves = 1e9
+        # while l <= r:
+        #     mid = (l + r) // 2
+        #     print('trying', mid)
+        #     if valid(mid):
+        #         print('got', mid)
+        #         moves = min(moves, mid)
+        #         r = mid - 1
+        #     else:
+        #         l = mid + 1
+        return -1
