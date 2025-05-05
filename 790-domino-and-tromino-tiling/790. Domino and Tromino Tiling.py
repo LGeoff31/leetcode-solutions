dac31a9db0c70d241dbe82d947f572a40a44dddd
Geoffrey Lee <geoffrey31415@gmail.com>
2025-05-04 23:31:54 -0400

    Runtime: 419 ms (5.01%), Memory: 19.3 MB (14.74%) - Gitcode

--- 790-domino-and-tromino-tiling/790. Domino and Tromino Tiling.py
+++ 790-domino-and-tromino-tiling/790. Domino and Tromino Tiling.py
@@ -1,17 +1,20 @@
 class Solution:
     def numTilings(self, n: int) -> int:
-        if n == 1: return 1
-        if n == 2: return 2
-        if n == 3: return 5
-        dp = [0] * n
-        dp[0] = 1
-        dp[1] = 2
-        dp[2] = 5
-        for i in range(3, n):
-            dp[i] = 2*dp[i-1] + dp[i-3]
-        return dp[-1] % (10**9 + 7)
-
-
-
-
-        
+        MOD = 10 ** 9 + 7
+        @cache
+        def dp(n):
+            if n < 0:
+                return 0
+            if n == 1 or n == 0:
+                return 1
+            if n == 2:
+                return 2
+            res = 0
+            for i in range(1, n + 1):
+                if i == 1 or i == 2:
+                    res += dp(n-i)
+                else:
+                    res += 2*dp(n-i)
+            return res % MOD             
+            # return dp(n-1) + dp(n-2) + 2*dp(n-3) + 2*dp(n-4) + 2*dp(n-5)
+        return dp(n)
