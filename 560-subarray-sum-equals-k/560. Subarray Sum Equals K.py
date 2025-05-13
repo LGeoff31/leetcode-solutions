0244536860bdaf90b389e4fc84256edd37b1f616
Geoffrey Lee <geoffrey31415@gmail.com>
2025-05-13 16:19:54 -0400

    Runtime: 39 ms (38.16%), Memory: 20.4 MB (39.03%) - Gitcode

--- 560-subarray-sum-equals-k/560. Subarray Sum Equals K.py
+++ 560-subarray-sum-equals-k/560. Subarray Sum Equals K.py
@@ -1,16 +1,16 @@
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
-        prefix = list(accumulate(nums))
-        dic = defaultdict(int)
+        dic = defaultdict(int) # Prefix Sum : Frequency
+        dic[0] = 1
+        accumulate = 0
         res = 0
-        for i in range(len(nums)):
-            currSum = prefix[i]
-            if currSum == k:
-                res += 1
+        for i, num in enumerate(nums):
+            accumulate += num
 
-            target = k - currSum
-            if target in dic:
-                res += dic[target]
-            dic[-currSum] += 1
+            target_value = accumulate - k
+            if target_value in dic:
+                res += dic[target_value]
+                
+            dic[accumulate] += 1
+
         return res
-
