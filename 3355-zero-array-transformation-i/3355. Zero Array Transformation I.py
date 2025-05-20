16500b9afbf99e023de91176ae224ca8e1669ead
Geoffrey Lee <geoffrey31415@gmail.com>
2025-05-19 21:32:23 -0400

    Runtime: 87 ms (49.85%), Memory: 54.9 MB (70.61%) - Gitcode

--- 3355-zero-array-transformation-i/3355. Zero Array Transformation I.py
+++ 3355-zero-array-transformation-i/3355. Zero Array Transformation I.py
@@ -1,11 +1,11 @@
 class Solution:
     def isZeroArray(self, nums: List[int], queries: List[List[int]]) -> bool:
-        prefix = [0] * len(nums)
-        for l,r in queries:
-            prefix[l] -= 1
-            if r+1 < len(prefix):
-                prefix[r+1] += 1
+        prefix = [0] * (len(nums) + 1)
+        for l, r in queries:
+            prefix[l] += 1
+            prefix[r + 1] -= 1
         prefix = list(accumulate(prefix))
+        res = True
         for i in range(len(nums)):
-            nums[i] += prefix[i]
-        return sum(num > 0 for num in nums) == 0
+            res = res and nums[i] <= prefix[i]
+        return res
