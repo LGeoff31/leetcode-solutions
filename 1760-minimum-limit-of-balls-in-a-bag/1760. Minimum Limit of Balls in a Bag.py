71b6147118b5a7ca76be7e5cda77e046ffad966f
Geoffrey Lee <geoffrey31415@gmail.com>
2025-05-22 19:32:16 -0400

    Runtime: 1087 ms (5.17%), Memory: 29.8 MB (19.52%) - Gitcode

--- 1760-minimum-limit-of-balls-in-a-bag/1760. Minimum Limit of Balls in a Bag.py
+++ 1760-minimum-limit-of-balls-in-a-bag/1760. Minimum Limit of Balls in a Bag.py
@@ -1,20 +1,31 @@
 class Solution:
     def minimumSize(self, nums: List[int], maxOperations: int) -> int:
-        n = len(nums)
-        l, r = 1, max(nums)
-        def valid(mid):
-            if mid == 0: return True
-            res = 0
-            for num in nums:
-                res += ceil(num/mid)
-            return res <= maxOperations + n
+        l, r = 1, 1e9
         res = 1e9
+        def valid(val):
+            if val == 0: return True
+            # we want all the values of nums ot be <= val
+            operations = 0
+            for n in nums:
+                if n > val:
+                    operations += ceil(n/val)-1
+            return operations <= maxOperations
         while l <= r:
             mid = (l + r) // 2
-            print(mid)
+            print('trying mid', mid)
             if valid(mid):
                 res = min(res, mid)
                 r = mid - 1
             else:
                 l = mid + 1
         return int(res)
+
+        # 2 4 8 2
+        # 4
+
+
+        # 9 -> 4, 5
+
+        #1000000000,1000000000,1000000000
+        #1000000000
+        #500000000
