07d57dcca5f384d9aa9f71165147e0a595659c31
Geoffrey Lee <geoffrey31415@gmail.com>
2025-05-22 21:29:24 -0400

    Runtime: 5362 ms (22.17%), Memory: 18.1 MB (20.96%) - Gitcode

--- 79-word-search/79. Word Search.py
+++ 79-word-search/79. Word Search.py
@@ -1,30 +1,26 @@
 
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
-        if len(board) == 1 and len(board[0]) == 1 and len(word) == 1 and word[0] == board[0][0]: return True
-        def dfs(r, c, idx, visited):
+        visited = set()
+        rows, cols = len(board), len(board[0])
+        def dfs(r,c, idx, visited):
             if idx == len(word):
                 return True
-            if r < 0 or r >= rows or c < 0 or c >= cols: return False
-            if (r,c) in visited: return False
-            if board[r][c] != word[idx]: return False
-            a = visited.copy()
-            a.add((r,c))
-            directions = [(-1, 0), (1,0), (0, 1), (0, -1)]
-            for dr, dc in directions:
-                if 0 <= r + dr < rows and 0 <= c + dc < cols:
-                    if dfs(r+dr, c+dc, idx+1, a): 
-                        return True
-
-            return False
-            
-        
-        rows, cols = len(board), len(board[0])
+            if not (0 <= r < rows and 0 <= c < cols) or (r,c) in visited:
+                return False
+            visited.add((r,c))
+            res = False
+            if board[r][c] == word[idx]:
+                res = dfs(r-1, c, idx + 1, visited) or dfs(r+1, c, idx + 1, visited) or dfs(r, c-1, idx + 1, visited) or dfs(r, c+1, idx + 1, visited)
+            visited.remove((r,c))
+            return res
 
         for r in range(rows):
             for c in range(cols):
-                if board[r][c] == word[0]:
-                    if dfs(r,c, 0, set()):
-                        return True
+                if dfs(r,c, 0, set()):
+                    return True
         return False
-       
+
+        # a b c e
+        # s f e s
+        # a d e e
