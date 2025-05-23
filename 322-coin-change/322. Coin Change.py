bb53091d3898d9fb90c590430ab94f293a895565
Geoffrey Lee <geoffrey31415@gmail.com>
2025-05-22 22:43:39 -0400

    Runtime: 807 ms (40.32%), Memory: 18.9 MB (31.75%) - Gitcode

--- 322-coin-change/322. Coin Change.py
+++ 322-coin-change/322. Coin Change.py
@@ -1,19 +1,13 @@
-from functools import lru_cache
-
-
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        @lru_cache(None)
-        def dfs(rem):
-            if rem < 0:
-                return -1
-            if rem == 0:
-                return 0
-            min_cost = float('inf')
-            for coin in coins:
-                res = dfs(rem - coin)
-                if res != -1:
-                    min_cost = min(min_cost, res + 1)
-            return min_cost if min_cost != float('inf') else -1
-
-        return dfs(amount)
+        if amount == 0:
+            return 0
+            
+        dp = [float('inf')] * (amount + 1)
+        dp[0] = 0
+        
+        for i in range(1, amount+1):
+            for n in coins:
+                if i-n >= 0: dp[i] = min(dp[i], 1 + dp[i-n])
+        print(dp)
+        return dp[amount] if dp[amount] != float('inf') else -1
