68ae33cd9880051ac4421818df7d7002966e3acb
Geoffrey Lee <geoffrey31415@gmail.com>
2025-06-05 14:59:38 -0400

    Runtime: 111 ms (15.99%), Memory: 24.4 MB (5.59%) - Gitcode

--- 227-basic-calculator-ii/227. Basic Calculator II.py
+++ 227-basic-calculator-ii/227. Basic Calculator II.py
@@ -1,19 +1,52 @@
 class Solution:
     def calculate(self, s: str) -> int: #2-3/2 -> 1
-        num, preSign, stack = 0, "+", []
-        for c in s+"+":
-            if c.isdigit():
-                num = num * 10 + int(c)
-            elif c in "+-*/":
-                if preSign == "+":
-                    stack.append(num)
-                elif preSign == "-":
-                    stack.append(-num)
-                elif preSign == "*":
-                    stack.append(stack.pop() * num)
-                elif preSign == "/":
-                    stack.append(math.trunc(stack.pop() / num))
-                
-                preSign = c
-                num = 0
-        return sum(stack)
+        """
+        Have a stack or both the numbers and operations
+        Whenever the operations is a / or *, apply that algorithm on the last two digits since its forced
+        At the end clean up via the + or - respectively
+        """
+        s = s.replace(" ", "")
+        print('s', s)
+        s += "/"
+        operations = []
+        numbers = []
+        i = 0
+        while i < len(s):
+            if s[i] == " ":
+                i += 1
+                continue
+            num = 0
+            # Extract the number    
+            while i < len(s) and s[i] not in "+/-*" and s[i] != " ":
+                num = 10 * num + int(s[i])
+                i += 1
+            numbers.append(num)
+            if operations and operations[-1] in "/*":
+                a = numbers.pop()
+                b = numbers.pop()
+                if operations[-1] == "/":
+                    numbers.append(b//a)
+                    operations.pop()
+                else:
+                    numbers.append(b*a)
+                    operations.pop()
+            if s[i] != " ":
+                operations.append(s[i])
+            i += 1
+            # print(numbers)
+            # print(operations)
+        operations=operations[:-1]
+        # print()
+        # print(numbers)
+        # print(operations)
+        idx = 0
+        res = numbers[0] 
+        while idx < len(operations):
+            if operations[idx] == "+":
+                res += numbers[idx + 1]
+            else:
+                res += -numbers[idx + 1]
+            idx += 1
+           
+        
+        return res
