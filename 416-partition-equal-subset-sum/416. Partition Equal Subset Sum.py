0661ff770c539ebcc1254b1cf89c5dd0f1af33c5
Geoffrey Lee <geoffrey31415@gmail.com>
2025-06-05 16:43:30 -0400

    Runtime: 1706 ms (17.54%), Memory: 501.5 MB (5.03%) - Gitcode

--- 416-partition-equal-subset-sum/416. Partition Equal Subset Sum.py
+++ 416-partition-equal-subset-sum/416. Partition Equal Subset Sum.py
@@ -1,51 +1,14 @@
 class Solution:
     def canPartition(self, nums: List[int]) -> bool:
-        totalSum = sum(nums)
-        if totalSum % 2 == 1: return False
-        goal = totalSum // 2
+        if sum(nums) % 2:
+            return False 
+        target_sum = sum(nums) // 2
 
-        # # Find a subset that sums to goal
-        # @cache
-        # def dfs(idx, currSum): #O(2*10^4 * 2*10^2)
-        #     if currSum > goal:
-        #         return False
-        #     if currSum == goal:
-        #         return True
-        #     if idx >= len(nums):
-        #         return False
+        @cache
+        def dfs(i, currSum):
+            if i == len(nums):
+                return currSum == target_sum
             
-        #     # # TAKE
-        #     # if dfs(idx+1, currSum + nums[idx]): return True
-        #     # # DONT TAKE
-        #     # if dfs(idx+1, currSum): return True
-        #     return dfs(idx+1, currSum + nums[idx]) or dfs(idx+1, currSum)
-            
-        # return dfs(0 ,0)
-
-        # nums.sort()
-        # curr = 0
-        # for num in nums:
-        #     curr += num
-        #     if curr == goal:
-        #         return True
-        #     elif curr > goal:
-        #         return False
-        # return False
-        # if sum(nums) % 2 == 1: return False
-
-        targetSum = sum(nums) // 2
-        # #Time: O(n^2)
-        # #Memory: O(n)
-        dp = set()
-        dp.add(0)
-        for i in range(len(nums)):
-            new_dp = set()
-            for elem in dp:
-                new_dp.add(elem)
-                if elem + nums[i] <= targetSum:
-                    new_dp.add(elem + nums[i])
-            dp = new_dp
-        return targetSum in dp
-
-
-
+            return dfs(i+1, currSum + nums[i]) or dfs(i+1, currSum)    
+        
+        return dfs(0, 0)
