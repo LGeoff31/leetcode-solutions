10b65117532a9e84e97d33097e54c6df5d91dc21
Geoffrey Lee <geoffrey31415@gmail.com>
2025-06-05 15:04:19 -0400

    Runtime: 298 ms (14.54%), Memory: 37 MB (6.03%) - Gitcode

--- 200-number-of-islands/200. Number of Islands.py
+++ 200-number-of-islands/200. Number of Islands.py
@@ -1,20 +1,23 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        visited = set()
         rows, cols = len(grid), len(grid[0])
         count = 0
-        direction = [(0,1), (0,-1), (1,0), (-1, 0)]
+        visited = set()
         def dfs(r,c):
+            if (r,c) in visited:
+                return
             visited.add((r,c))
-            if 0 <= r < rows and 0 <= c < cols:
-                for dr, dc in direction:
-                    if 0<=r+dr<rows and 0<=c+dc<cols and (r+dr, c+dc) not in visited and grid[r+dr][c+dc] == "1":
-                        dfs(r+dr, c+dc)
+            if not (0 <= r < rows and 0 <= c < cols) or grid[r][c] == "0":
+                return
+            
+            for new_r, new_c in [(r-1, c), (r+1, c), (r, c-1), (r,c+1)]:
+                if (new_r, new_c) not in visited:
+                    dfs(new_r, new_c)
+            
 
         for r in range(rows):
             for c in range(cols):
-                if grid[r][c] == "1" and (r,c) not in visited:
+                if (r,c) not in visited and grid[r][c] == "1":
                     dfs(r,c)
                     count += 1
-                    # print(visited)
-        return count
+        return count 
