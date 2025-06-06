6ff763155dfd41ea2cb88e3ac4cc2c74d1f39c9a
Geoffrey Lee <geoffrey31415@gmail.com>
2025-06-05 22:06:59 -0400

    Runtime: 55 ms (32.14%), Memory: 30.4 MB (24.92%) - Gitcode

--- 41-first-missing-positive/41. First Missing Positive.py
+++ 41-first-missing-positive/41. First Missing Positive.py
@@ -1,13 +1,20 @@
 class Solution:
     def firstMissingPositive(self, nums: List[int]) -> int:   
-        lst = [num for num in nums if num > 0]
-        print(lst)
-        for i in range(len(lst)):
-            idx = abs(lst[i]) - 1
-            if idx < len(lst) and lst[idx] > 0:
-                lst[idx] *= -1
-        for i in range(len(lst)):
-            if lst[i] > 0:
-                return i + 1
-        return len(lst) + 1
-        
+        """
+        Negative numebrs do not imapct the result.
+        If you come across a positive value, mark the corresponding index as negative
+        Loop through nums, if you come across a positive value, that means it hasn't been seen before so return its value
+        """
+        # [1,3,4]
+        # Hey we're missing index 2
+        nums = [a for a in nums if a > 0]
+        n = len(nums) 
+        for i in range(n):
+            idx = abs(nums[i]) - 1
+            if idx < len(nums) and nums[idx] > 0:
+                nums[idx] *= -1
+
+        for i in range(len(nums)):
+            if nums[i] > 0:
+                return i+1
+        return len(nums) + 1
