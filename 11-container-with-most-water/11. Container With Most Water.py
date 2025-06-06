9f7827f554b234b20622c1a36dec71e74d01693c
Geoffrey Lee <geoffrey31415@gmail.com>
2025-06-05 22:32:04 -0400

    Runtime: 107 ms (38.78%), Memory: 28.5 MB (67.53%) - Gitcode

--- 11-container-with-most-water/11. Container With Most Water.py
+++ 11-container-with-most-water/11. Container With Most Water.py
@@ -1,11 +1,20 @@
 class Solution:
     def maxArea(self, height: List[int]) -> int:
+        """ 
+        height = [1,8,6,2,5,4,8,3,7]
+                        ^ ^         
+
+        Time complexity: O(n)
+        Space complexity: O(1)
+
+        """
+        global_maximum_area = 0
         l, r = 0, len(height) - 1
-        res = 0
         while l <= r:
-            res = max(res, min(height[l], height[r]) * (r-l))
-            if height[l] < height[r]:
+            global_maximum_area = max(global_maximum_area, (r-l) * min(height[l], height[r]))
+            if height[l] <= height[r]:
                 l += 1
             else:
                 r -= 1
-        return res
+        return global_maximum_area
+
