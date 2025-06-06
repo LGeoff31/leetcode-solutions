2016143251a5e5a6c91615a655125543529f69ef
Geoffrey Lee <geoffrey31415@gmail.com>
2025-06-05 23:53:23 -0400

    Runtime: 2360 ms (5.00%), Memory: 22 MB (6.46%) - Gitcode

--- 15-3sum/15. 3Sum.py
+++ 15-3sum/15. 3Sum.py
@@ -1,18 +1,31 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
+        a = []
+        dic = Counter(nums)
+        for key in dic:
+            if dic[key] > 3:
+                for i in range(3):
+                    a.append(key)
+            else:
+                for i in range(dic[key]):
+                    a.append(key)
+        nums = a
         res = []
-        visited = set()  #store tuples 
-        nums.sort()
-
+        visited = set()
         for i in range(len(nums)):
             target = -nums[i]
+            # Start two sum
             dic = {}
-            for j in range(i+1, len(nums)):
-                if target - nums[j] in dic:
-                    if (nums[i], nums[j], target-nums[j]) not in visited:
-                        res.append([nums[i], nums[j], target - nums[j]])
-                        visited.add((nums[i], nums[j], target-nums[j]))
-                else:
-                    dic[nums[j]] = j
-        
+            for j in range(len(nums)):
+                if i == j:
+                    continue
+                
+                if nums[j] in dic:
+                    a,b,c = nums[i], nums[j], dic[nums[j]]
+                    d,e,f = min(a,b,c), a+b+c-min(a,b,c)-max(a,b,c), max(a,b,c)
+                    if (d,e,f) not in visited:
+                        visited.add((d,e,f))
+                        res.append((d,e,f))
+                dic[target - nums[j]] = nums[j]
         return res
+
