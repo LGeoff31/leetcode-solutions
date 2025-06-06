9797b5ee885b7411c597bd2ceab34a5ac8037a2b
Geoffrey Lee <geoffrey31415@gmail.com>
2025-06-06 01:04:57 -0400

    Runtime: 95 ms (39.34%), Memory: 89.2 MB (36.28%) - Gitcode

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/105. Construct Binary Tree from Preorder and Inorder Traversal.py
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/105. Construct Binary Tree from Preorder and Inorder Traversal.py
@@ -6,11 +6,24 @@
 #         self.right = right
 class Solution:
     def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
-        if not preorder or not inorder:
-            return None
-        root = TreeNode(preorder[0])
-        mid = inorder.index(preorder[0])
-        root.left = self.buildTree(preorder[1:mid+1], inorder[:mid+1])
-        root.right = self.buildTree(preorder[mid+1:], inorder[mid+1:])
-        return root
-        
+        """
+        preorder -> current, left, right
+        inorder -> left, current, right
+
+        Observations:
+        - We know the roow will be the same as the first element in the inorder
+        - We can identify where the parent node is within the inorder array, everything to the left is on the left subtree, everything to right is right subtree
+        - Generate the tree recursively
+        """
+        def dfs(preorder, inorder):
+            if len(preorder) == 1:
+                return TreeNode(preorder[0])
+            if not preorder:
+                return None
+            parent = TreeNode(preorder[0])
+            split_idx = inorder.index(preorder[0]) # 1
+            parent.left = dfs(preorder[1 : split_idx + 1], inorder[:split_idx])
+            parent.right = dfs(preorder[split_idx + 1 : ], inorder[split_idx+1 : ])
+            return parent
+        return dfs(preorder, inorder)
+
