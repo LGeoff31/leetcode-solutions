f83abb9d95ec3450f5d76ae85123c6afc609be49
Geoffrey Lee <geoffrey31415@gmail.com>
2025-06-06 11:50:24 -0400

    Runtime: 40 ms (0.19%), Memory: 17 MB (100.00%) - Gitcode

--- 153-find-minimum-in-rotated-sorted-array/153. Find Minimum in Rotated Sorted Array.py
+++ 153-find-minimum-in-rotated-sorted-array/153. Find Minimum in Rotated Sorted Array.py
@@ -1,24 +1,16 @@
 class Solution:
-    def findMin(self, nums: List[int]) -> int:  
-        # The minimum element will be greater than left element and smaller than the right element
-        # If that doesn't exist, the minimum element will either be the first or last element
-        """
-        [4,5,6,7,8,9,10,11,12,0,1,2,3]
-        [1,2,3,4,5]
-        """
-        if len(nums) <= 2:
-            return min(nums)
-        l, r = 0, len(nums) - 1
-        while l <= r:
-            mid = (l + r) // 2
-            print('tryihng', mid)
-            # if mid == 0 or mid == len(nums) - 1:
-            #     return nums[mid]
-            
-            if (mid!=0 and mid!=len(nums)-1) and nums[mid] < nums[mid-1] and nums[mid] < nums[mid+1]:
-                return nums[mid]
-            elif nums[mid] > nums[-1]:
-                l = mid + 1
-            else:
-                r = mid - 1
-        return min(nums[0], nums[-1])
+    def findMin(self, nums: List[int]) -> int:
+        left, right = 0, len(nums) - 1
+        #about empty arrays or length 1
+        if len(nums) == 1:
+            return nums[0]
+        while True:
+            #perfectly partitioned scenario
+            partition = (left + right) // 2
+            if nums[0] <= nums[partition] and nums[partition+1] <= nums[-1]:
+                return min(nums[0], nums[partition+1])
+            elif nums[0] > nums[partition]: #left side needs partitioned
+                right = partition
+                pass
+            elif nums[partition+1] > nums[-1]: #right side needs partitioned
+                left = partition + 1
