c0259b3537774f33e81aa8f4c7fb45b0819dca3c
Geoffrey Lee <geoffrey31415@gmail.com>
2025-06-05 22:41:55 -0400

    Runtime: 0 ms (100.00%), Memory: 18 MB (22.91%) - Gitcode

--- 17-letter-combinations-of-a-phone-number/17. Letter Combinations of a Phone Number.py
+++ 17-letter-combinations-of-a-phone-number/17. Letter Combinations of a Phone Number.py
@@ -1,33 +1,37 @@
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
-        dic = {}
-        dic[2] = ["a","b","c"]
-        dic[3] = ["d","e","f"]
-        dic[4] = ["g","h","i"]
-        dic[5] = ["j","k","l"]
-        dic[6] = ["m","n","o"]
-        dic[7] = ["p","q","r", "s"]
-        dic[8] = ["t","u", "v"]
-        dic[9] = ["w","x", "y", "z"]
-        if not digits:
-            return []
-        res = []
-        curr_string = ""
-        def dfs(i, curr_string):
-            if i >= len(digits):
-                res.append(curr_string)
-                return
+        """
+        Assuming all digits mapped to 3 letters, the total amount of combinations in our output will be of length 3**(n), where n = len(digits)
+
+        digits = "23"
+
+        dfs(i, current_combination)
+        """
+
+        self.combinations = []
+        digit_to_letters = defaultdict(list)
+        offset = 0
+        for i in range(2, 10):
+            if i == 7 or i == 9:
+                for j in range(4):
+                    digit_to_letters[i].append(chr(95 + i + offset))
+                    offset += 1
+            else:
+                for j in range(3):
+                    digit_to_letters[i].append(chr(95 + i + offset))
+                    offset += 1
+            offset -= 1
             
-            for letter in dic[int(digits[i])]:
-                curr_string += letter
-                dfs(i+1, curr_string)
-                curr_string = curr_string[0:len(curr_string)-1]
-        
+        print(digit_to_letters)
+        def dfs(i, current_combination):
+            if i == len(digits):
+                if not current_combination: return
+                self.combinations.append(current_combination)
+                return
+
+            for letter in digit_to_letters[int(digits[i])]:
+                dfs(i+1, current_combination + letter)
+            
         dfs(0, "")
+        return self.combinations
 
-        return res
-
-
-
-
-        
