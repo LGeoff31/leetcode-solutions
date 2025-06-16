894e9e27c46f61e43bdf4d723bdad1531cbc5e23
Geoffrey Lee <geoffrey31415@gmail.com>
2025-06-15 20:05:42 -0400

    Runtime: 0 ms (100.00%), Memory: 11.9 MB (38.11%) - Gitcode

--- 2016-maximum-difference-between-increasing-elements/2016. Maximum Difference Between Increasing Elements.cpp
+++ 2016-maximum-difference-between-increasing-elements/2016. Maximum Difference Between Increasing Elements.cpp
@@ -2,12 +2,12 @@ class Solution {
 public:
     int maximumDifference(vector<int>& nums) {
         int res = -1;
+        int min_element = INT_MAX;
         for (int i = 0; i < nums.size(); i++) {
-            for (int j = i+1; j < nums.size(); j++) {
-                if (nums[j] > nums[i]) {
-                    res = max(res, nums[j] - nums[i]);
-                }
+            if (nums[i] > min_element) {
+                res = max(res, nums[i] - min_element);
             }
+            min_element = min(min_element, nums[i]);
         }
         return res;
     }
