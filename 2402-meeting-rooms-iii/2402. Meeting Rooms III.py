faac51e7b4258c229063fbb2e3b0cb8b90ee06d8
Geoffrey Lee <geoffrey31415@gmail.com>
2025-07-10 23:54:31 -0400

    Runtime: 928 ms (5.06%), Memory: 51.5 MB (68.69%) - Gitcode

--- 2402-meeting-rooms-iii/2402. Meeting Rooms III.py
+++ 2402-meeting-rooms-iii/2402. Meeting Rooms III.py
@@ -1,28 +1,64 @@
 class Solution:
     def mostBooked(self, n: int, meetings: List[List[int]]) -> int:
+        """
+        If empty room, place lowest meeting id
+        If no empty rooms, wait for room and put meeting with earliest start time
+        Example 1
+        1 | 1
+        t=5s
+        1 | 2
+        t=10s
+        2 | 2
+        return meeting 0
+
+        Simulation??
+        SortedList / min heap + cnt array
+        """
         meetings.sort()
-        
-        available = [i for i in range(n)]
-        used = [] #(end_time, room_number)
-        count = [0] * n
+        cnt = [0] * n
+        q = SortedList() # (end_time, room)
+        available_rooms = SortedList([i for i in range(n)])
+        i = 0
+        t = meetings[0][0]
+        print(meetings)
+        while i < len(meetings):
+            if len(available_rooms) == 0:
+                t = q[0][0]
+                available_rooms.add(q[0][1])
+                q.remove(q[0])
+            if not q:
+                t = meetings[i][0]
+                q.add((meetings[i][1], 0))
+                available_rooms.remove(0)
+                cnt[0] += 1
+                i+=1
+                # print(q)
+                # print(available_rooms)
+                # print(cnt)
+                # print()
+                continue
 
-        for start, end in meetings:
-            while used and start >= used[0][0]:
-                _, room = heapq.heappop(used)
-                heapq.heappush(available, room)
-            #no room is available
-            if not available:
-                end_time, room = heapq.heappop(used)
-                end = end_time + (end - start)
-                heapq.heappush(available, room)
+            while q and q[0][0] <= meetings[i][0]:
+                t = q[0][0]
+                available_rooms.add(q[0][1])
+                q.remove(q[0])
 
-
-            #a room is avaialble
-            room = heapq.heappop(available)
-            heapq.heappush(used, (end, room))
-            count[room] += 1
+            smallest_available_room = available_rooms[0]
+            if t > meetings[i][0]:
+                # Delay
+                duration = meetings[i][1] - meetings[i][0]
+                q.add((t+duration, smallest_available_room))
+                available_rooms.remove(smallest_available_room)
+                cnt[smallest_available_room] += 1
+            else:
+                t = meetings[i][0]
+                q.add((meetings[i][1], smallest_available_room))
+                available_rooms.remove(smallest_available_room)
+                cnt[smallest_available_room] += 1
+            # print(q)
+            # print(available_rooms)
+            # print(cnt)
+            # print()
+            i += 1
             
-            
-        return count.index(max(count))
-        
-        
+        return cnt.index(max(cnt))
