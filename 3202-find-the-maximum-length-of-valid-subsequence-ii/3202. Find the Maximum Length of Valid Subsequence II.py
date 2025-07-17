eb44de2f9700af698cfe47ea73502dccef92b095
Geoffrey Lee <geoffrey31415@gmail.com>
2025-07-16 21:40:45 -0400

    Runtime: 8271 ms (5.73%), Memory: 647.7 MB (5.74%) - Gitcode

--- 3202-find-the-maximum-length-of-valid-subsequence-ii/3202. Find the Maximum Length of Valid Subsequence II.py
+++ 3202-find-the-maximum-length-of-valid-subsequence-ii/3202. Find the Maximum Length of Valid Subsequence II.py
@@ -1,21 +1,47 @@
-class Solution:
-    def maximumLength(self, nums: List[int], k: int) -> int:
-        # Why not MOD all numbers by k????
-        for i in range(len(nums)):
-            nums[i] = nums[i] % k
-        
-        # Dp[i] = max length of subsequence with last element x such that x % k == i
-        # (CurrNum + prevNum) % k == mod -> (prevNum + k - CurrNum) % k
-        
-        n = len(nums)
-        dp = [[0] * k for _ in range(k)]
+from collections import defaultdict
+from functools import cache
+from bisect import bisect_left
 
-        for num in nums:
-            currNum = num % k
-            for mod in range(k):
-                prevNum = (mod - currNum + k)%k
-                dp[currNum][mod] = max(dp[currNum][mod], 1 + dp[prevNum][mod])
+class Solution:
+    def maximumLength(self, nums: list[int], k: int) -> int:
+        n = len(nums)
+        
+        # Step 1: Reduce nums mod k
+        for i in range(n):
+            nums[i] %= k
+        
+        # Step 2: Group indices by mod value
+        dic = defaultdict(list)
+        for i, val in enumerate(nums):
+            dic[val].append(i)
+
+        # Step 3: Try all pair (a + b) % k values
+        pair_sums = set()
+        for i in range(n):
+            for j in range(i + 1, n):
+                pair_sums.add((nums[i] + nums[j]) % k)
+
+        @cache
+        def dfs(i: int, target: int, s: int) -> int:
+            if target not in dic:
+                return 1  # Can't continue the chain
+
+            idx_list = dic[target]
+            pos = bisect_left(idx_list, i)  # Find next valid index >= i
+
+            max_len = 1
+            while pos < len(idx_list):
+                j = idx_list[pos]
+                next_target = (s - nums[j]) % k
+                max_len = max(max_len, 1 + dfs(j + 1, next_target, s))
+                pos += 1
+
+            return max_len
+
         res = 0
-        for arr in dp:
-            res = max(res, max(arr))
-        return res
+        for s in pair_sums:
+            for i in range(n):
+                target = (s - nums[i]) % k
+                res = max(res, dfs(i + 1, target, s) + 1)  # +1 for nums[i] itself
+
+        return res-1
