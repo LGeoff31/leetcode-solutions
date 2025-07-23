fc4483c835e3ec46ffd01c09060b6202b690aa2d
Geoffrey Lee <geoffrey31415@gmail.com>
2025-07-22 20:29:26 -0400

    Runtime: 190 ms (81.93%), Memory: 19.7 MB (26.45%) - Gitcode

--- 1717-maximum-score-from-removing-substrings/1717. Maximum Score From Removing Substrings.py
+++ 1717-maximum-score-from-removing-substrings/1717. Maximum Score From Removing Substrings.py
@@ -1,32 +1,25 @@
 class Solution:
-    # count the target string
-    def cnt(self, s, target):
-        res = 0
-        st = []
-        for ch in s:
-            # if encounter the target string
-            # by combine current character with previous character
-            # in stack
-            if st and ch == target[1] and st[-1] == target[0]:
-                st.pop()
-                res += 1
-            else: 
-                st.append(ch)
-        # also return the remain string after delete target-string
-        return res, "".join(st)
-
     def maximumGain(self, s: str, x: int, y: int) -> int:
+        def remove_pattern(s, first, second, score):
+            stack = []
+            gain = 0
+            for c in s:
+                if stack and stack[-1] == first and c == second:
+                    stack.pop()
+                    gain += score
+                else:
+                    stack.append(c)
+            return "".join(stack), gain
         res = 0
+        if x > y:
+            s, gain = remove_pattern(s, 'a', 'b', x)
+            res += gain
+            _, gain = remove_pattern(s, 'b', 'a', y)
+            res += gain
+        else:
+            s, gain = remove_pattern(s, 'b', 'a', y)
+            res += gain
+            _, gain = remove_pattern(s, 'a', 'b', x)
+            res += gain
 
-        # decide which one is high-string 
-        # and the remain one is low-string
-        hs = "ba" if y > x else "ab"
-        ls = "ab" if hs == "ba" else "ba"
-
-        # first turn: count how many high-string
-        cnth, nexts = self.cnt(s, hs)
-
-        # second turn: count how many low-string
-        cntl, _ = self.cnt(nexts, ls)
-
-        return cnth * max(x, y) + cntl * min(x, y)
+        return res
