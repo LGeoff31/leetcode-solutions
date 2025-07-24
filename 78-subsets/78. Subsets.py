3be1b42f80b6c57153643495b2f2cfa51ddf6a29
Geoffrey Lee <geoffrey31415@gmail.com>
2025-07-23 23:31:02 -0400

    Runtime: 0 ms (100.00%), Memory: 18 MB (21.18%) - Gitcode

--- 78-subsets/78. Subsets.py
+++ 78-subsets/78. Subsets.py
@@ -1,12 +1,11 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        self.res = []
-        def dfs(i, arr):
-            if i == len(nums):
-                self.res.append(arr)
-                return
-            print(i)
-            dfs(i+1, arr+[nums[i]])
-            dfs(i+1, arr)
-        dfs(0, [])
-        return self.res
+        res = []
+        n = len(nums)
+        for mask in range(1 << n):
+            cur = []
+            for i in range(n):
+                if mask & (1 << i):
+                    cur.append(nums[i])
+            res.append(cur)
+        return res
