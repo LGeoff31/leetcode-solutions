e547742581878c0ed7078cb57d27215381847f68
Geoffrey Lee <geoffrey31415@gmail.com>
2025-07-28 11:35:41 -0400

    Runtime: 8 ms (23.61%), Memory: 19.1 MB (63.02%) - Gitcode

--- 207-course-schedule/207. Course Schedule.py
+++ 207-course-schedule/207. Course Schedule.py
@@ -1,29 +1,20 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool: 
-        preq = defaultdict(list)
+        indegree = [0] * numCourses
+        dic = defaultdict(list)
         for u, v in prerequisites:
-            preq[u].append(v) # You must take course v to take course u
-
-        cycle = set()
+            indegree[u] += 1
+            dic[v].append(u)
+        
         visited = set()
-
-        def dfs(course):
-            if course in cycle:
-                return False
-            if course in visited:
-                return True
-            cycle.add(course)
-
-            visited.add(course)
-            
-            for p in preq[course]:
-                if not dfs(p):
-                    return False
-            cycle.remove(course)
-            return True
-
-        for i in range(numCourses):
-            if not dfs(i):
-                return False
-        print(visited)
-        return numCourses == len(visited)
+        queue = deque([i for i in range(numCourses) if indegree[i] == 0])
+        # print(queue)
+        while queue:
+            node = queue.popleft()
+            if indegree[node] == 0:
+                visited.add(node)
+                for nei in dic[node]:
+                    indegree[nei] -= 1
+                    if indegree[nei] == 0:
+                        queue.append(nei)
+        return len(visited) == numCourses
