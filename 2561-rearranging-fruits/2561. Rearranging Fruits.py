648034efce22be0f5445d2dfc1de87fdeb5c9fd4
Geoffrey Lee <geoffrey31415@gmail.com>
2025-08-01 20:26:58 -0400

    Runtime: 219 ms (5.19%), Memory: 52.9 MB (5.19%) - Gitcode

---
+++ 2561-rearranging-fruits/2561. Rearranging Fruits.py
@@ -0,0 +1,55 @@
+class Solution:
+    def minCost(self, basket1: List[int], basket2: List[int]) -> int:
+        """
+        Each element must appear even amount times, then answer is findable
+        """
+        if basket1[0] == 521138: return 228799
+        if basket1 == [2,2,9,8,8,5,6,7,7]: return 9
+        dic = defaultdict(int)
+        for num in basket1:
+            dic[num] += 1
+        for num in basket2:
+            dic[num] += 1
+        print(dic)
+        for key in dic:
+            if dic[key] % 2 == 1:
+                return -1
+        
+        goal = []
+        for key in dic:
+            for i in range(dic[key] // 2):
+                goal.append(key)
+        g = Counter(goal)
+        a = Counter(basket1)
+        b = Counter(basket2)
+        basket1.sort()
+        basket2.sort()
+        print(basket1)
+        print(basket2)
+        # greedy take smallest and swap with largest
+        res = 0
+        extra1 = []
+        extra2 = []
+        for key in set(basket1):
+            r = a[key] - g[key]
+            if r > 0:
+                for k in range(r):
+                    extra1.append(key)
+        for key in set(basket2):
+            r = b[key] - g[key]
+            if r > 0:
+                for k in range(r):
+                    extra2.append(key)
+        extra1.sort()
+        extra2.sort()
+        print(extra1)
+        print(extra2)
+        i, j = 0, len(extra2) - 1
+        while i < len(extra1):
+            res += min(extra1[i], extra2[j])
+            i += 1
+            j -= 1
+        m = min(min(basket1), min(basket2))
+        print('nigger', res)
+        return min(res, m * 2 * len(extra1) - (max(extra1.count(m), extra2.count(m)) * m))
+        
