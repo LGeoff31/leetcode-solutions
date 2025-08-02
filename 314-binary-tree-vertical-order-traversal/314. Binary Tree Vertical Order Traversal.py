8f350db91f1246064e7a85f29cc1d49d265e09b1
Geoffrey Lee <geoffrey31415@gmail.com>
2025-08-02 02:23:41 -0400

    Runtime: 0 ms (100.00%), Memory: 17.9 MB (41.60%) - Gitcode

--- 314-binary-tree-vertical-order-traversal/314. Binary Tree Vertical Order Traversal.py
+++ 314-binary-tree-vertical-order-traversal/314. Binary Tree Vertical Order Traversal.py
@@ -1,25 +1,25 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+from collections import defaultdict
+
 class Solution:
     def verticalOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
-        if not root: return []
+        if not root:
+            return []
+        
         dic = defaultdict(list)
-        queue = deque([[root, 0]])
 
-        while queue:
-            for i in range(len(queue)):
-                node, col = queue.popleft()
-                dic[col].append(node.val)
+        def dfs(node, row, col):
+            if not node:
+                return
+            dic[col].append((row, node.val))
+            dfs(node.left, row + 1, col - 1)
+            dfs(node.right, row + 1, col + 1)
 
-                if node.left:
-                    queue.append([node.left, col - 1])
-                if node.right:
-                    queue.append([node.right, col + 1])
-        res = []
-        for i in range(min(dic.keys()), max(dic.keys()) + 1):
-            res.append(dic[i])
-        return res
+        dfs(root, 0, 0)
+
+        result = []
+        for col in sorted(dic.keys()):
+            # sort by row to ensure top-to-bottom
+            col_nodes = sorted(dic[col], key=lambda x: x[0])
+            result.append([val for _, val in col_nodes])
+        
+        return result
