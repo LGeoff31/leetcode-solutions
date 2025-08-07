018c8d12510e6d2e66cf9246916b4558d2fd5538
Geoffrey Lee <geoffrey31415@gmail.com>
2025-08-07 01:10:15 -0400

    Runtime: 173 ms (30.77%), Memory: 40.3 MB (9.64%) - Gitcode

--- 295-find-median-from-data-stream/295. Find Median from Data Stream.py
+++ 295-find-median-from-data-stream/295. Find Median from Data Stream.py
@@ -1,40 +1,29 @@
 class MedianFinder:
     def __init__(self):
-        # All elements in maxHeap <= all elements in minHeap
-        # abs(len(minHeap) - len(maxHeap)) <= 1
-        self.minHeap = []
-        self.maxHeap = []
+        self.minHeap = [] # Store later half of the array
+        self.maxHeap = [] # Store start half of the array
     
     def addNum(self, num: int) -> None: # O(logn)
-        # Base case
-        if not self.maxHeap:
-            heappush(self.maxHeap, -num)
-            return
+        # Want to ensure the two heaps are balanced or maxHeap has 1 more element
+        heappush(self.maxHeap, -num)
 
-        if len(self.maxHeap) > len(self.minHeap): # Greater by 1
-            heappush(self.minHeap, num)
-            if -self.maxHeap[0] > self.minHeap[0]:
-                self.swapValues()
-        elif len(self.minHeap) > len(self.maxHeap):
-            heappush(self.maxHeap, -num)
-            if -self.maxHeap[0] > self.minHeap[0]:
-                self.swapValues()
-        else:
-            if num <= self.minHeap[0]:
-                heappush(self.maxHeap, -num)
-            else:
-                heappush(self.minHeap, num)
-
+        if self.minHeap and self.maxHeap and (-self.maxHeap[0] > self.minHeap[0]):
+            val = -heappop(self.maxHeap)
+            heappush(self.minHeap, val)
+        
+        if len(self.maxHeap) > len(self.minHeap) + 1:
+            val = -heappop(self.maxHeap)
+            heappush(self.minHeap, val)
+        if len(self.minHeap) > len(self.maxHeap):
+            val = heappop(self.minHeap)
+            heappush(self.maxHeap, -val)
+        
     def findMedian(self) -> float: # O(1)
         if (len(self.minHeap) + len(self.maxHeap)) % 2 == 1:
-            return self.minHeap[0] if len(self.minHeap) > len(self.maxHeap) else -self.maxHeap[0]
+            return -self.maxHeap[0]
         return (-self.maxHeap[0] + self.minHeap[0]) / 2
-    
-    def swapValues(self):
-        temp1 = heappop(self.minHeap)
-        temp2 = -heappop(self.maxHeap)
-        heappush(self.maxHeap, -temp1)
-        heappush(self.minHeap, temp2)
+       
+       
         
 
 
