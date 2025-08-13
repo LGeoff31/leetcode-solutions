8e44c2e077b5b2cc59b3ccdc52d6ea61dd15eb17
Geoffrey Lee <geoffrey31415@gmail.com>
2025-08-13 15:10:27 -0400

    Runtime: 0 ms (100.00%), Memory: 18 MB (16.55%) - Gitcode

--- 1450-number-of-students-doing-homework-at-a-given-time/1450. Number of Students Doing Homework at a Given Time.py
+++ 1450-number-of-students-doing-homework-at-a-given-time/1450. Number of Students Doing Homework at a Given Time.py
@@ -1,7 +1,3 @@
 class Solution:
     def busyStudent(self, startTime: List[int], endTime: List[int], queryTime: int) -> int:
-        res = 0
-        for i in range(len(startTime)):
-            if startTime[i] <= queryTime <= endTime[i]:
-                res += 1
-        return res
+        return sum(startTime[i] <= queryTime <= endTime[i] for i in range(len(startTime)))
