7f807bbf56208585de3efab2f8784e4c2a7feb3a
Geoffrey Lee <geoffrey31415@gmail.com>
2025-08-23 20:19:13 -0400

    Runtime: 273 ms (5.11%), Memory: 149.1 MB (6.20%) - Gitcode

--- 1493-longest-subarray-of-1s-after-deleting-one-element/1493. Longest Subarray of 1's After Deleting One Element.py
+++ 1493-longest-subarray-of-1s-after-deleting-one-element/1493. Longest Subarray of 1's After Deleting One Element.py
@@ -1,19 +1,17 @@
 class Solution:
     def longestSubarray(self, nums: List[int]) -> int:
         if nums.count(1) == len(nums): return len(nums) - 1
+        @cache
+        def dfs(i, used):
+            if i == len(nums):
+                return 0
+            if nums[i] == 1:
+                return 1 + dfs(i+1, used)
+            if used:
+                return 0
+            return dfs(i+1, True)
+
         res = 0
         for i in range(len(nums)):
-            if nums[i] == 0:
-                l, r = i-1, i+1
-                countLeft = 0
-                countRight = 0
-                while l >= 0 and nums[l] == 1:
-                    l -= 1
-                    countLeft += 1
-                while r < len(nums) and nums[r] == 1:
-                    r += 1
-                    countRight += 1
-                res = max(res, countRight + countLeft)
-                
+            res = max(res, dfs(i, False))
         return res
-        
