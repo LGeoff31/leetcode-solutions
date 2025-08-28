352b277c76a46f8879d8f671e61605df185b2e6a
Geoffrey Lee <geoffrey31415@gmail.com>
2025-08-27 20:24:47 -0400

    Runtime: 11 ms (68.33%), Memory: 18 MB (9.17%) - Gitcode

--- 3446-sort-matrix-by-diagonals/3446. Sort Matrix by Diagonals.py
+++ 3446-sort-matrix-by-diagonals/3446. Sort Matrix by Diagonals.py
@@ -1,38 +1,34 @@
 class Solution:
     def sortMatrix(self, grid: List[List[int]]) -> List[List[int]]:
-        n = len(grid)
-        numberDiagonals = 2*n-1
-        for i in range(n-1, -1, -1):
-            r,c = i, 0
-            arr = []
-            while r < n and c < n:
-                arr.append(grid[r][c])
-                r += 1
-                c += 1
-            arr.sort(reverse=True)
-            print(arr)
-            idx = 0
-            r,c = i, 0
-            while r < n and c < n:
-                grid[r][c] = arr[idx]
-                r += 1
-                c += 1
-                idx += 1
-        for i in range(1, n):
-            r,c = 0, i
-            arr = []
-            while r < n and c < n:
-                arr.append(grid[r][c])
-                r += 1
-                c += 1
-            arr.sort()
-            idx = 0
-            r,c = 0, i
-            while r < n and c < n:
-                grid[r][c] = arr[idx]
-                r += 1
-                c += 1
-                idx += 1
-        print(grid)
-        return grid
+        rows, cols = len(grid), len(grid[0])
+        
+        for r in range(rows):
+            a = []
+            new_c = 0
+            for new_r in range(r, rows):
+                a.append(grid[new_r][new_c])
+                new_c += 1
+            a.sort(reverse=True)
+            i = 0
+            new_c = 0
+            for new_r in range(r, rows):
+                grid[new_r][new_c] = a[i]
+                new_c += 1
+                i += 1
+        for c in range(1, cols):
+            a = []
+            new_r = 0
+            for new_c in range(c, cols):
+                a.append(grid[new_r][new_c])
+                new_r += 1
+            a.sort()
+            i = 0
+            new_r = 0
+            for new_c in range(c, cols):
+                grid[new_r][new_c] = a[i]
+                new_r += 1
+                i += 1
             
+
+        return grid
+
