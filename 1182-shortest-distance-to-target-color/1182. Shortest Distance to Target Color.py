e1d40c333e35e473bb05790b3946c25213d7f06f
Geoffrey Lee <geoffrey31415@gmail.com>
2025-08-29 16:26:10 -0400

    Runtime: 403 ms (17.76%), Memory: 39 MB (24.30%) - Gitcode

--- 1182-shortest-distance-to-target-color/1182. Shortest Distance to Target Color.py
+++ 1182-shortest-distance-to-target-color/1182. Shortest Distance to Target Color.py
@@ -1,28 +1,34 @@
 class Solution:
     def shortestDistanceColor(self, colors: List[int], queries: List[List[int]]) -> List[int]:
-        dic = defaultdict(list)
-        for i in range(len(colors)):
-            dic[colors[i]].append(i)
-        res=[]
-        print(dic)
-        for idx, c in queries:
-            if not dic[c]: 
-                res.append(-1)
-                continue
-
-            i = bisect_left(dic[c], idx)
-            a = 1e9
-            if i >= len(dic[c]):
-                a = min(a, abs(idx-dic[c][i-1]))
+        non_exist = {1,2,3} - set(colors)
+        # dp[i][x] = closest distance to x starting at i
+        dp = [[1e9, 1e9, 1e9] for _ in range(len(colors))]
+        # FORWARD
+        dp[0][0] = 0 if colors[0] == 1 else 1e9
+        dp[0][1] = 0 if colors[0] == 2 else 1e9
+        dp[0][2] = 0 if colors[0] == 3 else 1e9
+        for i in range(1, len(colors)):
+            dp[i][0] = 0 if colors[i] == 1 else 1e9
+            dp[i][1] = 0 if colors[i] == 2 else 1e9
+            dp[i][2] = 0 if colors[i] == 3 else 1e9
+            for j in range(3):
+                dp[i][j] = min(dp[i][j], 1 + dp[i-1][j])
+        print(dp)
+        # BACKWARD
+        # dp[-1][0] = 0 if colors[-1] == 1 else 1e9
+        # dp[-1][1] = 0 if colors[-1] == 2 else 1e9
+        # dp[-1][2] = 0 if colors[-1] == 3 else 1e9
+        for i in range(len(colors) -2, -1, -1):
+            # dp[i][0] = 0 if colors[i] == 1 else 1e9
+            # dp[i][1] = 0 if colors[i] == 2 else 1e9
+            # dp[i][2] = 0 if colors[i] == 3 else 1e9
+            for j in range(3):
+                dp[i][j] = min(dp[i][j], 1 + dp[i+1][j])
+        a = []
+        for u,v in queries:
+            if v in non_exist:
+                a.append(-1)
             else:
-                a = abs(dic[c][i]-idx)
-                if i+1 < len(dic[c]):
-                    a = min(a, abs(dic[c][i+1] - idx))
-                if i-1 >= 0:
-                    a = min(a, abs(dic[c][i-1] - idx))
-
-            # left_side_idx = dic[c][bisect_left(dic[c], idx)]
-            # right_side_idx = dic[c][bisect_left(dic[c], idx)]
-            res.append(a)
-
-        return res
+                a.append(dp[u][v-1])
+        return a
+        # return list(dp[u][v-1] for u,v in queries)
