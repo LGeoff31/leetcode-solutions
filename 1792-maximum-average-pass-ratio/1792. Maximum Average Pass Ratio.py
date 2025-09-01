b1a59fb20bcab1c942c3a5eaa04346b21917f481
Geoffrey Lee <geoffrey31415@gmail.com>
2025-08-31 21:29:50 -0400

    Runtime: 1058 ms (93.04%), Memory: 54.9 MB (68.26%) - Gitcode

--- 1792-maximum-average-pass-ratio/1792. Maximum Average Pass Ratio.py
+++ 1792-maximum-average-pass-ratio/1792. Maximum Average Pass Ratio.py
@@ -1,27 +1,22 @@
 class Solution:
     def maxAverageRatio(self, classes: List[List[int]], extraStudents: int) -> float:
-        # What would increase the sum the most?
-        # Pick a option that has small denominator and ensure it's not of form x/x
-        # If there's a tie, pick the one with the smaller denomintator
-
-
-        lst = []
+        minHeap = []
         for a,b in classes:
-            profit = (a+1)/(b+1) - a/b
-            heappush(lst, (-profit, a, b))
-        
+            heappush(minHeap, (-((a+1)/(b+1) - a/b), a, b))
         for i in range(extraStudents):
-            profit, a, b = heappop(lst)
-            new_profit = (a+2)/(b+2) - (a+1)/(b+1)
-            heappush(lst, (-new_profit,a+1,b+1))
-        print(lst)
-        res = 0
-        for a,b,c in lst:
-            res += b/c
-        return res / len(lst)
+            c,a,b = heappop(minHeap)
+            heappush(minHeap, (-((a+2)/(b+2) - (a+1)/(b+1)), a+1, b+1))
+        
+        return sum(a/b for _, a,b in minHeap) / len(minHeap)
 
-        # 4/6 - 3/5 = 0.0666666667
-        # 5/6 - 4/5 = 0.0333333333
 
-        # 5/6 - 4/5 = 0.03333
-        # 5/7 - 4/6 = 0.0476190476
+        """
+        5/6 - 4/5 vs 4/6 - 3/5
+        0.0333.   vs 0.0666
+
+        3/6, 3/9, 4/5, 2/10
+
+        5/6-4/5 vs 4/7 - 3/6
+
+        3/4 - 2/3 vs 4/6 - 3/5
+        """
