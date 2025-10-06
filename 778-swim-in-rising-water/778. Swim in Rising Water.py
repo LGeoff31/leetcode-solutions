6e133f4da06a3e95807bf6ccac823b8dd34d904f
Geoffrey Lee <geoffrey31415@gmail.com>
2025-10-05 20:04:40 -0700

    Runtime: 7669 ms (5.01%), Memory: 18 MB (99.50%) - Gitcode

--- 778-swim-in-rising-water/778. Swim in Rising Water.py
+++ 778-swim-in-rising-water/778. Swim in Rising Water.py
@@ -1,20 +1,23 @@
 class Solution:
     def swimInWater(self, grid: List[List[int]]) -> int:
         n = len(grid)
-        minHeap = [(grid[0][0], 0, 0)]
-        visited = set((0,0))
-        def in_bounds(r,c):
-            return min(r,c) >= 0 and max(r,c) < n
-        while minHeap:
-            min_dist, r, c = heapq.heappop(minHeap)
-            if (r,c) == (n-1, n-1):
-                return min_dist
-            nei = [(r+1, c), (r-1, c), (r,c+1), (r,c-1)]
-            for r2, c2 in nei:
-                if in_bounds(r2,c2) and (r2,c2) not in visited:
-                    visited.add((r2,c2))
-                    heapq.heappush(minHeap, (max(min_dist, grid[r2][c2]), r2, c2))
-
-
-
-        
+        def is_possible(val, r, c):
+            if grid[0][0] > val: return False
+            queue = deque([(r,c)])
+            visited = set()
+            visited.add((r,c))
+            while queue:
+                r, c = queue.popleft()
+                if r == n-1 and c == n-1:
+                    return True
+                for nei_r, nei_c in [(r-1, c), (r+1, c), (r, c+1), (r, c-1)]:
+                    if 0 <= nei_r < n and 0 <= nei_c < n and (nei_r, nei_c) not in visited and grid[nei_r][nei_c] <= val:
+                        visited.add((nei_r, nei_c))
+                        queue.append((nei_r, nei_c))
+            return False
+        res = 1e9
+        for r in range(len(grid)):
+            for c in range(len(grid[r])):
+                if is_possible(grid[r][c], 0, 0):
+                    res = min(res, grid[r][c])
+        return res
