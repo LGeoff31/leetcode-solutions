68d0dc1a2d087e2023e8a19ac76f1d13a3479f8c
Geoffrey Lee <geoffrey31415@gmail.com>
2025-10-07 21:18:27 -0700

    Runtime: 227 ms (82.08%), Memory: 42.4 MB (69.65%) - Gitcode

--- 2300-successful-pairs-of-spells-and-potions/2300. Successful Pairs of Spells and Potions.py
+++ 2300-successful-pairs-of-spells-and-potions/2300. Successful Pairs of Spells and Potions.py
@@ -1,25 +1,7 @@
 class Solution:
     def successfulPairs(self, spells: List[int], potions: List[int], success: int) -> List[int]:
-        dic = {}
-
+        res = []
         potions.sort()
-
-        lst = [0] * len(spells)
-        
-        for i in range(len(spells)):
-            count=0
-            l, r = 0, len(potions) -1
-            while l <= r:
-                mid = (l+r)//2
-                if (potions[mid] * spells[i] >= success and mid == 0 ) or (potions[mid] * spells[i] >= success and potions[mid-1] * spells[i] < success):
-                    count = len(potions) - mid
-                    break
-                elif potions[mid] * spells[i] >= success:
-                    r = mid - 1
-                else:
-                    l = mid + 1
-
-            lst[i] = count
-
-        return lst
-        
+        for s in spells:
+            res.append(len(potions) - bisect_left(potions, success/s))
+        return res
