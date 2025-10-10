8d023068576d1004475306e47dacc5b247c6391d
Geoffrey Lee <geoffrey31415@gmail.com>
2025-10-09 20:08:25 -0700

    Runtime: 1461 ms (5.00%), Memory: 195.6 MB (8.75%) - Gitcode

--- 3147-taking-maximum-energy-from-the-mystic-dungeon/3147. Taking Maximum Energy From the Mystic Dungeon.py
+++ 3147-taking-maximum-energy-from-the-mystic-dungeon/3147. Taking Maximum Energy From the Mystic Dungeon.py
@@ -1,11 +1,11 @@
 class Solution:
     def maximumEnergy(self, energy: List[int], k: int) -> int:
-        dp = [-1e9] * len(energy)
-        dp[-1] = energy[-1]
-
-        for i in range(len(energy) -2, -1, -1):
-            if i + k < len(energy):
-                dp[i] = energy[i] + dp[i+k]
-            else:
-                dp[i] = energy[i]
-        return max(dp)
+        res = -1e9
+        @cache
+        def dfs(idx):
+            if idx >= len(energy):
+                return 0
+            return energy[idx] + dfs(idx + k)
+        for i in range(len(energy)):
+            res = max(res, dfs(i))
+        return int(res)
