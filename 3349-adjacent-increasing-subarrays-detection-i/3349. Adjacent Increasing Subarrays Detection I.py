a6c78e2bedcff672661950e4df1efe28094a13e4
Geoffrey Lee <geoffrey31415@gmail.com>
2025-10-13 21:19:59 -0700

    Runtime: 89 ms (36.90%), Memory: 17.7 MB (86.63%) - Gitcode

--- 3349-adjacent-increasing-subarrays-detection-i/3349. Adjacent Increasing Subarrays Detection I.py
+++ 3349-adjacent-increasing-subarrays-detection-i/3349. Adjacent Increasing Subarrays Detection I.py
@@ -1,14 +1,14 @@
 class Solution:
     def hasIncreasingSubarrays(self, nums: List[int], k: int) -> bool:
-        def increasing(nums):
-            for i in range(1, len(nums)):
-                if nums[i] <= nums[i-1]:
+        def increasing(arr):
+            for i in range(1, len(arr)):
+                if arr[i] <= arr[i-1]:
                     return False
             return True
-        for i in range(len(nums) - 2 * k + 1):
-            a = nums[i:i+k]
-            b = nums[i+k: i+2*k]
-            print(a,b)
-            if increasing(a) and increasing(b):
+        for i in range(len(nums) - k):
+            arr1 = nums[i: i+k]
+            arr2 = nums[i+k: i+2*k]
+            if len(arr2) != len(arr1): return False
+            if increasing(arr1) and increasing(arr2):
                 return True
         return False
