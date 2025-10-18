92fa95ce42d4d07e8aebea1e2282440d6a571c9f
Geoffrey Lee <geoffrey31415@gmail.com>
2025-10-17 22:36:53 -0700

    Runtime: 877 ms (10.06%), Memory: 36.8 MB (32.54%) - Gitcode

--- 3397-maximum-number-of-distinct-elements-after-operations/3397. Maximum Number of Distinct Elements After Operations.py
+++ 3397-maximum-number-of-distinct-elements-after-operations/3397. Maximum Number of Distinct Elements After Operations.py
@@ -1,13 +1,26 @@
 class Solution:
     def maxDistinctElements(self, nums: List[int], k: int) -> int:
-        seen = set()
-        res = 0
+        # Greedy algorithm: Have a visited set. For each num, pick smallest possible avaialble position, if none add unfixable
         nums.sort()
-        max_elem = -1e9
-        for n in nums:
-            for i in range(max(-k, max_elem-n), k+1):
-                if n+i not in seen:
-                    max_elem = n+i
-                    seen.add(n+i)
+        visited = set()
+        nums[0] = nums[0] - k
+        prevNum = nums[0]
+        visited.add(nums[0])
+        unfixable = 0
+        for i in range(1, len(nums)):
+            if prevNum >= nums[i] + k:
+                unfixable += 1
+                continue
+            found = False
+            for num in range(max(prevNum, nums[i] - k), nums[i] + k + 1):
+                if num not in visited:
+                    visited.add(num)
+                    prevNum = num
+                    found = True
                     break
-        return len(seen)
+            if not found:
+                unfixable += 1
+        return len(nums) - unfixable
+                
+                    
+                
