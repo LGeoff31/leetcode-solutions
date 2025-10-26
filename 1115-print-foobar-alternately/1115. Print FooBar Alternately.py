3bd3bfc50071f6a3a1329cced6be0132cefee488
Geoffrey Lee <geoffrey31415@gmail.com>
2025-10-26 00:31:11 -0700

    Runtime: 80 ms (25.97%), Memory: 18.2 MB (22.89%) - Gitcode

---
+++ 1115-print-foobar-alternately/1115. Print FooBar Alternately.py
@@ -0,0 +1,19 @@
+from threading import Lock
+class FooBar:
+    def __init__(self, n):
+        self.n = n
+        self.lock1 = Lock()
+        self.lock2 = Lock()
+        self.lock2.acquire() # aquire means locked
+
+    def foo(self, printFoo: 'Callable[[], None]') -> None:
+        for i in range(self.n):
+            self.lock1.acquire()
+            printFoo()
+            self.lock2.release()
+
+    def bar(self, printBar: 'Callable[[], None]') -> None:
+        for i in range(self.n):
+            self.lock2.acquire()
+            printBar()
+            self.lock1.release()
