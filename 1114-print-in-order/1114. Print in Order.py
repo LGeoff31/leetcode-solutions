839ff97aeea655472fe60999f9775d901ebec8fd
Geoffrey Lee <geoffrey31415@gmail.com>
2025-10-26 00:12:58 -0700

    Runtime: 63 ms (21.27%), Memory: 18.2 MB (73.76%) - Gitcode

--- 1114-print-in-order/1114. Print in Order.py
+++ 1114-print-in-order/1114. Print in Order.py
@@ -1,18 +1,20 @@
-from threading import Barrier
+from threading import Barrier, Lock
 class Foo:
     def __init__(self):
-        self.b1 = Barrier(2)
-        self.b2 = Barrier(2)
+        self.lock1 = Lock()
+        self.lock2 = Lock()
+        self.lock1.acquire()
+        self.lock2.acquire()
 
     def first(self, printFirst: 'Callable[[], None]') -> None:
         printFirst()
-        self.b1.wait()
+        self.lock1.release()
 
     def second(self, printSecond: 'Callable[[], None]') -> None:
-        self.b1.wait()
-        printSecond()
-        self.b2.wait()
+        with self.lock1:
+            printSecond()
+            self.lock2.release()
 
     def third(self, printThird: 'Callable[[], None]') -> None:
-        self.b2.wait()
-        printThird()
+        with self.lock2:
+            printThird()    
