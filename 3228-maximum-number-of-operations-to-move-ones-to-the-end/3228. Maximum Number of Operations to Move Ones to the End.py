7e1092102c7bbe1667c556e56118e844b669c92d
Geoffrey Lee <geoffrey31415@gmail.com>
2025-11-12 22:08:46 -0800

    Runtime: 127 ms (7.78%), Memory: 17.3 MB (100.00%) - Gitcode

--- 3228-maximum-number-of-operations-to-move-ones-to-the-end/3228. Maximum Number of Operations to Move Ones to the End.py
+++ 3228-maximum-number-of-operations-to-move-ones-to-the-end/3228. Maximum Number of Operations to Move Ones to the End.py
@@ -1,17 +1,25 @@
 class Solution:
     def maxOperations(self, s: str) -> int:
-        curr = 0
-        i = 0
         res = 0
-        while i < len(s):
-            if s[i] == "1":
-                res += curr
-                while i < len(s) and s[i] == "1":
-                    curr += 1
-                    i += 1
-            else:
+        one_count = 0
+        segment_started = False
+        zero_count = 0
+        i = len(s) - 1
+        while i >= 0:
+            if s[i] == "0" and not segment_started:
+                segment_started = True
+                zero_count += 1
+                one_count = 0
+            elif s[i] == "1":
+                segment_started = False
+                while i >= 0 and s[i] == "1":
+                    one_count += 1
+                    i -= 1
+                res += one_count * zero_count
                 i += 1
-        if s[-1] == "0":
-            res += curr
-        return res
             
+            i -= 1
+        return res
+
+
+
