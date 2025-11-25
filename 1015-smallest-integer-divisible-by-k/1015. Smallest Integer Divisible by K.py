ff3af4cfeae1d441f1fab6a02fe9755c095b61ff
Geoffrey Lee <geoffrey31415@gmail.com>
2025-11-24 17:05:45 -0800

    Runtime: 15 ms (35.24%), Memory: 17.4 MB (99.86%) - Gitcode

--- 1015-smallest-integer-divisible-by-k/1015. Smallest Integer Divisible by K.py
+++ 1015-smallest-integer-divisible-by-k/1015. Smallest Integer Divisible by K.py
@@ -1,12 +1,9 @@
 class Solution:
     def smallestRepunitDivByK(self, k: int) -> int:
-        curr = 1
-        a = 1
-        if k == 49993: return 49992
-        for i in range(20000):
-            a = ((curr * 10) + 1) % k
-            if curr % k == 0:
-                return i+1
-            curr = curr*10 + 1
-            
+        
+        rem = 0
+        for i in range(1, k+1):
+            rem = (rem * 10 + 1) % k
+            if rem == 0:
+                return i
         return -1
