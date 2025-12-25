d02203acdad9571ed3ac42d1273fec1f25c34845
Geoffrey Lee <geoffrey31415@gmail.com>
2025-12-25 17:36:19 -0500

    Runtime: 1036 ms (5.40%), Memory: 46.1 MB (10.81%) - Gitcode

--- 3075-maximize-happiness-of-selected-children/3075. Maximize Happiness of Selected Children.py
+++ 3075-maximize-happiness-of-selected-children/3075. Maximize Happiness of Selected Children.py
@@ -1,11 +1,19 @@
 class Solution:
     def maximumHappinessSum(self, happiness: List[int], k: int) -> int:
-        happiness.sort(reverse=True)
         res = 0
-        for i in range(k):
-            if happiness[i]-i > 0:
-                res += happiness[i] - i
-        return res
+        happiness.sort()
+        dic = Counter(happiness)
+        cnt = len(happiness) - 1
+        x = 1
+        for i in range(len(happiness) -1, -1, -1):
+            res += max(0, happiness[i] - (x-1))
+            k -= 1
             
-        
-        
+            cnt -= 1
+            if x in dic and k:
+                cnt -= dic[x]
+            x+=1
+            if k == 0: break
+            print(res)
+        print(res)
+        return res
