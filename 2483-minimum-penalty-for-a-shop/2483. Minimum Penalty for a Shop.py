4d31d8c7df67e0ea92dcd0dcebff6d07d78ff36c
Geoffrey Lee <geoffrey31415@gmail.com>
2025-12-25 19:49:47 -0500

    Runtime: 83 ms (51.52%), Memory: 18 MB (92.15%) - Gitcode

--- 2483-minimum-penalty-for-a-shop/2483. Minimum Penalty for a Shop.py
+++ 2483-minimum-penalty-for-a-shop/2483. Minimum Penalty for a Shop.py
@@ -1,20 +1,19 @@
 class Solution:
     def bestClosingTime(self, customers: str) -> int:
-        suffix_dic = Counter(customers)
-        prefix_dic = defaultdict(int)
-        res = float('inf')
-        res = min(res, suffix_dic["Y"])
-        closing_time = 0
+        """
+        # of N's BEFORE + # of Y's AFTER
+        """
+        n = len(customers)
+        total_y = customers.count("Y")
+        minimum_penalty = total_y
+        earliest_hour = 0
+        n_before = 0
+        for i, c in enumerate(customers):
+            n_before += c == "N"
+            y_after = total_y - (i+1 - n_before)
 
-        def penalty(suffix_dic, prefix_dic): 
-            return suffix_dic["Y"] + prefix_dic["N"]
-
-        for hour in range(len(customers)):
-            suffix_dic[customers[hour]] -= 1
-            prefix_dic[customers[hour]] += 1
-            p = penalty(suffix_dic, prefix_dic)
-            if p < res:
-                closing_time = hour + 1
-                res = p 
-        
-        return closing_time
+            if n_before + y_after < minimum_penalty:
+                earliest_hour = i+1
+                minimum_penalty = n_before + y_after
+    
+        return earliest_hour
