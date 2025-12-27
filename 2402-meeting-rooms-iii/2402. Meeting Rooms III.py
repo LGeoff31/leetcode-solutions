0a74c1f0865ad8b7f87e706c9fd39ea25647fd18
Geoffrey Lee <geoffrey31415@gmail.com>
2025-12-27 02:17:34 -0500

    Runtime: 509 ms (10.35%), Memory: 51.1 MB (94.93%) - Gitcode

--- 2402-meeting-rooms-iii/2402. Meeting Rooms III.py
+++ 2402-meeting-rooms-iii/2402. Meeting Rooms III.py
@@ -1,64 +1,39 @@
 class Solution:
     def mostBooked(self, n: int, meetings: List[List[int]]) -> int:
-        """
-        If empty room, place lowest meeting id
-        If no empty rooms, wait for room and put meeting with earliest start time
-        Example 1
-        1 | 1
-        t=5s
-        1 | 2
-        t=10s
-        2 | 2
-        return meeting 0
-
-        Simulation??
-        SortedList / min heap + cnt array
-        """
         meetings.sort()
-        cnt = [0] * n
-        q = SortedList() # (end_time, room)
-        available_rooms = SortedList([i for i in range(n)])
-        i = 0
-        t = meetings[0][0]
-        print(meetings)
-        while i < len(meetings):
-            if len(available_rooms) == 0:
-                t = q[0][0]
-                available_rooms.add(q[0][1])
-                q.remove(q[0])
-            if not q:
-                t = meetings[i][0]
-                q.add((meetings[i][1], 0))
-                available_rooms.remove(0)
-                cnt[0] += 1
-                i+=1
-                # print(q)
-                # print(available_rooms)
-                # print(cnt)
-                # print()
-                continue
-
-            while q and q[0][0] <= meetings[i][0]:
-                t = q[0][0]
-                available_rooms.add(q[0][1])
-                q.remove(q[0])
-
-            smallest_available_room = available_rooms[0]
-            if t > meetings[i][0]:
-                # Delay
-                duration = meetings[i][1] - meetings[i][0]
-                q.add((t+duration, smallest_available_room))
-                available_rooms.remove(smallest_available_room)
-                cnt[smallest_available_room] += 1
+        dic = defaultdict(int)
+        minHeap = [] # (end_time, room)
+        available_rooms = SortedList(range(0, n))
+        # print(meetings)
+        for i, (s, e) in enumerate(meetings):
+            if not available_rooms:
+                end_time, room = heappop(minHeap)
+                available_rooms.add(room)
+                if s >= end_time:
+                    while minHeap and s >= minHeap[0][0]:
+                        _, r = heappop(minHeap)
+                        available_rooms.add(r)
+                    heappush(minHeap, (e, available_rooms[0]))
+                else:
+                    heappush(minHeap, (end_time + (e-s), available_rooms[0]))
+                dic[available_rooms[0]] += 1
+                available_rooms.remove(available_rooms[0])
             else:
-                t = meetings[i][0]
-                q.add((meetings[i][1], smallest_available_room))
-                available_rooms.remove(smallest_available_room)
-                cnt[smallest_available_room] += 1
-            # print(q)
-            # print(available_rooms)
-            # print(cnt)
-            # print()
-            i += 1
-            
-        return cnt.index(max(cnt))
+                while minHeap and s >= minHeap[0][0]:
+                    _, r = heappop(minHeap)
+                    available_rooms.add(r)
+
+                dic[available_rooms[0]] += 1
+                heappush(minHeap, (e, available_rooms[0]))
+                available_rooms.remove(available_rooms[0])
+            # print(available_rooms, minHeap, s, e)
+
+        # a = list(sorted((value, key) for key, value in dic.items()))
+        # print(a)
+        lst = list(sorted((value, key) for key, value in dic.items()))
+        max_value = max(a for a, _ in lst)
+        for x,y in lst:
+            if x == max_value:
+                return y
+
+        # return list(sorted((value, key) for key, value in dic.items()))[-1][1]
