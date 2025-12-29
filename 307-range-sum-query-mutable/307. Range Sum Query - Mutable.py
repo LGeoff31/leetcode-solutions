d751d4c16d984322b817bd58b59081669b3b05ca
Geoffrey Lee <geoffrey31415@gmail.com>
2025-12-29 00:49:06 -0500

    Runtime: 1034 ms (20.37%), Memory: 34.1 MB (100.00%) - Gitcode

--- 307-range-sum-query-mutable/307. Range Sum Query - Mutable.py
+++ 307-range-sum-query-mutable/307. Range Sum Query - Mutable.py
@@ -1,45 +1,39 @@
-class SegmentTree:
-    def __init__(self, nums):
-        self.n = len(nums)
-        self.trees = [0] * (2*self.n) # Note that the first element is a blank??????
-
-        # Populate the leaf nodes
-        for i in range(self.n):
-            self.trees[i+self.n] = nums[i]
-        # Populate remainder tree
-        for i in range(self.n - 1, 0, -1):
-            self.trees[i] = self.trees[2*i] + self.trees[2*i+1]
-
-
 class NumArray:
+
     def __init__(self, nums: List[int]):
-        self.segmentTree = SegmentTree(nums)
         self.n = len(nums)
+        self.nums = nums
+        self.tree = [0] * (self.n+1)
 
-    def update(self, index: int, val: int) -> None:
-        trees = self.segmentTree.trees
-        pos = index + self.n
-        trees[pos] = val
-        # Update the tree accordingly
-        while pos > 1:
-            pos //= 2
-            trees[pos] = trees[2*pos] + trees[2*pos+1]
+        for i, val in enumerate(self.nums):
+            self.updateTree(i+1, val)
+        
+    def updateTree(self, i, val):
+        while i <= self.n:
+            self.tree[i] += val
+            i += i&-i
 
-    def sumRange(self, left: int, right: int) -> int:   
+    def getSum(self, i):
         res = 0
-        left += self.n
-        right += self.n
-        trees = self.segmentTree.trees
-        while left <= right:
-            if left % 2 == 1:
-                res += trees[left]
-                left += 1
-            if right % 2 == 0:
-                res += trees[right]
-                right -= 1
-            left //= 2
-            right //= 2
+        while i > 0:
+            res += self.tree[i]
+            i -= i&-i
         return res
 
 
+    def update(self, index: int, val: int) -> None: #O(1)
+        diff = val - self.nums[index]
+        self.nums[index] = val
+        self.updateTree(index+1, diff)
+
+
+    def sumRange(self, left: int, right: int) -> int: #O(n)
+        return self.getSum(right+1) - self.getSum(left)
         
+        
+
+
+# Your NumArray object will be instantiated and called as such:
+# obj = NumArray(nums)
+# obj.update(index,val)
+# param_2 = obj.sumRange(left,right)
