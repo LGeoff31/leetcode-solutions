f3027e8cdfffa707930dd862a928a4b9c4d30c83
Geoffrey Lee <geoffrey31415@gmail.com>
2025-12-29 00:49:18 -0500

    Runtime: 1729 ms (6.07%), Memory: 48.3 MB (18.83%) - Gitcode

--- 307-range-sum-query-mutable/307. Range Sum Query - Mutable.py
+++ 307-range-sum-query-mutable/307. Range Sum Query - Mutable.py
@@ -1,62 +1,61 @@
-class Node(object):
+class Node():
     def __init__(self, start, end):
         self.start = start
         self.end = end
-        self.val = 0
-        self.left = None #point to attatch next node
+        self.left = None
         self.right = None
+        self.val = 0
 
 class NumArray:
     def __init__(self, nums: List[int]):
         def createTree(nums, l, r):
             if l > r:
                 return None
-            
             if l == r:
-                n = Node(l, r)
-                n.val = nums[r]
-                return n
-
-            mid = (l+r) // 2
+                node = Node(l, r)
+                node.val = nums[l]
+                return node
+            
+            mid = (l+r)//2
             root = Node(l, r)
 
             root.left = createTree(nums, l, mid)
             root.right = createTree(nums, mid+1, r)
             root.val = root.left.val + root.right.val
             return root
-        
+
         self.root = createTree(nums, 0, len(nums) - 1)
 
     def update(self, index: int, val: int) -> None:
-        #recursively get to that leaf node and then recurse upwards to update all the values
-        def updateVal(root, i, val):
-            if root.start == root.end:
-                root.val = val
+        def updateVal(node, i, val):
+            if node.start == node.end:
+                node.val = val
                 return val
-
-            mid = (root.start + root.end) // 2
-            if i <= mid: #want to go down left branch
-                updateVal(root.left, i, val)
+            mid = (node.start + node.end) // 2
+            if i > mid:
+                updateVal(node.right, i, val)
             else:
-                updateVal(root.right, i, val)
-            root.val = root.left.val + root.right.val
-            return root.val
+                updateVal(node.left, i, val)
+            node.val = node.left.val + node.right.val
+            return node.val
         return updateVal(self.root, index, val)
 
     def sumRange(self, left: int, right: int) -> int:
-        def rangeSum(root, left, right):
-            if root.start == left and root.end == right:
-                return root.val
-            
-            mid = (root.start + root.end) // 2
-            if right <= mid:
-                return rangeSum(root.left, left, right)
-            elif left >= mid + 1:
-                return rangeSum(root.right, left, right)
-            #otherwise the interval is split between both
+        def rangeSum(node, l, r):
+            if node.start == l and node.end == r:
+                return node.val
+            mid = (node.start + node.end) // 2
+            #check if the query range is strictly on the left or right side
+            if r <= mid: #strictly on the left
+                return rangeSum(node.left, l, r)
+            elif l > mid: #strictly on the right
+                return rangeSum(node.right,l, r)
             else:
-                return rangeSum(root.left, left, mid) + rangeSum(root.right, mid+1 ,right)
+                return rangeSum(node.left, l, mid) + rangeSum(node.right, mid+1, r)
         return rangeSum(self.root, left, right)
+                
+
+       
         
 
 # Your NumArray object will be instantiated and called as such:
