9417ba61d6f22398c7f0cbd1123ef77ee1c68a40
Geoffrey Lee <geoffrey31415@gmail.com>
2025-12-30 23:12:18 -0500

    Runtime: 1490 ms (22.65%), Memory: 26.3 MB (48.07%) - Gitcode

---
+++ 1970-last-day-where-you-can-still-cross/1970. Last Day Where You Can Still Cross.py
@@ -0,0 +1,35 @@
+class Solution:
+    def latestDayToCross(self, rows: int, cols: int, cells: List[List[int]]) -> int:
+        l, r = 0, len(cells)
+        def possible(day):
+            filled_cells = set((c[0],c[1]) for c in cells[:day])
+            queue = deque([])
+            visited = set()
+            directions = [(-1, 0), (1, 0), (0, 1), (0, -1)]
+            for c in range(cols):
+                if (1, c+1) not in filled_cells:
+                    visited.add((1, c+1))
+                    queue.append((1, c+1))
+            # print(queue)
+            while queue:
+                r,c = queue.popleft()
+                for dr, dc in directions:
+                    new_r, new_c = r + dr, c + dc
+                    # print(new_r, new_c)
+                    if not (1 <= new_r <= rows and 1 <= new_c <= cols) or (new_r, new_c) in visited or (new_r, new_c) in filled_cells:
+                        continue
+                    if new_r == rows:
+                        return True
+                    visited.add((new_r, new_c))
+                    queue.append((new_r, new_c))
+            return False
+        res = -1e9
+        while l <= r:
+            mid = (l + r) // 2
+            # print(mid, possible(mid))
+            if possible(mid):
+                res = max(res, mid)
+                l = mid + 1
+            else:
+                r = mid - 1
+        return res
