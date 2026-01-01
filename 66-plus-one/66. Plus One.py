407e8571ce0aea3df47aedab69d987ae28260ce4
Geoffrey Lee <geoffrey31415@gmail.com>
2025-12-31 20:56:29 -0500

    Runtime: 0 ms (100.00%), Memory: 17.3 MB (86.09%) - Gitcode

--- 66-plus-one/66. Plus One.py
+++ 66-plus-one/66. Plus One.py
@@ -1,5 +1,3 @@
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
-        a = "".join(list(str(d) for d in digits))
-        b = str(int(a) + 1)
-        return list(int(z) for z in b)
+        return list(int(z) for z in str(int("".join(list(str(d) for d in digits))) + 1))
