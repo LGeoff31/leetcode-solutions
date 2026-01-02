8d5ee146d08d007fdc65880d5359e82d597c59d9
Geoffrey Lee <geoffrey31415@gmail.com>
2026-01-02 00:51:36 -0500

    Runtime: 3 ms (92.41%), Memory: 18.3 MB (93.35%) - Gitcode

--- 787-cheapest-flights-within-k-stops/787. Cheapest Flights Within K Stops.py
+++ 787-cheapest-flights-within-k-stops/787. Cheapest Flights Within K Stops.py
@@ -1,13 +1,27 @@
 class Solution:
-    def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int: 
-        dp = [float('inf')] * n
-        dp[src] = 0
-
-        for _ in range(k + 1):
-            temp = dp[:]
-            for flight in flights:
-                if dp[flight[0]] != float('inf'):
-                    temp[flight[1]] = min(temp[flight[1]], dp[flight[0]] + flight[2])
-            dp = temp
+    def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
+        adj = defaultdict(list)
+        for u,v, weight in flights:
+            adj[u].append((v, weight))
         
-        return dp[dst] if dp[dst] != float('inf') else -1
+        res = float('inf')
+        queue = deque([(src, 0)])
+        distances = {src : 0}
+        while queue:
+            for i in range(len(queue)):
+                node, weight = queue.popleft()
+                if node == dst:
+                    res = min(res, distances[node])
+                    continue
+                
+                if k == -1:
+                    continue
+
+                for nei, nei_weight in adj[node]:
+                    if nei not in distances or weight+nei_weight < distances[nei]:
+                        distances[nei] = weight + nei_weight
+                        queue.append((nei, weight + nei_weight))
+            k -= 1
+        
+
+        return res if res != float('inf') else -1
