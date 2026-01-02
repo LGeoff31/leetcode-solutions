1ac03dd2f8523f7b023874a0200cac52c9bf8f35
Geoffrey Lee <geoffrey31415@gmail.com>
2026-01-01 23:40:37 -0500

    Runtime: 2 ms (9.64%), Memory: 17.7 MB (76.55%) - Gitcode

--- 399-evaluate-division/399. Evaluate Division.py
+++ 399-evaluate-division/399. Evaluate Division.py
@@ -1,58 +1,30 @@
 class Solution:
     def calcEquation(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
-        """
-        numerator: [(denominator, weight)]
-        denominator: [(numerator, weight)]
+        adj = defaultdict(list)
+        for i, (u,v) in enumerate(equations):
+            adj[u].append((v, values[i]))
+            adj[v].append((u, 1/values[i]))
+        
+        def bfs(start, end):
+            # if start == end and start in adj: return 1
+            # -1 if start or end do no exist in graph
 
-        a: [(b, 2)]
-        b: [(c, 3), (a, 1/2)]
-        c: [(b, 1/3)]
-
-        query: a/c
-        goal: start from a, try to find c
-
-        queue = [(c, 6)]
-
-        WHEN IS IT NOT VALID?
-        if a variable doesn't exist at all in numerator, 
-        """
-        number_equations = len(equations)
-        conversions = defaultdict(list)
-
-        for i in range(number_equations):
-            n, d = equations[i]
-            quotient = values[i]
-
-            conversions[n].append((d, quotient))
-            conversions[d].append((n, 1/quotient))
-            conversions[n].append((n, 1))
-            conversions[d].append((d, 1))
+            queue = deque([(start, 1)])
+            visited = set()
+            visited.add(start)
+            while queue:
+                for i in range(len(queue)):
+                    node, weight = queue.popleft()
+                    if node not in adj: return -1
+                    if node == end:
+                        return weight
+                    for n, w in adj[node]:
+                        if n not in visited:
+                            visited.add(n)
+                            queue.append((n, w * weight))
+            return -1
 
         res = []
-        for n, d in queries:
-            if n not in conversions:
-                res.append(-1)
-                continue
-            
-            queue = deque([den_weight for den_weight in conversions[n]])
-            visited = set()
-
-            while queue:
-                den, weight = queue.popleft()
-                if den in visited:
-                    continue
-                visited.add(den)
-                
-                if den == d:
-                    res.append(weight)
-                    break
-                
-                for new_den in conversions[den]:
-                    den_2, weight_2 = new_den
-                    queue.append((den_2, weight_2 * weight))
-            else:
-                res.append(-1)
-
+        for start, end in queries:
+            res.append(bfs(start, end))
         return res
-
-
