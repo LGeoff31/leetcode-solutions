c5bbe1b6f62d319264bdb01bc405a0ca4ee995a1
Geoffrey Lee <geoffrey31415@gmail.com>
2026-01-01 23:39:27 -0500

    Runtime: 0 ms (100.00%), Memory: 17.5 MB (85.57%) - Gitcode

--- 399-evaluate-division/399. Evaluate Division.py
+++ 399-evaluate-division/399. Evaluate Division.py
@@ -1,33 +1,58 @@
 class Solution:
     def calcEquation(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
-        adj = collections.defaultdict(list) #maps a -> list of [b, a/b]
-        for i, eq in enumerate(equations):
-            a,b = eq
-            adj[a].append([b, values[i]])
-            adj[b].append([a, 1/values[i]])
-        
-        def bfs(src, target):
-            if src not in adj or target not in adj:
-                return -1
-            q, visited = deque(), set()
-            q.append([src, 1])
-            visited.add(src )
-            while q:
-                for i in range(len(q)):
-                    currElem, val = q.popleft()
-                    if currElem == target: return val
-                    for n, w in adj[currElem]:
-                        if n not in visited:
-                            q.append([n, val * w])
-                            visited.add(n)
+        """
+        numerator: [(denominator, weight)]
+        denominator: [(numerator, weight)]
 
-            return -1
+        a: [(b, 2)]
+        b: [(c, 3), (a, 1/2)]
+        c: [(b, 1/3)]
 
-        
+        query: a/c
+        goal: start from a, try to find c
+
+        queue = [(c, 6)]
+
+        WHEN IS IT NOT VALID?
+        if a variable doesn't exist at all in numerator, 
+        """
+        number_equations = len(equations)
+        conversions = defaultdict(list)
+
+        for i in range(number_equations):
+            n, d = equations[i]
+            quotient = values[i]
+
+            conversions[n].append((d, quotient))
+            conversions[d].append((n, 1/quotient))
+            conversions[n].append((n, 1))
+            conversions[d].append((d, 1))
+
         res = []
+        for n, d in queries:
+            if n not in conversions:
+                res.append(-1)
+                continue
+            
+            queue = deque([den_weight for den_weight in conversions[n]])
+            visited = set()
 
-        for q in queries:
-            result = bfs(q[0], q[1])
-            res.append(result)
+            while queue:
+                den, weight = queue.popleft()
+                if den in visited:
+                    continue
+                visited.add(den)
+                
+                if den == d:
+                    res.append(weight)
+                    break
+                
+                for new_den in conversions[den]:
+                    den_2, weight_2 = new_den
+                    queue.append((den_2, weight_2 * weight))
+            else:
+                res.append(-1)
+
         return res
+
 
