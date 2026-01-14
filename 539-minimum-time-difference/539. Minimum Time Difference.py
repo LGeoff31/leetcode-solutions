b3293b065bf3f0704e6491d67fd9d78af83bb0ef
Geoffrey Lee <geoffrey31415@gmail.com>
2026-01-14 12:15:54 -0500

    Runtime: 40 ms (5.61%), Memory: 22.6 MB (5.06%) - Gitcode

--- 539-minimum-time-difference/539. Minimum Time Difference.py
+++ 539-minimum-time-difference/539. Minimum Time Difference.py
@@ -1,14 +1,33 @@
 class Solution:
     def findMinDifference(self, timePoints: List[str]) -> int:
-        # 12h = 7200m
-        lst = []
-        for time in timePoints:
-            hour, mins = time.split(":")
-            lst.append(int(hour) * 60 + int(mins))
-        res = 1e9
-        lst.sort()
-        for i in range(len(lst)-1):
-            res = min(res, abs(lst[i+1] - lst[i]), 1440 - abs(lst[i+1] - lst[i]))
+        timePoints.sort(key = lambda x: (int(x.split(":")[0]), int(x.split(":")[1])))
+        print(timePoints)
+        def calc_diff(time1, time2):
+            """
+            time1 = 13:50
+            time2 = 14:34
 
-        return min(res, abs(lst[-1] - lst[0]), 1440 - abs(lst[-1] - lst[0]))
-        
+            diff_min = -16
+            diff_h = 1 -> (60 minutes)
+            60 - 16 = 44
+
+            HOW MANY MINUTES ARE THERE ON 24-HOUR CLOCK??
+            24x60 = 1440
+
+            """
+            h1, m1 = [int(x) for x in time1.split(":")]
+            h2, m2 = [int(x) for x in time2.split(":")]
+
+            if h1 > h2 or (h1 == h2 and m1 > m2):
+                return calc_diff(time2, time1)
+            print(time2, time1)
+            # Enforced time2 > time1
+            res = (m2 - m1) + (h2-h1) * 60
+            return min(res, 1440 - res)
+
+        minimum_time_difference = float('inf')
+        for i in range(1, len(timePoints)):
+            minimum_time_difference = min(minimum_time_difference, calc_diff(timePoints[i-1], timePoints[i]))
+
+        minimum_time_difference = min(minimum_time_difference, calc_diff(timePoints[0], timePoints[-1]))
+        return minimum_time_difference
