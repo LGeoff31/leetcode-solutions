a383fa73ed39d184ecb4ac5831f32f57e0337819
Geoffrey Lee <geoffrey31415@gmail.com>
2026-01-15 23:21:08 -0500

    Runtime: 1377 ms (53.66%), Memory: 40.1 MB (78.66%) - Gitcode

--- 2975-maximum-square-area-by-removing-fences-from-a-field/2975. Maximum Square Area by Removing Fences From a Field.py
+++ 2975-maximum-square-area-by-removing-fences-from-a-field/2975. Maximum Square Area by Removing Fences From a Field.py
@@ -1,26 +1,23 @@
 class Solution:
     def maximizeSquareArea(self, m: int, n: int, hFences: List[int], vFences: List[int]) -> int:
-        MOD = 10**9+7
-        hFences.append(1)
-        hFences.append(m)
-        vFences.append(1)
-        vFences.append(n)
+        MOD = 10 ** 9 + 7
+
+        rows, cols = m, n
         hFences.sort()
         vFences.sort()
-        print(hFences)
-        print(vFences)
+        hFences = [1] + hFences + [m]
+        vFences = [1] + vFences + [n]
 
-        widths = set()
-        for i in range(len(vFences)):
-            for j in range(i+1, len(vFences)):
-                widths.add(vFences[j] - vFences[i])
         heights = set()
         for i in range(len(hFences)):
             for j in range(i+1, len(hFences)):
-                heights.add(hFences[j] - hFences[i])
-        res = -1
-        for num in widths:
-            if num in heights:
-                res = max(res, num*num)
-        if res > MOD: return res % MOD
-        return res
+                height = hFences[j] - hFences[i]
+                heights.add(height)
+        res = 0
+        for i in range(len(vFences)):
+            for j in range(i+1, len(vFences)):
+                width = vFences[j] - vFences[i]
+                if width in heights:
+                    res = max(res, width * width)
+        if res == 0: return -1
+        return res % MOD
