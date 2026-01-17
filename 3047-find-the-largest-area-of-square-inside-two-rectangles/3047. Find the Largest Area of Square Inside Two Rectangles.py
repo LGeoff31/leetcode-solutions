83943a4d9f3933312099b51d17238ebf6738216c
Geoffrey Lee <geoffrey31415@gmail.com>
2026-01-16 19:20:31 -0500

    Runtime: 3807 ms (76.62%), Memory: 20 MB (12.99%) - Gitcode

---
+++ 3047-find-the-largest-area-of-square-inside-two-rectangles/3047. Find the Largest Area of Square Inside Two Rectangles.py
@@ -0,0 +1,21 @@
+class Solution:
+    def largestSquareArea(self, bottomLeft: List[List[int]], topRight: List[List[int]]) -> int:
+        res = 0
+        n = len(bottomLeft)
+        def maxArea(i,j):
+            a,b = bottomLeft[i]
+            c,d = topRight[i]
+
+            e,f = bottomLeft[j]
+            g,h = topRight[j]
+
+            width_overlap = max(0, min(c, g) - max(a, e))
+            height_overlap = max(0, min(d, h) - max(b, f))
+
+            return min(height_overlap, width_overlap) ** 2
+
+        for i in range(n):
+            for j in range(i+1, n):
+                res = max(res, maxArea(i,j))
+
+        return res
