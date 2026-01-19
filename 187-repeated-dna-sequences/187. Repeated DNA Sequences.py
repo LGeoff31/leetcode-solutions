50a143d4993424388ae4761eb4e6587442b2f855
Geoffrey Lee <geoffrey31415@gmail.com>
2026-01-19 13:21:06 -0500

    Runtime: 27 ms (45.84%), Memory: 31.4 MB (13.10%) - Gitcode

--- 187-repeated-dna-sequences/187. Repeated DNA Sequences.py
+++ 187-repeated-dna-sequences/187. Repeated DNA Sequences.py
@@ -1,12 +1,17 @@
 class Solution:
     def findRepeatedDnaSequences(self, s: str) -> List[str]:
-        visited = set()
-        res = []
-        for i in range(len(s) - 10 + 1):
-            substring = s[i:i+10]      
-            if substring in visited:
-                res.append(substring)
-            else:
-                visited.add(substring)      
-        return list(set(res))
+        seen = set()
         
+        if len(s) < 10:
+            return []
+        res = []
+        start = s[:10]
+        seen.add(start)
+        for i in range(10, len(s)):
+            start = start[1:] + s[i]
+            if start in seen:
+                res.append(start)
+            
+            seen.add(start)
+
+        return list(set(res))
