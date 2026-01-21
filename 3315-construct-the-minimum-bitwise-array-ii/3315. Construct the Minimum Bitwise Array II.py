81dab5bcb3ea16bb5775ee9971e7c7c54ab33907
Geoffrey Lee <geoffrey31415@gmail.com>
2026-01-21 01:00:02 -0500

    Runtime: 12 ms (18.87%), Memory: 19.5 MB (13.21%) - Gitcode

--- 3315-construct-the-minimum-bitwise-array-ii/3315. Construct the Minimum Bitwise Array II.py
+++ 3315-construct-the-minimum-bitwise-array-ii/3315. Construct the Minimum Bitwise Array II.py
@@ -1,21 +1,29 @@
 class Solution:
     def minBitwiseArray(self, nums: List[int]) -> List[int]:
-        ans = [0] * len(nums)
-        for i in range(len(nums)):
-            if nums[i] == 2:
-                ans[i] = -1
-            else:
-                bin_rep = bin(nums[i])[2:]
-                idx_leftmost_1 = -1 # Will definitely find a 1
-                for j in range(len(bin_rep)-1,-1,-1):
-                    if bin_rep[j] == "1":
-                        idx_leftmost_1 = j
-                    else:
-                        break
-                # The bit at idx_leftmost_1 must be swapped
-                a = list(bin_rep)
-                a[idx_leftmost_1] = "0"
-                print(a)
-                b = "".join(a)
-                ans[i] = int(b, 2)
-        return ans
+        """
+        2: 000010
+        3: 000011
+        5: 000101
+        7: 000111
+
+        11: 1011
+        13: 1101
+        15: 1111
+
+        31: 11111
+        """
+        res = []
+        for n in nums:
+            if n % 2 == 0:
+                res.append(-1)
+                continue
+            bin_rep = ["0"] * (32 - len(bin(n)[2:])) + list(bin(n)[2:])
+            number_ones = 0
+            for i in range(len(bin_rep) - 1, -1, -1):
+                if bin_rep[i] == "1":
+                    number_ones += 1
+                else:
+                    bin_rep[i+1] = "0"
+                    break   
+            res.append(int("".join(bin_rep), 2))
+        return res
