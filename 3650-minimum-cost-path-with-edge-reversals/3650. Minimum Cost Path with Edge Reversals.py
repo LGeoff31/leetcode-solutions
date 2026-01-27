f9e012716e4dfce1f02655cccc575664e56182b9
Geoffrey Lee <geoffrey31415@gmail.com>
2026-01-27 14:17:58 -0500

    Runtime: 1517 ms (10.97%), Memory: 84.9 MB (10.98%) - Gitcode

---
+++ 3650-minimum-cost-path-with-edge-reversals/3650. Minimum Cost Path with Edge Reversals.py
@@ -0,0 +1,33 @@
+class Solution:
+    def minCost(self, n: int, _edges: List[List[int]]) -> int:
+        dist = defaultdict(int)
+        edges = defaultdict(list)
+        reversed_edges = defaultdict(list)
+
+
+        for i in range(n):
+            dist[i] = float('inf')
+        dist[0] = 0
+
+        for u,v,w in _edges:
+            edges[u].append((v, w))
+            reversed_edges[v].append((u, 2*w))
+        
+
+        queue = deque([(0, 0)])
+
+        while queue:
+            node, weight = queue.popleft()
+            # try going in right direction
+            for nei, nei_w in edges[node]:
+                if nei_w + weight < dist[nei]:
+                    print('reached', nei)
+                    dist[nei] = nei_w + weight
+                    queue.append((nei, nei_w + weight))
+            # try going in reverse direction
+            for nei, nei_w in reversed_edges[node]:
+                if nei_w + weight < dist[nei]:
+                    dist[nei] = nei_w + weight
+                    queue.append((nei, nei_w + weight))
+        print('dist', dist)
+        return dist[n-1] if dist[n-1] != float('inf') else -1
