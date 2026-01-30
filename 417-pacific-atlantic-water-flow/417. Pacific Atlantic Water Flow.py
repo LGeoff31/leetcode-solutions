2c494dd40c6a56b8ea227ac59b555eae42c27e8c
Geoffrey Lee <geoffrey31415@gmail.com>
2026-01-29 22:32:59 -0500

    Runtime: 2794 ms (5.02%), Memory: 20.6 MB (41.02%) - Gitcode

--- 417-pacific-atlantic-water-flow/417. Pacific Atlantic Water Flow.py
+++ 417-pacific-atlantic-water-flow/417. Pacific Atlantic Water Flow.py
@@ -1,24 +1,31 @@
 class Solution:
     def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
         rows, cols = len(heights), len(heights[0])
-        
-        p, a = set(), set()
-        def dfs(r,c,visited):
-            if not (0 <= r < rows and 0 <= c < cols) or (r,c) in visited:
-                return
-            visited.add((r,c))
-            for nei_r, nei_c in [(r-1,c), (r+1,c), (r,c-1), (r,c+1)]:
-                if 0 <= nei_r < rows and 0 <= nei_c < cols and (nei_r, nei_c) not in visited and heights[nei_r][nei_c] >= heights[r][c]:
-                    dfs(nei_r, nei_c, visited)
-        
+        res = []
+
+        def valid(r,c):
+            queue = deque([(r,c)])
+            visited = set()
+            _dir = [(-1, 0), (1, 0), (0, 1), (0, -1)]
+            pac_found = atl_found = False
+            print(queue)
+            while queue:
+                r,c = queue.popleft()
+                curr_height = heights[r][c]
+                if r == 0 or c == 0:
+                    pac_found = True
+                if r == rows -1 or c == cols - 1:
+                    atl_found = True
+
+                for dr, dc in _dir:
+                    new_r, new_c = r+dr, c+dc
+                    if 0<=new_r<rows and 0<=new_c<cols and heights[new_r][new_c] <= curr_height and (new_r, new_c) not in visited:
+                        visited.add((new_r, new_c))
+                        queue.append((new_r, new_c))
+            return pac_found and atl_found
+
         for r in range(rows):
-            dfs(r,0,p)
-        for c in range(cols):
-            dfs(0,c,p)
-        
-        for r in range(rows):
-            dfs(r,cols-1,a)
-        for c in range(cols):
-            dfs(rows-1,c,a)
-        print(list(p&a))
-        return list(p & a)
+            for c in range(cols):
+                if valid(r,c):
+                    res.append((r,c))
+        return res
