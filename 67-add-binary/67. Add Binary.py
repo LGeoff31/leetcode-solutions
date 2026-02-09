3d8e477d04298f5268ecde08447de8f27ed13a69
Geoffrey Lee <geoffrey31415@gmail.com>
2026-02-09 15:53:24 -0500

    Runtime: 0 ms (100.00%), Memory: 19.6 MB (10.86%) - Gitcode

--- 67-add-binary/67. Add Binary.py
+++ 67-add-binary/67. Add Binary.py
@@ -1,12 +1,3 @@
 class Solution:
     def addBinary(self, a: str, b: str) -> str:
-        def val(bin_str):
-            res = 0
-            for i in range(len(bin_str)):
-                if bin_str[i] == "1":
-                    res += 2 ** (len(bin_str) - i - 1)
-            return res
-        print(val(a))
-        print(val(b))
-
-        return bin(val(a) + val(b))[2:]
+        return bin(int(a, 2) + int(b, 2))[2:]
