38291e35e7c16018742aff8eefa5092c5735516d
Geoffrey Lee <geoffrey31415@gmail.com>
2026-02-11 12:26:41 -0500

    Runtime: 429 ms (5.23%), Memory: 19.8 MB (24.73%) - Gitcode

--- 2085-count-common-words-with-one-occurrence/2085. Count Common Words With One Occurrence.py
+++ 2085-count-common-words-with-one-occurrence/2085. Count Common Words With One Occurrence.py
@@ -1,4 +1,3 @@
 class Solution:
     def countWords(self, words1: List[str], words2: List[str]) -> int:
-        a,b = Counter(words1), Counter(words2)
-        return sum(a[word] == 1 and word in b and b[word] == 1 for word in a)
+        return sum(Counter(words1)[word] == 1 and word in Counter(words2) and Counter(words2)[word] == 1 for word in Counter(words1))
