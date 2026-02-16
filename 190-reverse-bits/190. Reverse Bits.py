cb1f6499e0a2e4b36f3875664fe9ec9822ede800
Geoffrey Lee <geoffrey31415@gmail.com>
2026-02-15 19:06:26 -0500

    Runtime: 33 ms (93.67%), Memory: 19.3 MB (22.24%) - Gitcode

--- 190-reverse-bits/190. Reverse Bits.py
+++ 190-reverse-bits/190. Reverse Bits.py
@@ -1,11 +1,3 @@
 class Solution:
     def reverseBits(self, n: int) -> int:
-        mask = 1 << 31
-        result = 0
-        for i in range(32):
-            if n & mask:
-                result += 2**(i)
-            mask >>= 1
-
-        return result
-        
+        return int(("0" * (32 - len(bin(n)[2:])) + bin(n)[2:])[::-1], 2)
