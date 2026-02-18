f380b6d0d2bdaee70977507dd9a02e19ef3be8d7
Geoffrey Lee <geoffrey31415@gmail.com>
2026-02-18 02:04:38 -0500

    Runtime: 0 ms (100.00%), Memory: 19.5 MB (25.94%) - Gitcode

--- 693-binary-number-with-alternating-bits/693. Binary Number with Alternating Bits.py
+++ 693-binary-number-with-alternating-bits/693. Binary Number with Alternating Bits.py
@@ -1,7 +1,3 @@
 class Solution:
     def hasAlternatingBits(self, n: int) -> bool:
-        b = bin(n)[2:]
-        for i in range(1, len(b)):
-            if b[i] == b[i-1]:
-                return False
-        return True
+        return not(any(bin(n)[2:][i] == bin(n)[2:][i-1] for i in range(1, len(bin(n)[2:]))))
