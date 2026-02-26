81c206ebaa307162797731886c468f223ce5b393
Geoffrey Lee <geoffrey31415@gmail.com>
2026-02-26 01:05:25 -0500

    Runtime: 0 ms (100.00%), Memory: 19.5 MB (21.65%) - Gitcode

--- 1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one/1404. Number of Steps to Reduce a Number in Binary Representation to One.py
+++ 1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one/1404. Number of Steps to Reduce a Number in Binary Representation to One.py
@@ -1,11 +1,20 @@
 class Solution:
     def numSteps(self, s: str) -> int:
+        """
+        1101 -> 
+        1110 
+        0111
+        1000
+        0100
+        0010
+        0001
+        """
         a = int(s, 2)
         cnt = 0
         while a != 1:
-            if a % 2 == 0:
-                a //= 2
-            else:
+            if a & 1:
                 a += 1
+            else:
+                a >>= 1
             cnt += 1
         return cnt
