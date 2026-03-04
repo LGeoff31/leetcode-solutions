1a3e2372bef743b7b467bd822aae8b39496f50fc
Geoffrey Lee <geoffrey31415@gmail.com>
2026-03-04 17:43:30 -0500

    Runtime: 3 ms (27.82%), Memory: 97.5 MB (6.28%) - Gitcode

--- 121-best-time-to-buy-and-sell-stock/121. Best Time to Buy and Sell Stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/121. Best Time to Buy and Sell Stock.cpp
@@ -1,16 +1,33 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        int globalMax = 0, l = 0, r = 1;
-        while (r < prices.size()) {
-            while (r < prices.size() && (prices[r] - prices[l]) > 0) {
-                globalMax = max(globalMax, prices[r] - prices[l]);
-                r++;
-            }
-            l = r;
-            r++;
-        }
-        return globalMax;
+        /*
+        int (32 bits) -> -2e9 - 2e9
+        long long (64 bits) -> -9e18 - 9e18
+        double -> floating point
+        float -> less precision
 
+        length(array) = array.length
+        length(string) = string.size()
+        array<str,5>
+        pair<int, int>
+        unordered_map<K,V>
+        map<K,V>
+        unordered_set<T>, set<T>
+        queue<T>
+        deque<T>
+        stack<T>
+        priority_queue<T>
+        INT_MIN
+        INT_MAX
+        size_t -> unsighted
+        */
+        int min_stock = INT_MAX;
+        int max_profit = 0;
+        for (auto p : prices) {
+            max_profit = max(max_profit, p - min_stock);
+            min_stock = min(min_stock, p);
+        }
+        return max_profit;
     }
 };
