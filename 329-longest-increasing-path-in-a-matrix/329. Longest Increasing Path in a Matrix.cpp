1e78d06f1875391e13a52155314ed40f4f3ae0c1
Geoffrey Lee <geoffrey31415@gmail.com>
2026-03-09 21:20:32 -0400

    Runtime: 71 ms (14.20%), Memory: 32.4 MB (15.84%) - Gitcode

--- 329-longest-increasing-path-in-a-matrix/329. Longest Increasing Path in a Matrix.cpp
+++ 329-longest-increasing-path-in-a-matrix/329. Longest Increasing Path in a Matrix.cpp
@@ -1,24 +1,25 @@
 class Solution {
 public:
     struct KeyHash {
-        size_t operator()(const tuple<int,int,int>& key) const {
-            auto [a,b,c] = key;
-            return hash<int>()(a) ^ (hash<int>()(b) << 1) ^ (hash<int>()(c) << 2);
-        }
+        size_t operator()(const tuple<int,int>& key) const {
+            auto [a,b] = key;
+            return hash<int>()(a) ^ (hash<int>()(b) << 1);
+        } 
     };
+
     vector<pair<int, int>> dir = {{-1, 0}, {1, 0}, {0,1}, {0,-1}};
-    unordered_map<tuple<int, int, int>, int, KeyHash> memo;
-    int dfs(int r, int c, int prevVal, vector<vector<int>>& matrix, int rows, int cols) {
+    unordered_map<tuple<int, int>, int, KeyHash> memo;
+    int dfs(int r, int c, vector<vector<int>>& matrix, int rows, int cols) {
+        if (memo.count({r,c})) return memo[{r,c}];
         int res = 0;
-        if (memo.count({r,c,prevVal})) return memo[{r,c,prevVal}];
         for (auto [dr, dc] : dir) {
             int new_r = r + dr;
             int new_c = c + dc;
-            if (0 <= new_r && new_r < rows && 0 <= new_c && new_c < cols && matrix[new_r][new_c] > prevVal) {
-            res = max(res, 1 + dfs(new_r, new_c, matrix[new_r][new_c], matrix, rows, cols));
+            if (0 <= new_r && new_r < rows && 0 <= new_c && new_c < cols && matrix[new_r][new_c] > matrix[r][c]) {
+            res = max(res, 1 + dfs(new_r, new_c, matrix, rows, cols));
             }
         }
-        memo[{r,c,prevVal}] = res;
+        memo[{r,c}] = res;
         return res;
     }
     int longestIncreasingPath(vector<vector<int>>& matrix) {
@@ -26,10 +27,9 @@ public:
         int res = 0;
         for (size_t r = 0; r < matrix.size(); r++) {
             for (size_t c = 0; c < matrix[0].size(); c++) {
-            res = max(res, dfs(r, c, -1, matrix, matrix.size(), matrix[0].size()));
+            res = max(res, dfs(r, c, matrix, matrix.size(), matrix[0].size()));
             }
         }
-        
-        return res;
+        return res + 1;
     }
 };
