0442d262467167160795ab9fd57dfad85fdfcf91
Geoffrey Lee <geoffrey31415@gmail.com>
2026-03-12 17:49:24 -0400

    Runtime: 9 ms (14.05%), Memory: 23.4 MB (8.81%) - Gitcode

--- 337-house-robber-iii/337. House Robber III.py
+++ 337-house-robber-iii/337. House Robber III.py
@@ -6,18 +6,16 @@
 #         self.right = right
 class Solution:
     def rob(self, root: Optional[TreeNode]) -> int:
-        # DP: At each node, you want to return the maximum amount of money you can get with the subtree for both (Taking that node vs Not taking that node)
         @cache
-        def dfs(node, take): # If take is true, then you're taking the current node # O(n)
+        def dfs(node, prevTaken):
             if not node:
                 return 0
             
-            if take:
-                return node.val + dfs(node.left, not take) + dfs(node.right, not take)
-            else:
-                return max(dfs(node.left, not take) + dfs(node.right, not take), dfs(node.left, take) + dfs(node.right, not take), dfs(node.left, not take) + dfs(node.right, take), dfs(node.left, take) + dfs(node.right, take))
+            if prevTaken:
+                return dfs(node.left, False) + dfs(node.right, False)
             
-        
-        return max(dfs(root, False), dfs(root, True))
+            take = node.val + dfs(node.left, True) + dfs(node.right, True)
+            skip = dfs(node.left, False) + dfs(node.right, False)
+            return max(take, skip)
 
-        
+        return dfs(root, False)
