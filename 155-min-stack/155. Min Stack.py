33fa9cf50348a52309148cfc402b1e116c49f845
Geoffrey Lee <geoffrey31415@gmail.com>
2026-03-21 23:55:10 -0400

    Runtime: 0 ms (100.00%), Memory: 22.4 MB (90.94%) - Gitcode

--- 155-min-stack/155. Min Stack.py
+++ 155-min-stack/155. Min Stack.py
@@ -1,49 +1,27 @@
-class Node:
-    def __init__(self, value, prev, minElem):
-        self.prev = prev
-        self.val = (value, minElem)
-
 class MinStack:
+
     def __init__(self):
-        self.tail = None
+        self.stack = []
+        self.minstack = [] 
 
     def push(self, val: int) -> None:
-        if self.tail is None:
-            self.tail = Node(val, None, val)
+        self.stack.append(val)
+        if self.minstack: 
+            self.minstack.append(min(val, self.minstack[-1]))
         else:
-            newNode = Node(val, self.tail, min(val, self.getMin()))
-            newNode.prev = self.tail
-            self.tail = newNode
+            self.minstack.append(val)
 
     def pop(self) -> None:
-        self.tail = self.tail.prev
+        self.stack.pop()
+        self.minstack.pop() 
 
-    def top(self) -> int:   
-        return self.tail.val[0]    
+    def top(self) -> int:
+        return self.stack[-1]
 
     def getMin(self) -> int:
-        if self.tail:
-            return self.tail.val[1]
-        return None
+        return self.minstack[-1]
         
-"""
-0, 1, -2, -1
-Node
-value
-pointer to prev & next
-next smaller number
 
-linkedlist is head -> tail in order of the simulated stack
-min variable ; Node
-
-[-2, -8, -3, 12, 17, 0, -100]
-[-2, -8, -8, -8, -8, -8, -100]
-
-
-getMin -> -100
-pop 
-getMin -> -8
-"""
 
 # Your MinStack object will be instantiated and called as such:
 # obj = MinStack()
