2d25e8d7d3acc248d0aac39683d1e5f2b1d4d1ae
Geoffrey Lee <geoffrey31415@gmail.com>
2026-03-25 01:47:33 -0400

    Runtime: 171 ms (47.26%), Memory: 44.4 MB (54.79%) - Gitcode

--- 3546-equal-sum-grid-partition-i/3546. Equal Sum Grid Partition I.py
+++ 3546-equal-sum-grid-partition-i/3546. Equal Sum Grid Partition I.py
@@ -1,9 +1,44 @@
 class Solution:
     def canPartitionGrid(self, grid: List[List[int]]) -> bool:
-        # print(zip(*grid))
-        h = [sum(arr) for arr in grid]
-        prefix_h = list(accumulate(h))
-        v = [sum(col) for col in zip(*grid)]
-        prefix_v = list(accumulate(v))
-        totalSum = sum(grid[i][j] for i in range(len(grid)) for j in range(len(grid[i])))
-        return totalSum % 2 == 0 and (totalSum // 2 in prefix_h or totalSum // 2 in prefix_v)
+        """
+        O(10^5) -> O(n) or O(nlogn)
+        O(1000) -> O(n^2)
+        O(10) -> O(2^n)
+        """
+        rows, cols = len(grid), len(grid[0])
+
+        # Try all horizontal cuts
+        prefix = [] # [6, 10, 12] -> row1 = 6, row2 = 4, row3 = 2
+        for r in range(rows):
+            cnt = 0
+            for c in range(cols):
+                cnt += grid[r][c]
+            if not prefix:
+                prefix.append(cnt)
+            else:
+                prefix.append(prefix[-1] + cnt)
+        
+        total_sum = prefix[-1]
+        for r in range(rows):
+            TOP_HALF = prefix[r]
+            BOTTOM_HALF = total_sum - prefix[r]
+            if TOP_HALF == BOTTOM_HALF:
+                return True
+
+        # Try all vertical cuts
+        prefix = []
+        for c in range(cols):
+            cnt = 0
+            for r in range(rows):
+                cnt += grid[r][c]
+            if not prefix:
+                prefix.append(cnt)
+            else:
+                prefix.append(prefix[-1] + cnt)
+        toatl_sum = prefix[-1]
+        for c in range(cols):
+            TOP_HALF = prefix[c]
+            BOTTOM_HALF = total_sum - prefix[c]
+            if TOP_HALF == BOTTOM_HALF:
+                return True
+        return False
