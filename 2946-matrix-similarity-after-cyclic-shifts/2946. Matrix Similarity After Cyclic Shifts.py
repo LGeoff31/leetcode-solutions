e84ee9c19b17acebdea7ffe80c6525c879ee5761
Geoffrey Lee <geoffrey31415@gmail.com>
2026-03-27 03:26:12 -0400

    Runtime: 59 ms (8.24%), Memory: 19.6 MB (10.59%) - Gitcode

--- 2946-matrix-similarity-after-cyclic-shifts/2946. Matrix Similarity After Cyclic Shifts.py
+++ 2946-matrix-similarity-after-cyclic-shifts/2946. Matrix Similarity After Cyclic Shifts.py
@@ -1,8 +1,14 @@
 class Solution:
     def areSimilar(self, grid: List[List[int]], k: int) -> bool:
+        org_grid = copy.deepcopy(grid)
         rows, cols = len(grid), len(grid[0])
-        k%=cols
-        for row in grid:
-            if row != row[k:] + row[0:k]: return False
-        return True
-        
+        for i in range(k):
+            for r in range(rows):
+                row_copy = grid[r].copy()
+                if r % 2 == 0:
+                    row_copy = [row_copy[-1]] + row_copy[:-1]
+                else:
+                    row_copy = row_copy[1:] + [row_copy[0]]
+                grid[r] = row_copy
+            print(grid)
+        return grid == org_grid
