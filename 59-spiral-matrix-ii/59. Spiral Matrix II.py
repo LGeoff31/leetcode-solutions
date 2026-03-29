3b1ab6eebc235e362ac78a7d4371cb8fef65ebe8
Geoffrey Lee <geoffrey31415@gmail.com>
2026-03-29 18:39:24 -0400

    Runtime: 0 ms (100.00%), Memory: 19.5 MB (31.71%) - Gitcode

--- 59-spiral-matrix-ii/59. Spiral Matrix II.py
+++ 59-spiral-matrix-ii/59. Spiral Matrix II.py
@@ -1,34 +1,35 @@
 class Solution:
     def generateMatrix(self, n: int) -> List[List[int]]:
-        idx = 1
-        l, r = 0, n-1
-        t, d = 0, n-1
-
-        horz_increasing = True
-        vert_increasing = True
-
-        dp = [[0] * n for _ in range(n)]
-
-
-        while l <= r and t <= d:
-            for i in range(l, r+1):
-                dp[t][i] = idx
-                idx+=1
-            t+=1
-            for i in range(t, d+1):
-                dp[i][r] = idx
-                idx+=1
-            r -= 1
-            if l > r or t > d: break
-            for i in range(r, l-1, -1):
-                dp[d][i] = idx
-                idx+=1
-            d -=1
-            for i in range(d, t-1, -1):
-                dp[i][l] = idx
-                idx+=1
-            l+=1
-        return dp
-
-
-        
+        left, right = 0, n-1
+        top, bottom = 0, n-1
+        res = [[-1] * n for _ in range(n)]
+        cnt = 1
+        if n == 1: return [[1]]
+        while True:
+            # left to right
+            for l in range(left, right + 1):
+                res[top][l] = cnt
+                cnt += 1
+            print(cnt)
+            # right to bottom
+            for r in range(top + 1, bottom + 1):
+                res[r][right] = cnt
+                cnt += 1
+            print(cnt)
+            # right to left
+            for l in range(right-1, left-1, -1):
+                res[bottom][l] = cnt
+                cnt += 1
+            print(cnt)
+            # bottom to up
+            for r in range(bottom - 1, top, -1):
+                res[r][left] = cnt
+                cnt += 1
+            left += 1
+            right -= 1
+            top += 1
+            bottom -= 1
+            print('reached', left, right, bottom, top, cnt)
+            if cnt == n*n+1:
+                break
+        return res
