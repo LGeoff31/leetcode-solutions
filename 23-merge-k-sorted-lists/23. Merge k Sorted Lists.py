d50d30707c844fd28deb5932d30f5cef8112bf85
Geoffrey Lee <geoffrey31415@gmail.com>
2026-04-13 12:49:37 -0400

    Runtime: 32 ms (15.31%), Memory: 24.3 MB (6.04%) - Gitcode

--- 23-merge-k-sorted-lists/23. Merge k Sorted Lists.py
+++ 23-merge-k-sorted-lists/23. Merge k Sorted Lists.py
@@ -4,24 +4,48 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
-        minHeap = []
-        c = count()
-        for list_head in lists:
-            if list_head:
-                heappush(minHeap, (list_head.val, next(c), list_head))
-        
-        merged_list = ListNode()
-        head_list = merged_list
-        while minHeap:
-            val, _, node = heappop(minHeap)
-            merged_list.next = node
-            merged_list = merged_list.next
+    def mergeKLists(self, _lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        lists = []
+        for node in _lists:
+            a = []
+            start = node
+            while start:
+                a.append(start.val)
+                start = start.next 
+            lists.append(a)
+        def merge(l1, l2):
+            lst = []
+            p1, p2 = 0, 0
+            print(l1, l2)
+            while p1 < len(l1) and p2<len(l2):
+                if l1[p1] < l2[p2]:
+                    lst.append(l1[p1])
+                    p1 += 1
+                else:
+                    lst.append(l2[p2])
+                    p2 += 1
 
-            if node.next:
-                heappush(minHeap, (node.next.val, next(c), node.next))
-
-        return head_list.next
-        
-
-        
+            while p1<len(l1):
+                lst.append(l1[p1])
+                p1 += 1
+            while p2<len(l2):
+                lst.append(l2[p2])
+                p2 += 1
+            return lst
+        def mergeK(l, r):
+            if l < 0 or l >= len(lists):
+                return []
+            if l == r:
+                return lists[l]
+            mid = (l+r)//2
+            leftMerge = mergeK(l, mid)
+            rightMerge = mergeK(mid + 1, r)
+            return merge(leftMerge, rightMerge)
+        result = mergeK(0, len(lists) - 1)
+        res = ListNode()
+        curr = res
+        for elem in result:
+            curr.next = ListNode(elem)
+            curr = curr.next
+        print(result)
+        return res.next
