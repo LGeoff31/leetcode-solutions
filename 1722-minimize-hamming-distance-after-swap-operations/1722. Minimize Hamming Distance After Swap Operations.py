a0727a0afe366a5c7bfeef06f52d9905ad15f1d8
Geoffrey Lee <geoffrey31415@gmail.com>
2026-04-20 23:19:56 -0400

    Runtime: 935 ms (5.26%), Memory: 62.6 MB (37.59%) - Gitcode

--- 1722-minimize-hamming-distance-after-swap-operations/1722. Minimize Hamming Distance After Swap Operations.py
+++ 1722-minimize-hamming-distance-after-swap-operations/1722. Minimize Hamming Distance After Swap Operations.py
@@ -1,52 +1,34 @@
-class UnionFind:
-    def __init__(self):
-        self.parent = {}
-
-    def find(self, x):
-        if x not in self.parent:
-            self.parent[x] = x
-        
-        if self.parent[x] != x:
-            self.parent[x] = self.find(self.parent[x])
-        return self.parent[x]
-
-    def union(self, x, y):
-        px = self.find(x)
-        py = self.find(y)
-        if px == py:
-            return
-        
-        self.parent[px] = py
-
 class Solution:
-    def exists(self, sorted_arr, val):
-        idx = bisect_left(sorted_arr, val)
-        if idx >= len(sorted_arr):
-            return False 
-        return sorted_arr[idx] == val
-
     def minimumHammingDistance(self, source: List[int], target: List[int], allowedSwaps: List[List[int]]) -> int:
-        uf = UnionFind()
-        for u,v in allowedSwaps:
-            uf.union(u, v)
-        if not uf.parent:
-            return sum(target[i] != source[i] for i in range(len(target)))
-
-        dic = defaultdict(SortedList)
-
-        for i in range(len(source)):
-            root = uf.find(i)
-            dic[root].add(source[i])
-
-        print(uf.parent)
-        print(dic)
-        res = 0
-        for i in range(len(source)):
-            root = uf.find(i)
-            valid_options = dic[root]
-            if self.exists(valid_options, target[i]):
-                valid_options.remove(target[i])
-            else:
-                res += 1
+        parent = list(range(len(source)))
+        def find(a):
+            if parent[a] != a:
+                parent[a] = find(parent[a])
+            return parent[a]
         
-        return res
+        def union(a,b):
+            parent[find(a)] = find(b)
+        
+        n = len(source)
+        parent = list(range(len(source)))
+        for e in allowedSwaps:
+            union(e[0], e[1])
+
+        d = defaultdict(dict)
+        for i in range(n):
+            p = find(i)
+            d[p][source[i]] = 1 + d[p].get(source[i], 0)
+        print(d)
+
+        ans = 0
+        for i in range(n):
+            p = find(i)
+            if target[i] in d[p] and d[p][target[i]] > 0:
+                d[p][target[i]] -= 1
+            else:
+                ans += 1
+
+        return ans
+
+
+        
