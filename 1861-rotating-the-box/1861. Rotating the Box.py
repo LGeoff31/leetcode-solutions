7b6a2b91cd89f07a48d1ec64b89285e233ba4b3b
Geoffrey Lee <geoffrey31415@gmail.com>
2026-05-05 20:13:59 -0400

    Runtime: 1419 ms (5.07%), Memory: 57 MB (33.27%) - Gitcode

--- 1861-rotating-the-box/1861. Rotating the Box.py
+++ 1861-rotating-the-box/1861. Rotating the Box.py
@@ -1,40 +1,28 @@
 class Solution:
     def rotateTheBox(self, box: List[List[str]]) -> List[List[str]]:
-        m = len(box)
-        n = len(box[0])
-        result = [["" for _ in range(m)] for _ in range(n)]
+        rows, cols = len(box), len(box[0])
+        box = [[box[r][c] for r in range(rows)][::-1] for c in range(cols)]
+        print(box)
+        def drop(c):
+            r = cols - 1
+            while r >= 0:
+                # basically go up until hit obstacle
+                new_r = r
+                gems = 0
+                while r >= 0 and box[r][c] != "*":
+                    if box[r][c] == "#":
+                        box[r][c] = "."
+                        gems += 1
+                    r -= 1
+                while new_r >= 0:
+                    if gems > 0:
+                        box[new_r][c] = "#"
+                        gems -= 1
+                    new_r -= 1
+                r -= 1
+                
+        for c in range(rows):
+            drop(c)
+        return box
 
-        # Create the transpose of the input grid in `result`
-        for i in range(n):
-            for j in range(m):
-                result[i][j] = box[j][i]
-
-        # Reverse each row in the transpose grid to complete the 90° rotation
-        for i in range(n):
-            result[i].reverse()
-
-        # Apply gravity to let stones fall to the lowest possible empty cell in each column
-        for j in range(m):
-            # Process each cell in column `j` from bottom to top
-            for i in range(n - 1, -1, -1):
-                if (
-                    result[i][j] == "."
-                ):  # Found an empty cell; check if a stone can fall into it
-                    next_row_with_stone = -1
-
-                    # Look for a stone directly above the empty cell `result[i][j]`
-                    for k in range(i - 1, -1, -1):
-                        if result[k][j] == "*":
-                            break  # Obstacle blocks any stones above
-                        if (
-                            result[k][j] == "#"
-                        ):  # Stone found with no obstacles in between
-                            next_row_with_stone = k
-                            break
-
-                    # If a stone was found above, let it fall into the empty cell `result[i][j]`
-                    if next_row_with_stone != -1:
-                        result[next_row_with_stone][j] = "."
-                        result[i][j] = "#"
-
-        return result
+        return 
