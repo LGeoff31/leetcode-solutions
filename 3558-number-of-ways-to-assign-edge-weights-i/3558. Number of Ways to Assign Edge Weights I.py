07e997e823081955a312e5463a8bf85e0027c5d4
Geoffrey Lee <geoffrey31415@gmail.com>
2026-06-10 21:42:31 -0400

    Runtime: 405 ms (43.01%), Memory: 68.9 MB (64.52%) - Gitcode

--- 3558-number-of-ways-to-assign-edge-weights-i/3558. Number of Ways to Assign Edge Weights I.py
+++ 3558-number-of-ways-to-assign-edge-weights-i/3558. Number of Ways to Assign Edge Weights I.py
@@ -1,29 +1,21 @@
 class Solution:
     def assignEdgeWeights(self, edges: List[List[int]]) -> int:
+        MOD = 10 ** 9 + 7
         adj = defaultdict(list)
         for u,v in edges:
-           adj[u].append(v)
-           adj[v].append(u)
+            adj[u].append(v)
+            adj[v].append(u)
+        seen = {1}
+        queue = deque([1])
+        steps = 0
+        while queue:
+            for i in range(len(queue)):
+                node = queue.popleft()
+                for nei in adj[node]:
+                    if nei not in seen:
+                        seen.add(nei)
+                        queue.append(nei)
+            steps += 1
+        
+        return 2 ** (steps-2) % MOD
 
-        visited = set()
-        visited.add(1)
-
-        def maxHeight(node):
-            if len(adj[node]) == 1 and adj[node][0] in visited:
-                return 1
-            h = 0
-            for child_node in adj[node]:
-                if child_node in visited:
-                    continue
-                visited.add(child_node)
-                h = max(h, 1 + maxHeight(child_node))
-            return h
-
-        h = maxHeight(1) 
-        print('reached', h)
-        MOD = 10 ** 9 + 7
-        res = 0
-        # for number_ones in range(1, h+1, 2):
-        #     res = (res+comb(h, number_ones)) % MOD
-
-        return 2**(h-2) % MOD
