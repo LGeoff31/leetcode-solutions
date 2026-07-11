36171ac6063314cec076035acfc8a91ae9cddec5
Geoffrey Lee <geoffrey31415@gmail.com>
2026-07-11 11:50:07 -0400

    Runtime: 351 ms (22.41%), Memory: 20.1 MB (92.71%) - Gitcode

--- 127-word-ladder/127. Word Ladder.py
+++ 127-word-ladder/127. Word Ladder.py
@@ -1,32 +1,31 @@
 class Solution:
     def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
-        visited = set()
-        if beginWord=="a" and endWord=="c": return 2
-        if beginWord=="hot" and endWord =="dog" and len(wordList) == 2: return 0
-        elif beginWord=="hot" and endWord =="dog": return 3
-        if beginWord=="hbo" and endWord=="qbx": return 4
-        queue = deque([beginWord])
+        """ 
+        Idea: Create a graph where a node represents a word, and its edges are all the 
+        other nodes which differ by a single letter
 
-        def diff(s1, s2):
-            count = 0
-            for i in range(len(s1)):
-                count+=s1[i]!=s2[i]
-            return count
-        moves = 1
+        1. brute force, wordList <= 10 but O(n^2)
+        2. put all words in a freq hashmap, then look over each diff = 2
+        """
+        if endWord not in wordList:
+            return 0
+        word_list_set = set(wordList)
+        
+        queue = deque([beginWord])
+        seen = {beginWord}
+        cnt = 0
         while queue:
             for i in range(len(queue)):
                 word = queue.popleft()
-                if word == endWord: return moves
-                if word not in visited:
-                    visited.add(word)
-                    for j in wordList:
-                        if diff(j, word) == 1 and j not in visited:
-                            print(j)
-                            queue.append(j)
-                            wordList.remove(j)
-            moves+=1
-            
-
-
+                if word == endWord:
+                    return cnt + 1
+                
+                for i in range(len(word)):
+                    for ch in range(26):
+                        new_word = word[:i] + chr(ch + ord('a')) + word[i+1:]
+                        if new_word in word_list_set and new_word not in seen:
+                            seen.add(new_word)
+                            queue.append(new_word)
+            cnt += 1
         return 0
-        
+
