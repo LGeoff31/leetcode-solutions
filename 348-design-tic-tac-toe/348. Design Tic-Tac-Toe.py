9ed28dcaf880a27e778750db535892d86a3f6948
Geoffrey Lee <geoffrey31415@gmail.com>
2026-07-11 12:07:41 -0400

    Runtime: 5687 ms (5.09%), Memory: 21.6 MB (20.11%) - Gitcode

--- 348-design-tic-tac-toe/348. Design Tic-Tac-Toe.py
+++ 348-design-tic-tac-toe/348. Design Tic-Tac-Toe.py
@@ -1,35 +1,69 @@
 class TicTacToe:
+    """
+    0 -> none
+    1 -> player 1
+    2 -> player 2
+
+    0 2
+    1 2
+
+    """
+    def check_game_status(self, board):
+        # DIGONAL CHECKS
+        curr = board[0][0]
+        DIAGONAL_TOP_LEFT_BOTTOM_RIGHT = True
+        for i in range(self.n):
+            if board[i][i] != curr or curr == 0:
+                DIAGONAL_TOP_LEFT_BOTTOM_RIGHT = False 
+        curr = board[-1][0]
+        DIAGONAL_TOP_RIGHT_BOTTOM_LEFT = True
+        for i in range(self.n):
+            if board[i][self.n - i - 1] != curr or curr == 0:
+                DIAGONAL_TOP_RIGHT_BOTTOM_LEFT = False
+
+        # HORIZONTAL CHECKS
+        HORZONTAL_WIN = False
+        for r in range(self.n):
+            curr =  board[r][0]
+            HORZONTAL_WIN_ROW = True
+            for c in range(self.n):
+                if curr == 0 or board[r][c] != curr:
+                    HORZONTAL_WIN_ROW = False 
+            if HORZONTAL_WIN_ROW:
+                HORZONTAL_WIN = True 
+        
+        # VERTICAL CHECKS
+        """
+        0 2
+        1 2
+        """
+        VERTICAL_WIN = False
+        for c in range(self.n):
+            curr = board[0][c]
+            VERTICAL_WIN_ROW = True
+            for r in range(self.n):
+                if curr == 0 or board[r][c] != curr:
+                    VERTICAL_WIN_ROW = False 
+            if VERTICAL_WIN_ROW:
+                VERTICAL_WIN = True 
+
+        if DIAGONAL_TOP_LEFT_BOTTOM_RIGHT or DIAGONAL_TOP_RIGHT_BOTTOM_LEFT or HORZONTAL_WIN or VERTICAL_WIN:
+            return [True, "win"]
+        if all(board[r][c] != 0 for r in range(self.n) for c in range(self.n)):
+            return [True, "tie"]
+        return [False, "in progress"]
+
     def __init__(self, n: int):
         self.n = n
-        self.rows1 = [0] * self.n
-        self.cols1 = [0] * self.n
-        self.rows2 = [0] * self.n
-        self.cols2 = [0] * self.n
-        self.diagonal1_tl_br = 0
-        self.diagonal1_tr_bl = 0
-        self.diagonal2_tl_br = 0
-        self.diagonal2_tr_bl = 0
-
+        self.board = [[0] * n for _ in range(n)] 
+        
 
     def move(self, row: int, col: int, player: int) -> int:
-        if player == 1:
-            self.rows1[row] += 1
-            self.cols1[col] += 1
-            if row == col:
-                self.diagonal1_tl_br += 1
-            if row == self.n - col - 1:
-                self.diagonal1_tr_bl += 1
-            if max(self.rows1[row], self.cols1[col], self.diagonal1_tl_br, self.diagonal1_tr_bl) == self.n:
-                return 1
-        else:
-            self.rows2[row] += 1
-            self.cols2[col] += 1
-            if row == col:
-                self.diagonal2_tl_br += 1
-            if row == self.n - col - 1:
-                self.diagonal2_tr_bl += 1
-            if max(self.rows2[row], self.cols2[col], self.diagonal2_tl_br, self.diagonal2_tr_bl) == self.n:
-                return 2
+        self.board[row][col] = player
+        status, quote = self.check_game_status(self.board)
+        if status:
+            if quote == "win":
+                return player
         return 0
 
 
