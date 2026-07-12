919373e5f527e52accc62037df4225ababe21972
Geoffrey Lee <geoffrey31415@gmail.com>
2026-07-11 23:56:24 -0400

    Runtime: 1005 ms (17.36%), Memory: 67.2 MB (18.73%) - Gitcode

--- 123-best-time-to-buy-and-sell-stock-iii/123. Best Time to Buy and Sell Stock III.py
+++ 123-best-time-to-buy-and-sell-stock-iii/123. Best Time to Buy and Sell Stock III.py
@@ -3,18 +3,21 @@ class Solution:
         """
         At each index, you can either BUY (if not in transaction and < 2), SELL (if in transaction), NOTHING
         def dfs(i, transactions_left, free'd)
+        dp[i][transactions_left][ready_to_buy] = max amount achieved at index i with transactions_left and ready_to_buy booleean
         """
-        @cache
-        def dfs(i, transactions_left, ready_to_buy):
-            if i == len(prices):
-                return 0
-            
-            res = 0
-            if ready_to_buy:
-                if transactions_left > 0:
-                    res = max(res, dfs(i+1, transactions_left-1, not ready_to_buy) - prices[i])
-            else:
-                res = max(res, dfs(i+1, transactions_left, not ready_to_buy) + prices[i])
-            res = max(res, dfs(i+1, transactions_left, ready_to_buy))
-            return res
-        return dfs(0, 2, True)
+
+        n = len(prices)
+        dp = [[[0, 0] for _ in range(3)] for _ in range(n+1)]
+        for i in range(n-1, -1, -1):
+            for t in range(3):
+                for r in range(2):
+                    res = 0
+                    if r:
+                        if t > 0:
+                            res = max(res, dp[i+1][t-1][0] - prices[i])
+                    else:
+                        res = max(res, dp[i+1][t][1] + prices[i])
+                    res = max(res, dp[i+1][t][r])
+                    dp[i][t][r] = res
+
+        return dp[0][2][1]
