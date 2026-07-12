e0bcc62bae9207ad0a353df59d1fca7cc0f1b5af
Geoffrey Lee <geoffrey31415@gmail.com>
2026-07-12 00:25:55 -0400

    Runtime: 7577 ms (5.80%), Memory: 24.1 MB (11.75%) - Gitcode

--- 124-binary-tree-maximum-path-sum/124. Binary Tree Maximum Path Sum.py
+++ 124-binary-tree-maximum-path-sum/124. Binary Tree Maximum Path Sum.py
@@ -1,18 +1,30 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 class Solution:
     def maxPathSum(self, root: Optional[TreeNode]) -> int:
-        res = -10**9
-
+        """
+        The largest path sum 
+        """
+        self.max_element = -1e9
         def dfs(node):
-            nonlocal res
             if not node:
                 return 0
 
-            left = max(dfs(node.left), 0)
-            right = max(dfs(node.right), 0)
-
-            res = max(res, node.val + left + right)
-
-            return node.val + max(left, right)
-
-        dfs(root)
-        return res
+            return max(max(max(dfs(node.left), dfs(node.right)), 0) + node.val, 0)
+        self.res = 0
+        def traverse_tree(node):
+            if not node:
+                return
+            self.max_element = max(self.max_element, node.val)
+            self.res = max(self.res, node.val + dfs(node.left) + dfs(node.right))
+            traverse_tree(node.left)
+            traverse_tree(node.right)
+        traverse_tree(root)
+        print(self.max_element)
+        if self.res == 0:
+            return self.max_element
+        return self.res
