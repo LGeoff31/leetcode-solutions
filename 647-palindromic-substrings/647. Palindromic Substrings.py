68577544a6f935235f5f6ff4c66da964af1dd18c
Geoffrey Lee <geoffrey31415@gmail.com>
2026-07-12 20:53:44 -0400

    Runtime: 123 ms (62.80%), Memory: 19.3 MB (66.44%) - Gitcode

--- 647-palindromic-substrings/647. Palindromic Substrings.py
+++ 647-palindromic-substrings/647. Palindromic Substrings.py
@@ -1,32 +1,20 @@
 class Solution:
     def countSubstrings(self, s: str) -> int:
-        # DP
-        # Let's say we know a substirng is currently palindromic, let's expand 1 character to the left and right and if there equal, then boom
-        # dp[i][j] = True if s[i:j] is palindromic otherwise False
-        # Loop through this 2 dimensional dp grid and return the # of True's
-        # We know dp[i][i] = True for 0 <= i < len(s)
-        # For each i, we will say while (i-j, i+j) are equal, dp[i-j] = dp[i+j] = True, note that i-j >= 0 and i+j < len(s)
-        # This will indeed find all palindromic substrings of odd length in O(n^2) time 
-        # What about even length?
-        # Run similar algorithm but now, the start will be dp[i][i+1] = True if s[i] == s[i+1], i+1 < len(s)
-        n = len(s)
-        # dp = [[False] * n for _ in range(n)]
-        # for i in range(n):
-        #     dp[i][i] = True
-        res = 0
-        for i in range(n):
-            curr = 1
-            # Odd length
-            j = 1
-            while i-j >= 0 and i+j < n and s[i-j] == s[i+j]:
-                curr += 1
+        """
+        dp[i][j] = # palindromic substrings from s[i:j]
+        if s[i-1] == s[j+1]: dp[i][j] = 
+
+        """
+        def expand(i, j):
+            res = 0
+            while i >= 0 and j < len(s) and s[i] == s[j]:
+                res += 1
+                i -= 1
                 j += 1
-            j = 1
-            # Even length
-            if i+1 < n and s[i] == s[i+1]:
-                curr += 1
-                while i-j >= 0 and i+1+j < n and s[i-j] == s[i+j+1]:
-                    curr += 1
-                    j += 1
-            res += curr
+            return res
+        
+        res = 0
+        for i in range(len(s)):
+            res += expand(i, i)
+            res += expand(i, i+1)
         return res
