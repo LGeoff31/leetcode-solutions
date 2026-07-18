636d95bce0e56c851de67cc5b990a68cf89e6f5d
Geoffrey Lee <geoffrey31415@gmail.com>
2026-07-18 13:59:09 -0400

    Runtime: 150 ms (56.63%), Memory: 21.1 MB (9.04%) - Gitcode

--- 3223-minimum-length-of-string-after-operations/3223. Minimum Length of String After Operations.py
+++ 3223-minimum-length-of-string-after-operations/3223. Minimum Length of String After Operations.py
@@ -1,14 +1,14 @@
 class Solution:
     def minimumLength(self, s: str) -> int:
-        if len(s) <= 2:
-            return len(s)
-        
-        dic = Counter(s)
-        res = 0
-        freq = dic.values()
-        for val in freq:
-            if val%2:
-                res += 1
-            else:
-                res +=2
-        return res
+        """ 
+        1 -> 1
+        2 -> 2
+        3 -> 1
+        4 -> 2
+        5 -> 1 
+        6 -> 2
+
+
+        """
+        character_frequency = Counter(s)
+        return sum((2 if character_frequency[c] % 2 == 0 else 1) for c in character_frequency)
