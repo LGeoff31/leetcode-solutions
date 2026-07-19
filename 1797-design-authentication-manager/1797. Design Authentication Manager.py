d46d7eff4443fc005b6ba29a422ef00b6811ad76
Geoffrey Lee <geoffrey31415@gmail.com>
2026-07-19 02:53:46 -0400

    Runtime: 9 ms (94.49%), Memory: 21 MB (38.40%) - Gitcode

--- 1797-design-authentication-manager/1797. Design Authentication Manager.py
+++ 1797-design-authentication-manager/1797. Design Authentication Manager.py
@@ -1,33 +1,56 @@
+class Node:
+    def __init__(self, expiryTime):
+        self.prev = None
+        self.next = None
+        self.expiryTime = expiryTime
+
 class AuthenticationManager:
     def __init__(self, timeToLive: int):
+        self.currentSize = 0
         self.timeToLive = timeToLive
-        self.token_to_expiry = {}
-        self.lst = SortedList()
+        self.mapping = {}
+        self.head = None
+        self.tail = None
+
+    def _append(self, node):
+        if self.tail is None:
+            self.head = self.tail = node
+        else:
+            node.prev = self.tail
+            self.tail.next = node
+            self.tail = node
 
     def generate(self, tokenId: str, currentTime: int) -> None:
-        self.token_to_expiry[tokenId] = currentTime + self.timeToLive
+        node = Node(currentTime + self.timeToLive)
+        self._append(node)
+        self.mapping[tokenId] = node
+        self.currentSize += 1
 
     def renew(self, tokenId: str, currentTime: int) -> None:
-        if tokenId not in self.token_to_expiry:
-            return None
-        
-        previous_expiry_time = self.token_to_expiry[tokenId]
-        if previous_expiry_time <= currentTime:
+        if tokenId not in self.mapping or self.mapping[tokenId].expiryTime <= currentTime:
             return
 
-        self.token_to_expiry[tokenId] = currentTime + self.timeToLive
+        node = self.mapping[tokenId]
+        prevNode, nxtNode = node.prev, node.next
+        if prevNode:
+            prevNode.next = nxtNode
+        else:
+            self.head = nxtNode
+        if nxtNode:
+            nxtNode.prev = prevNode
+        else:
+            self.tail = prevNode
+
+        newNode = Node(currentTime + self.timeToLive)
+        self._append(newNode)
+        self.mapping[tokenId] = newNode  # important: update mapping
 
     def countUnexpiredTokens(self, currentTime: int) -> int:
-        cnt = 0
-        for key, value in self.token_to_expiry.items():
-            if value > currentTime:
-                cnt += 1
-        return cnt
-        
-
-
-# Your AuthenticationManager object will be instantiated and called as such:
-# obj = AuthenticationManager(timeToLive)
-# obj.generate(tokenId,currentTime)
-# obj.renew(tokenId,currentTime)
-# param_3 = obj.countUnexpiredTokens(currentTime)
+        while self.head and self.head.expiryTime <= currentTime:
+            self.head = self.head.next
+            if self.head:
+                self.head.prev = None
+            else:
+                self.tail = None
+            self.currentSize -= 1
+        return self.currentSize
