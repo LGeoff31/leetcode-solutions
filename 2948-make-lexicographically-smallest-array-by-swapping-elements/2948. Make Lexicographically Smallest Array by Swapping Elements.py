a152a200b070aec803f6ea0b7c5ec9e676c1cc8b
Geoffrey Lee <geoffrey31415@gmail.com>
2026-08-29 11:49:01 -0400

    Runtime: 1031 ms (5.22%), Memory: 72.4 MB (26.86%) - Gitcode

--- 2948-make-lexicographically-smallest-array-by-swapping-elements/2948. Make Lexicographically Smallest Array by Swapping Elements.py
+++ 2948-make-lexicographically-smallest-array-by-swapping-elements/2948. Make Lexicographically Smallest Array by Swapping Elements.py
@@ -1,36 +1,52 @@
+class UnionFind:
+    def __init__(self):
+        self.parent = {}
+        self.size = {}
+    
+    def union(self, a, b):
+        parent_a = self.find(a)
+        parent_b = self.find(b)
+        if parent_a == parent_b:
+            return 
+
+        if self.size[parent_a] > self.size[parent_b]:
+            self.size[parent_a] += self.size[parent_b]
+            self.parent[parent_b] = parent_a
+        else:
+            self.size[parent_b] += self.size[parent_a]
+            self.parent[parent_a] = parent_b
+    
+    def find(self, a):
+        self.parent.setdefault(a, a)
+        self.size.setdefault(a, 1)
+
+        if a != self.parent[a]:
+            self.parent[a] = self.find(self.parent[a])
+        return self.parent[a]
+
 class Solution:
     def lexicographicallySmallestArray(self, nums: List[int], limit: int) -> List[int]:
-        a = []
-        for i in range(len(nums)):
-            a.append((nums[i], i))
-        a.sort()
-        lst = []
-        curr = []
-        idx = 0
-        # Create disjoint sets
-        while idx < len(a):
-            if not curr:
-                curr.append((a[idx][0], a[idx][1]))
-                idx += 1
-                continue
+        formatted_nums = [(n, i) for i, n in enumerate(nums)]
+        formatted_nums.sort()
+        union_find = UnionFind()
+        union_find.union(0, 0)
 
-            if abs(a[idx][0] - curr[-1][0]) <= limit:
-                curr.append((a[idx][0], a[idx][1]))
-            else:
-                lst.append(curr)
-                curr = [(a[idx][0], a[idx][1])]
-            idx += 1
-        if curr:
-            lst.append(curr)
+
+        for i in range(1, len(formatted_nums)):
+            if formatted_nums[i][0] - formatted_nums[i-1][0] <= limit:
+                union_find.union(formatted_nums[i][1], formatted_nums[i-1][1])
+            union_find.union(i, i)
+        
+        sorted_values_by_group = defaultdict(list)
+        for idx, parent in union_find.parent.items():
+            sorted_values_by_group[parent].append((nums[idx], idx))
         res = [0] * len(nums)
-        for arr in lst:
-            idx_lst = []
-            for val, idx in arr:
-                idx_lst.append(idx)
-            idx_lst.sort()
-            for i in range(len(arr)):
-                res[idx_lst[i]] = arr[i][0]
-
-        print(lst)
+        for parent in sorted_values_by_group:
+            values = sorted_values_by_group[parent]
+            values.sort()
+            indexes = [idx for val, idx in values]
+            indexes.sort()
+            for i, idx in enumerate(indexes):
+                res[idx] = values[i][0]
 
         return res
