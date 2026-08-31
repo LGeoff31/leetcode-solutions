c1051ee13891bb301b5ee07b8ba177a053f65889
Geoffrey Lee <geoffrey31415@gmail.com>
2026-08-30 20:41:23 -0400

    Runtime: 137 ms (5.54%), Memory: 63.3 MB (23.68%) - Gitcode

--- 2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/2058. Find the Minimum and Maximum Number of Nodes Between Critical Points.py
+++ 2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/2058. Find the Minimum and Maximum Number of Nodes Between Critical Points.py
@@ -5,23 +5,37 @@
 #         self.next = next
 class Solution:
     def nodesBetweenCriticalPoints(self, head: Optional[ListNode]) -> List[int]:
-        lst = []
+        def is_critical_point(prev_node, curr, next_node):
+            if prev_node is None or next_node is None:
+                return False 
+            
+            return curr.val < min(prev_node.val, next_node.val) or curr.val > max(prev_node.val, next_node.val)
+
+        critical_node_indexes = []
+        prev_node = None
         curr = head
+        i = 0
+
         while curr:
-            lst.append(curr.val)
+            temp = curr
+            next_node = curr.next
+
+            if is_critical_point(prev_node, curr, next_node):
+                critical_node_indexes.append(i)
+
+            i += 1
+            prev_node = temp
             curr = curr.next
-        
-        points = []
-        for i in range(1, len(lst) - 1):
-            if lst[i] > max(lst[i-1], lst[i+1]) or lst[i] < min(lst[i-1], lst[i+1]):
-                points.append(i)
-        min_dist = 1e9
-        if not points:
+
+        if len(critical_node_indexes) < 2:
             return [-1, -1]
-        if len(points) == 1:
-            return [-1,-1]
-        for i in range(1, len(points)):
-            min_dist = min(min_dist, points[i] - points[i-1])
-        print(points)
-        return [min_dist, points[-1] - points[0]]
         
+        min_distance, max_distance = 1e9, critical_node_indexes[-1] - critical_node_indexes[0]
+        for i in range(1, len(critical_node_indexes)):
+            prev_idx = critical_node_indexes[i - 1]
+            curr_idx = critical_node_indexes[i]
+            min_distance = min(min_distance, curr_idx - prev_idx)
+        
+        return [min_distance, max_distance]
+        
+
