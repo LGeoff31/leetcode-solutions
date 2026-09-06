e711df77595fc5f6084df92f486446fb35fba7ec
Geoffrey Lee <geoffrey31415@gmail.com>
2026-09-06 11:10:09 -0400

    Runtime: 3 ms (6.71%), Memory: 19.4 MB (19.75%) - Gitcode

---
+++ 824-goat-latin/824. Goat Latin.py
@@ -0,0 +1,11 @@
+class Solution:
+    def toGoatLatin(self, sentence: str) -> str:
+        res = []
+        for i, word in enumerate(sentence.split()):
+            print(re.match(r'["aeiouAEIOU"]', word))
+            if re.match(r'["aeiouAEIOU"]', word):
+                res.append(word + "ma" + "a" * (i+1))
+            else:
+                res.append(word[1:] + word[0] + "ma" + "a" * (i+1))
+            
+        return " ".join(res)
