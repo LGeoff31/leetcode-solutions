445c6c8977d809c07e03128f38c19ab28741444c
Geoffrey Lee <geoffrey31415@gmail.com>
2026-09-10 20:09:05 -0400

    Runtime: 29 ms (38.24%), Memory: 19.5 MB (20.44%) - Gitcode

--- 3483-unique-3-digit-even-numbers/3483. Unique 3-Digit Even Numbers.py
+++ 3483-unique-3-digit-even-numbers/3483. Unique 3-Digit Even Numbers.py
@@ -1,11 +1,16 @@
 class Solution:
     def totalNumbers(self, digits: List[int]) -> int:
-        res = 0
-        for i in range(100, 1000):
-            valid = True
-            for d in str(i):
-                if digits.count(int(d)) < str(i).count(d) or int(str(i)[-1])%2==1:
-                    valid = False
-            if valid:
-                res += 1
-        return res
+        a = set()
+
+        for i in range(len(digits)):
+            for j in range(i+1, len(digits)):
+                for k in range(j+1, len(digits)):
+                    x,y,z = digits[i], digits[j], digits[k]
+                    if x != 0 and x!=0 and z%2 == 0:a.add(str(x) + str(y) + str(z))
+                    if x != 0 and x!=0 and y%2 == 0:a.add(str(x) + str(z) + str(y))
+                    if y != 0 and z %2==0: a.add(str(y) + str(x) + str(z))
+                    if y != 0 and x%2==0: a.add(str(y) + str(z) + str(x))
+                    if z != 0 and y%2==0: a.add(str(z) + str(x) + str(y))
+                    if z != 0 and x%2==0: a.add(str(z) + str(y) + str(x))
+        print(a)
+        return len(a)
