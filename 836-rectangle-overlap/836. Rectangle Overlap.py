0f10e0a47dc5d481afc5945d8f526f1dc5a06473
Geoffrey Lee <geoffrey31415@gmail.com>
2026-09-13 20:12:25 -0400

    Runtime: 0 ms (100.00%), Memory: 19.3 MB (59.18%) - Gitcode

--- 836-rectangle-overlap/836. Rectangle Overlap.py
+++ 836-rectangle-overlap/836. Rectangle Overlap.py
@@ -1,6 +1,5 @@
 class Solution:
     def isRectangleOverlap(self, rec1: List[int], rec2: List[int]) -> bool:
-        # Basically, find overlap with X, find overlap with Y, there should be both to return True
-        x_overlap = (rec1[0] <= rec2[0] < rec1[2]) or (rec2[0] <= rec1[0] < rec2[2])
-        y_overlap = (rec1[1] <= rec2[1] < rec1[3]) or (rec2[1] <= rec1[1] < rec2[3])
-        return x_overlap and y_overlap
+        if rec1 == rec2:
+            return True
+        return (rec1[0] < rec2[0] < rec1[2] or rec2[0] < rec1[0] < rec2[2] or rec1[0] < rec2[2] < rec1[2] or rec2[0] < rec1[2] < rec2[2]) and (rec1[1] < rec2[1] < rec1[3] or rec2[1] < rec1[1] < rec2[3] or rec1[1] < rec2[3] < rec1[3] or rec2[1] < rec1[3] < rec2[3])
