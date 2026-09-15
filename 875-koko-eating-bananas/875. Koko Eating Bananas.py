e9b1faf4a11e078f6d2a27b55ec47dfeb3a2daca
Geoffrey Lee <geoffrey31415@gmail.com>
2026-09-15 01:41:16 -0400

    Runtime: 179 ms (37.32%), Memory: 20.6 MB (44.37%) - Gitcode

--- 875-koko-eating-bananas/875. Koko Eating Bananas.py
+++ 875-koko-eating-bananas/875. Koko Eating Bananas.py
@@ -1,24 +1,37 @@
+import math
 class Solution:
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
-        if len(piles) == h:
-            return max(piles)
-        
-        minSpeed, maxSpeed = 1, max(piles)
+        # INITIAL thoughts
+        # if num piles == num hours, k must be max pile height
+        # [need some sort of initial guess]
+        # maybe can sort the array and then grab the middle
+        # compute num of hours to go through all piles by rounding up int division
+        # if >h, then increase k
+        # if <h, then decrease k
 
-        finalSpeed = float("inf")
-        while minSpeed <= maxSpeed:
-            middleSpeed = (minSpeed + maxSpeed) // 2
-            total_hours = 0
-            for pile in piles:
-                total_hours += pile // middleSpeed
-                total_hours += 1 if pile%middleSpeed!=0 else 0
+        # binary search on possible k vals????
+        # upper bound of k: max pile size
+        # lower bound of k: 1
 
-            if total_hours <= h:
-                finalSpeed = min(finalSpeed, middleSpeed)
-                maxSpeed = middleSpeed - 1 
-            else:
-                minSpeed = middleSpeed + 1
-        return finalSpeed
+        def compute_total_hours(test_k: int):
+            hours = 0
+            # round up
+            for p in piles:
+                hours += math.ceil(p/test_k)
 
+            return hours
+
+        s_piles = sorted(piles)
+        upp = s_piles[-1]
+        low = 1
+
+        while low < upp:
+            m = low + (upp - low)//2
+            test_h = compute_total_hours(m)
+            if test_h <= h:
+                upp = m
+            elif test_h > h:
+                low = m + 1
+        return low
 
         
