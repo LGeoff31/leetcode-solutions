13d01a93100b098a8d870693acd272f9fec25135
Geoffrey Lee <geoffrey31415@gmail.com>
2026-09-15 01:42:00 -0400

    Runtime: 251 ms (22.08%), Memory: 20.1 MB (9.36%) - Gitcode

--- 3-longest-substring-without-repeating-characters/3. Longest Substring Without Repeating Characters.py
+++ 3-longest-substring-without-repeating-characters/3. Longest Substring Without Repeating Characters.py
@@ -1,34 +1,20 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        letter_frequencies = {}
-        global_max = 0
+        seen = set()
+        res = 0
         l = 0
         r = 0
-        # s = "abcabcbb"
-        #              ^
-        # {a : 1, b: 2, c: 1}
+
         while r < len(s):
-            local_max = 0
-            duplicate_char = s[r]
-            # Continue expanding our window until we reach a duplicate
-            while r < len(s):
-                if s[r] in letter_frequencies:
-                    duplicate_char = s[r]
-                    letter_frequencies[s[r]] += 1
-                    break
-                
-                letter_frequencies[s[r]] = 1
+            if s[r] not in seen:
+                seen.add(s[r])
                 r += 1
-
-            local_max = r-l
-            global_max = max(local_max, global_max)
-
-            # Slide left pointer to make it a valid window
-            while letter_frequencies[duplicate_char] > 1:
-                letter_frequencies[s[l]] -= 1
-                if letter_frequencies[s[l]] == 0:
-                    del letter_frequencies[s[l]]
-                l += 1
-            r += 1
-
-        return global_max
+                res = max(res, r - l)
+            # continue moving l until all unique
+            else:
+                while s[r] in seen:
+                    seen.remove(s[l])
+                    l += 1
+        
+        return res
+                
