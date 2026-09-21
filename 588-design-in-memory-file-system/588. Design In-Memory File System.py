325b1cdf63396b0ceada6f04dede8b50d6f0d0fd
Geoffrey Lee <geoffrey31415@gmail.com>
2026-09-21 17:58:10 -0400

    Runtime: 4 ms (32.63%), Memory: 19.8 MB (80.48%) - Gitcode

--- 588-design-in-memory-file-system/588. Design In-Memory File System.py
+++ 588-design-in-memory-file-system/588. Design In-Memory File System.py
@@ -1,53 +1,46 @@
-class Node:
-    def __init__(self):
-        self.content = ""
-        self.children = {}
-
 class FileSystem:
     def __init__(self):
-        self.parent = Node()
+        self.root_directory = {}
+        self.content = {} # "/a/b": "Hello"
+        
+    def ls(self, path: str) -> list[str]:
+        curr = self.root_directory
+        if path == "/":
+            return sorted(curr.keys())
 
-    def ls(self, path: str) -> List[str]:
-        curr = self.parent
-        directories = path.split("/")
-        for d in directories:
-            if d == "": continue
-            curr = curr.children[d] # We should always have this path, since gave valid path
-
-        if curr.content:
-            # If path is file path
-            return [d]
-        else:
-            # If path is directory path
-            return sorted(list(curr.children))
-
+        for directory in path.split("/")[1:]:
+            if directory not in curr:
+                return []
+            curr = curr[directory]
+        
+        if len(curr) == 0 and path in self.content:
+            return [path.split("/")[-1]]
+        return sorted(curr.keys())
+        
     def mkdir(self, path: str) -> None:
-        curr = self.parent
-        directories = path.split("/") # Learn how to filter array to avoid /
-        for d in directories:
-            if d == "": continue
-            if d not in curr.children:
-                curr.children[d] = Node()
-            curr = curr.children[d]
+        curr = self.root_directory
+
+        for directory in path.split("/")[1:]:
+            if directory not in curr:
+                curr[directory] = {}
+            curr = curr[directory]
+
 
     def addContentToFile(self, filePath: str, content: str) -> None:
-        curr = self.parent
-        directories = filePath.split("/")
-        for d in directories:
-            if d == "": continue
-            if d not in curr.children:
-                curr.children[d] = Node()
-            curr = curr.children[d]
-        curr.content += content
+        curr = self.root_directory
+
+        for directory in filePath.split("/")[1:]:
+            if directory not in curr:
+                curr[directory] = {}
+            curr = curr[directory]
+
+        if filePath not in self.content:
+            self.content[filePath] = content
+        else:
+            self.content[filePath] += content
 
     def readContentFromFile(self, filePath: str) -> str:
-        curr = self.parent
-        directories = filePath.split("/")
-        for d in directories:
-            if d == "": continue
-            curr = curr.children[d]
-        return curr.content
-        
+        return self.content[filePath]
 
 
 # Your FileSystem object will be instantiated and called as such:
