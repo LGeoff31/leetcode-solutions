c32d8fa46e44065a3cf35613b64673d2b4ce886b
Geoffrey Lee <geoffrey31415@gmail.com>
2026-09-22 10:05:29 -0400

    Runtime: 5290 ms (21.88%), Memory: 53.6 MB (7.81%) - Gitcode

--- 1289-minimum-falling-path-sum-ii/1289. Minimum Falling Path Sum II.py
+++ 1289-minimum-falling-path-sum-ii/1289. Minimum Falling Path Sum II.py
@@ -1,12 +1,16 @@
 class Solution:
-    def minFallingPathSum(self, grid: List[List[int]]) -> int:
-        n=len(grid)
-        m=len(grid[0])
-        for i in range(1,n):
-            ans=heapq.nsmallest(2,grid[i-1])
-            for j in range(m):
-                if grid[i-1][j]==ans[0]:
-                    grid[i][j]+=ans[1]
-                else:
-                    grid[i][j]+=ans[0]
-        return min(grid[-1])                
+    def minFallingPathSum(self, grid: list[list[int]]) -> int:
+        rows, cols = len(grid), len(grid)
+        @cache
+        def dfs(row_level, prev_selected_col):
+            if row_level == rows:
+                return 0
+            
+            res = float('inf')
+            for c in range(cols):
+                if c != prev_selected_col:
+                    res = min(res, grid[row_level][c] + dfs(row_level + 1, c))
+            
+            return res
+        
+        return dfs(0, -1)
