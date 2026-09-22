861cf3793a435c946dd5e00a691172fb285b9734
Geoffrey Lee <geoffrey31415@gmail.com>
2026-09-22 10:12:50 -0400

    Runtime: 9573 ms (5.08%), Memory: 23 MB (52.93%) - Gitcode

--- 1289-minimum-falling-path-sum-ii/1289. Minimum Falling Path Sum II.py
+++ 1289-minimum-falling-path-sum-ii/1289. Minimum Falling Path Sum II.py
@@ -1,16 +1,30 @@
 class Solution:
     def minFallingPathSum(self, grid: list[list[int]]) -> int:
         rows, cols = len(grid), len(grid)
-        @cache
-        def dfs(row_level, prev_selected_col):
-            if row_level == rows:
-                return 0
-            
-            res = float('inf')
-            for c in range(cols):
-                if c != prev_selected_col:
-                    res = min(res, grid[row_level][c] + dfs(row_level + 1, c))
-            
-            return res
+
+        dp = [[float('inf')] * cols for _ in range(rows + 1)] # dp[r][c] = # ways to get to row r, with prev col selection of c on row (r-1)
+
+        for c in range(cols):
+            dp[0][c] = 0
         
-        return dfs(0, -1)
+        for r in range(1, rows+1):
+            for c in range(cols):
+                for nxt_c in range(cols):
+                    if c == nxt_c and cols != 1:
+                        continue 
+                    if dp[r-1][c] != float('inf'):
+                        dp[r][nxt_c] = min(dp[r][nxt_c], dp[r-1][c] + grid[r-1][nxt_c])
+        return min(dp[-1])
+        # @cache
+        # def dfs(row_level, prev_selected_col):
+        #     if row_level == rows:
+        #         return 0
+            
+        #     res = float('inf')
+        #     for c in range(cols):
+        #         if c != prev_selected_col:
+        #             res = min(res, grid[row_level][c] + dfs(row_level + 1, c))
+            
+        #     return res
+        
+        # return dfs(0, -1)
